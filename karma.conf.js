2236e4ce901c784c48a83ee51171e6a7414ede9c
Dominique <batistadominique@hotmail.com>
2019-11-22 15:16:17 +0000

    test build + coverage

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
         ],
 
         webpack: webpackConfig,
-        reporters: ['progress','coverage', 'coveralls'],
+        reporters: ['progress','coverage', 'coveralls', 'kjhtml'],
         browsers: ['ChromeHeadless'],
         preprocessors: {
             'src/**/*spec.js': ['webpack'],
@@ -27,8 +27,24 @@ module.exports = function(config) {
             require('karma-spec-reporter'),
             require('karma-chrome-launcher'),
             require('karma-coveralls'),
-            require('karma-chai')
+            require('karma-chai'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
         ],
+
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, './coverage/Angular8Test'),
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true
+        },
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
 
         coverageReporter: {
             type: 'lcov', // lcov or lcovonly are required for generating lcov.info files

89e75b5cc2b7e4714f6616129b02f9351d21acaf
Terazus <batistadominique@hotmail.com>
2019-11-26 06:30:34 -0800

    styling doc

--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./../components'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -42,7 +46,8 @@ extensions = [
     'sphinx.ext.coverage',
     'recommonmark',
     # Auto-generate section labels.
-    'sphinx.ext.autosectionlabel'
+    'sphinx.ext.autosectionlabel',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -70,7 +75,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -78,7 +83,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

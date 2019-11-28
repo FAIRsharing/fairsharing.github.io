c346a193a23de4f3e812506c8304704eb889aa25
Dominique <batistadominique@hotmail.com>
2019-11-28 15:56:38 +0000

    refactored doc processor to a class and fixed corrsponding unit tests

--- src/documentation/documentation_postprocess.js
+++ src/documentation/documentation_postprocess.js
@@ -7,63 +7,67 @@ It is trigger via the ```npm run doc``` command with an extra param to control t
  */
 let fs = require('fs');
 
-/* This function is the entry point of the documentation post processing. It's the one you want to use.
-    @arg First argument is the path to the documentation components generates by vuese
-    @arg Second argument is the branch name to build the links at the bottom of each âge.
-    @arg Source: a string representing the target of source files
- */
-function process_documentation_files(documentation_path, branch, target){
-    let source_files_path = get_source_files(target);
-    let files = fs.readdirSync(documentation_path);
+export default class DocumentationProcessor {
 
-    files.forEach(function(file){
-        let file_path = documentation_path + '/' + file,
-            file_content = fs.readFileSync(file_path, 'utf-8');
-        let processed_content = process_content(file_content, source_files_path, file_path, branch);
-        fs.writeFileSync(file_path, processed_content, 'utf-8')
-    });
-    return true;
-}
+    constructor(doc_path, branch, src_path) {
+        this.doc_path = doc_path;
+        this.src_path = src_path;
+        this.branch = branch;
 
+        this.source_files = {};
+        this.doc_files = {};
+        this.content = {};
+    }
 
-/* Given a markdown input, adds the link to the source file at the bottom and a backspace after each ``:start-->``` string
+    process_documentation_files(){
+        this.get_source_files(this.src_path);
+        let files = fs.readdirSync(this.doc_path);
+
+        let self = this;
+
+        files.forEach(function(file){
+            let file_path = self.doc_path + '/' + file;
+            let file_content = fs.readFileSync(file_path, 'utf-8');
+            self.doc_files[file] = file_path;
+            let processed_content = self.process_content(file_content, file_path);
+            fs.writeFileSync(file_path, processed_content, 'utf-8');
+            self.content[file.replace('.md', '').replace('.js', '')] = processed_content;
+        });
+    }
+
+    /* This functions builds the dictionary of the source file path  given an input target (should be ```'src'```)
+    @arg path: a string representing the directory to build the source files paths from.
+*/
+    get_source_files(path){
+        let files = fs.readdirSync(path + '/');
+
+        for (let fileIndex in files){
+            let file = files[fileIndex];
+            if ((file.includes('.js') || file.includes('.vue'))
+                && !file.includes('.spec')){
+                let fileName = file.replace('.js', '').replace('.vue', '');
+                this.source_files[fileName] = path + '/' + file;
+            }
+            if (!file.includes(".")){
+                this.get_source_files(path + '/' + file)
+            }
+        }
+    }
+
+    /* Given a markdown input, adds the link to the source file at the bottom and a backspace after each ``:start-->``` string
     @arg Markdown_content: documentation markdown string coming out of vuese output files.
     @arg paths: a dictionary of the source files paths
     @arg path: the path of the current documentation file
     @arf branch: the branch to build the link on
- */
-function process_content(markdown_content, paths, path, branch){
-    let fileNameArray = path.split('/');
-    let fileName = fileNameArray[fileNameArray.length -1].replace('.md', '');
-    let filePath = paths[fileName];
-    markdown_content += "[Find me at " + filePath + "](https://github.com/FAIRsharing/fairsharing.github.io/tree/" + branch  + "/" + filePath + ")";
-
-    return markdown_content.replace(/:start -->/g, ':start -->\n');
-}
-
-
-/* This functions builds the dictionary of the source file path  given an input target (should be ```'src'```)
-    @arg path: a string representing the directory to build the source files paths from.
-*/
-function get_source_files(path){
-    let path_output = {};
-    let files = fs.readdirSync(path + '/');
-
-    for (let fileIndex in files){
-        let file = files[fileIndex];
-        if ((file.includes('.js') || file.includes('.vue'))
-            && !file.includes('.spec')){
-            let fileName = file.replace('.js', '').replace('.vue', '');
-            path_output[fileName] = path + '/' + file;
-        }
-        else if (!file.includes(".")){
-            path_output = {...path_output, ...get_source_files(path + '/' +file) }
-        }
+     */
+    process_content(markdown_content, path){
+        let fileNameArray = path.split('/');
+        let fileName = fileNameArray[fileNameArray.length -1].replace('.md', '');
+        let filePath = this.source_files[fileName];
+        markdown_content += "[Find me at " + filePath + "](https://github.com/FAIRsharing/fairsharing.github.io/tree/" + this.branch  + "/" + filePath + ")";
+        return markdown_content.replace(/:start -->/g, ':start -->\n');
     }
-    return path_output;
 }
 
 
-exports.process_doc = function(target_doc_path, target_source_path){
-    return process_documentation_files(target_doc_path, 'master', target_source_path);
-};
+

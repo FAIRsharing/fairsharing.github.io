b662c689381ae19635b8740436103f58274cab5c
Dominique <batistadominique@hotmail.com>
2019-11-28 15:56:38 +0000

    refactored doc processor to a class and fixed corrsponding unit tests

--- src/documentation/documentation_postprocess.spec.js
+++ src/documentation/documentation_postprocess.spec.js
@@ -8,10 +8,12 @@ describe('Documentation postprocessor ', function () {
 
     beforeEach(() => {
         sinon.stub(fs,'readFileSync').withArgs('file1.md').returns("This is markdown :start -->");
+
         let readStub = sinon.stub(fs, 'readdirSync');
         readStub.withArgs('doc_path').returns(["file1.md", "file2.md"]);
         readStub.withArgs('source_path/').returns(["file1.vue", "file2.js", "directory"]);
-        readStub.withArgs('directory').returns();
+        readStub.withArgs('source_path/directory/').returns(["file3.vue"]);
+
         sinon.stub(fs, 'writeFileSync').withArgs('file1.vue').returns('This is markdown :start -->\n')
         ;
     });
@@ -22,8 +24,35 @@ describe('Documentation postprocessor ', function () {
 
     });
 
-    it('should run without errors', () => {
-        expect(DocProcessor.process_doc('doc_path', 'source_path')).toBe(true);
+    it('initializes correctly', () => {
+        let docProcessor = new DocProcessor('doc_path', 'master', 'source_path');
+        expect(docProcessor.branch).toBe('master');
+        expect(docProcessor.doc_path).toBe('doc_path');
+        expect(docProcessor.src_path).toBe('source_path');
+    });
+
+    it('has a process_documentation_files method', () => {
+        let docProcessor = new DocProcessor('doc_path', 'master', 'source_path');
+        docProcessor.process_documentation_files();
+        expect(JSON.stringify(docProcessor.source_files)).toBe(JSON.stringify({
+            "file1": "source_path/file1.vue",
+            "file2": "source_path/file2.js",
+            "file3": "source_path/directory/file3.vue"
+        }));
+    });
+
+    it('has a process_documentation_files method that builds the output', () => {
+        let docProcessor = new DocProcessor('doc_path', 'master', 'source_path');
+        docProcessor.source_files = {
+            "file1": "source_path/file1.vue"
+        };
+        let content = docProcessor.process_content("abc :start -->", 'source_path/file1.md');
+        expect(JSON.stringify(content)).toBe(JSON.stringify(
+            "abc :start -->\n[Find me at source_path/file1.vue](https://github.com/FAIRsharing/fairsharing.github.io/tree/master/source_path/file1.vue)"
+        ));
     });
 });
+
+
+
 

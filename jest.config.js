e8881e5e10f97294974f0ee1486fb8f4ab81dfe3
Dominique <batistadominique@hotmail.com>
2019-12-04 14:03:26 +0000

    cleaning babel and jest config + router

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest',
+    preset: "@vue/cli-plugin-unit-jest",
     collectCoverage: true,
     coverageReporters: ["lcov"],
     collectCoverageFrom: [
-        'src/**/*.{js,vue}',
-        '!src/main.js',
-        '!src/documentation/process_doc.js'
+        "src/**/*.{js,vue}",
+        "!src/main.js",
+        "!src/documentation/process_doc.js"
     ],
     testMatch: ["**/**.spec.js", "**/**/**.spec.js"]
 };

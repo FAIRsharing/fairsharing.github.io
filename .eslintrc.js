06e35191b2538bd562fb1f43a34b8805680d1718
Terazus <batistadominique@hotmail.com>
2020-01-27 09:58:15 -0800

    minor codacy stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
   root: true,
   env: {
-      node: true,
-      jest: true
+    node: true,
+    jest: true
   },
-  'extends': [
-    'plugin:vue/recommended',
-    'eslint:recommended'
+  "extends": [
+    "plugin:vue/recommended",
+    "eslint:recommended"
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parser: "vue-eslint-parser",
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true

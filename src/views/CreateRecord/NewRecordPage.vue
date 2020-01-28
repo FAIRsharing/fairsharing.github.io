0bc6810f2c5b62dc850801a972d646b4f8bb52fb
Dominique <batistadominique@hotmail.com>
2020-01-28 12:02:38 +0000

    fix newrecords

--- src/views/CreateRecord/NewRecordPage.vue
+++ src/views/CreateRecord/NewRecordPage.vue
@@ -1,23 +1,46 @@
 <template>
-    <div class="new" id="new">
-        <h2>Create new records</h2>
+  <div
+    id="new"
+    class="new"
+  >
+    <h2>Create new records</h2>
 
-        <button type="button" id="new_database_button">
-            <router-link to="/new/database">Database</router-link>
-        </button>
+    <button
+      id="new_database_button"
+      type="button"
+    >
+      <router-link to="/new/database">
+        Database
+      </router-link>
+    </button>
 
-        <button type="button" id="new_standard_button">
-            <router-link to="/new/standard">Standard</router-link>
-        </button>
+    <button
+      id="new_standard_button"
+      type="button"
+    >
+      <router-link to="/new/standard">
+        Standard
+      </router-link>
+    </button>
 
-        <button type="button" id="new_policy_button">
-            <router-link to="/new/policy">Policy</router-link>
-        </button>
+    <button
+      id="new_policy_button"
+      type="button"
+    >
+      <router-link to="/new/policy">
+        Policy
+      </router-link>
+    </button>
 
-        <button type="button" id="new_collection_button">
-            <router-link to="/new/collection">Collection</router-link>
-        </button>
-    </div>
+    <button
+      id="new_collection_button"
+      type="button"
+    >
+      <router-link to="/new/collection">
+        Collection
+      </router-link>
+    </button>
+  </div>
 </template>
 
 <script>

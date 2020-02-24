601dd51fc9eb4d088d06596f626f7d116cbed7c2
Terazus <batistadominique@hotmail.com>
2020-02-24 05:54:45 -0800

    fixed unit tests for vuetify

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,15 @@ module.exports = {
         "!src/main.js",
         "!src/documentation/process_doc.js",
         "!src/components/Records/FiltersLabelMapping.js",
-        "!src/store/index.js"
+        "!src/store/index.js",
+        "!src/plugins/**"
     ],
     testMatch: [
         "**/**.spec.js", "**/**/**.spec.js"
-    ]
+    ],
+    setupFiles: [
+      "./jest-setup.js"
+    ],
+    transform: { '^.*\\.js$': 'babel-jest' },
+    transformIgnorePatterns: ['node_modules/(?!vue-router|@babel|vuetify)']
 };

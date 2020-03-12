cdcec68173c877078d075dcb0b1e9df04c306d2c
Terazus <batistadominique@hotmail.com>
2020-03-12 16:41:41 -0700

    added graphQL query to get the user related records

--- src/views/Users/User.vue
+++ src/views/Users/User.vue
@@ -3,6 +3,8 @@
     Welcome {{ $store.state.users.currentUserID }} <br>
     Your profile is here : <br>
     {{ user }}
+    <v-divider />
+    {{ userDetail }}
     <div>
       <v-btn @click="logoutUser()">
         Logout
@@ -14,18 +16,25 @@
 <script>
     import { mapActions } from "vuex"
     import RESTClient from "@/components/Client/RESTClient.js"
+    import GraphClient from "@/components/GraphClient/GraphClient.js"
+    import query from "@/components/GraphClient/queries/getUser.json"
+
 
     let client = new RESTClient();
+    let graphClient = new GraphClient();
 
     export default {
         name: "User",
         data: () => {
           return {
-              user: null
+              user: null,
+              userDetail: null,
           }
         },
         async mounted(){
-            this.user = await client.getUser(this.$store.state.users.currentUserToken)
+            this.user = await client.getUser(this.$store.state.users.currentUserToken);
+            query.queryParam.id = this.user.id;
+            this.userDetail = await graphClient.executeQuery(query);
         },
         methods: {
             ...mapActions('users', ['logout']),

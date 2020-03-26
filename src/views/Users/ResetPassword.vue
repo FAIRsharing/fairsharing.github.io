1276f2dd54e55bd7abdfa7fbd12534d0d3f9f91c
Terazus <batistadominique@hotmail.com>
2020-03-26 07:47:31 -0700

    Users can now reset their passwords without email when already logged in

--- src/views/Users/ResetPassword.vue
+++ src/views/Users/ResetPassword.vue
@@ -7,6 +7,13 @@
         xl="12"
       >
         <h2>Reset your password</h2>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="12"
+        xl="12"
+      >
         <div
           v-if="message"
           class="alert"
@@ -15,7 +22,16 @@
           {{ message }}
         </div>
 
-        <v-form if="resetPwd">
+        <v-form
+          if="resetPwd"
+        >
+          <v-text-field
+            v-if="userLoggedIn"
+            v-model="formData['oldPwd']"
+            type="password"
+            label="Enter your current password"
+            required
+          />
           <v-text-field
             v-model="formData.password"
             type="password"
@@ -42,6 +58,7 @@
 </template>
 
 <script>
+    import { mapState, mapActions } from "vuex"
     import Client from "@/components/Client/RESTClient.js"
 
     const client = new Client();
@@ -55,27 +72,51 @@
                 formData: {}
             }
         },
-        mounted() {
+      computed: {
+          ...mapState("users", ["userLoggedIn", "currentUserToken", "errors"])
+      },
+      mounted() {
             this.$nextTick(async function () {
+              if (!this.userLoggedIn) {
                 const params = this.$route.query;
-                if (!Object.keys(params).includes('reset_password_token')){
-                    this.error = true;
-                    this.message = "Missing token"
+                if (!Object.keys(params).includes('reset_password_token')) {
+                  this.error = true;
+                  this.message = "Missing token"
                 }
+              }
             })
         },
         methods: {
+            ...mapActions("users", ["resetPwdWithoutToken"]),
             submitPassword: async function(){
                 let query = {
                     password: this.formData.password,
-                    password_confirmation: this.formData.passwordRepeat,
-                    reset_password_token: this.$route.query.reset_password_token
+                    password_confirmation: this.formData['passwordRepeat'],
                 };
-                let response = await client.resetPassword(query);
-                this.message = response;
-                if (response.error){
+
+                // Not logged in
+                if (!this.userLoggedIn){
+                  query.reset_password_token = this.$route.query.reset_password_token;
+                  let response = await client.resetPassword(query);
+                  this.message = response;
+                  if (response.error){
                     this.error = true;
                     this.message = response.error
+                  }
+                }
+
+                // Logged in users
+                else {
+                  query.current_password = this.formData['oldPwd'];
+                  await this.resetPwdWithoutToken(query);
+                  if (!this.errors){
+                    this.$router.push({path: "/accounts/login"})
+                  }
+                  else{
+                    this.error = true;
+                    this.message = this.errors
+                  }
+
                 }
             }
         }

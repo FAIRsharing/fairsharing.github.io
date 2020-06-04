49a1c9527d06615e46d542e8789cc72b6793a8e2
Terazus <batistadominique@hotmail.com>
2020-06-04 03:35:17 -0700

    finished tests up to standard

--- tests/unit/components/Editor/EditGeneralInfo.spec.js
+++ tests/unit/components/Editor/EditGeneralInfo.spec.js
@@ -7,34 +7,49 @@ import recordStore from "@/store/record.js";
 import userStore from "@/store/users.js";
 import status from "@/components/Editor/status.json"
 import GraphClient from "@/components/GraphClient/GraphClient.js";
-// import metaTemplate from "../../../fixtures/metaTemplate.json"
+import RestClient from "@/components/Client/RESTClient.js";
+import typesQuery from "@/components/GraphClient/queries/getRecordsTypes.json"
+import countriesQuery from "@/components/GraphClient/queries/getCountries.json"
+import metaTemplate from "../../../fixtures/metaTemplate.json"
 const sinon = require("sinon");
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 const vuetify = new Vuetify();
-recordStore.state.metaTemplate = {
-    type: 'type1',
-    status: 'ready'
-};;
+
+const countriesReady = [
+    {
+        name: "uk",
+        id: 1
+    },
+    {
+        name: "fr",
+        id: 2
+    }
+];
+let recordReady = metaTemplate.data;
+recordReady.countries = countriesReady;
+recordStore.state.metaTemplate = recordReady;
+userStore.state.user().credentials.token = "thisisatoken";
 const $store = new Vuex.Store({
     modules: {
         users: userStore,
         record: recordStore
     }
 });
-let $route = { name: "New_content", path: "/new" };
+let $route = { path: "/123/edit", params: {id: 123} };
 const router = new VueRouter();
 const $router = { push: jest.fn() };
 
 let graphStub;
+let restStub;
 
 describe("CreateRecord.vue", function() {
     let wrapper;
 
-    it("can be instantiated", async () => {
+    beforeEach( async () => {
         graphStub = sinon.stub(GraphClient.prototype, "executeQuery");
-        graphStub.returns({
+        graphStub.withArgs(typesQuery).returns({
             fairsharingRegistries: {
                 records: [
                     {
@@ -60,55 +75,49 @@ describe("CreateRecord.vue", function() {
                 ]
             }
         });
+        graphStub.withArgs(countriesQuery).returns({
+            searchCountries: countriesReady
+        });
+        restStub = sinon.stub(RestClient.prototype, "executeQuery");
+        restStub.returns({
+            data: {data: { id: 123}}
+        });
         wrapper = await shallowMount(editMeta, {
             localVue,
             vuetify,
             router,
             mocks: {$store, $route, $router}
         });
-        expect(wrapper.name()).toMatch("EditGeneralInfo");
-        expect(wrapper.vm.status).toStrictEqual(status.status);
+    });
+    afterEach(() => {
         graphStub.restore();
+        restStub.restore();
     });
 
-    it("can reaction to type change", async () => {
-        graphStub = sinon.stub(GraphClient.prototype, "executeQuery");
-        graphStub.returns({
-            fairsharingRegistries: {
-                records: [
-                    {
-                        name: "abc",
-                        id: 1,
-                        recordTypes: [
-                            {
-                                name: "def",
-                                id: 1
-                            }
-                        ]
-                    },
-                    {
-                        name: "tfc",
-                        id: 5,
-                        recordTypes: [
-                            {
-                                name: "jhb",
-                                id: 2
-                            }
-                        ]
-                    }
-                ]
-            }
-        });
-        wrapper = await shallowMount(editMeta, {
-            localVue,
-            vuetify,
-            router,
-            mocks: {$store, $route, $router}
-        });
+    it("can be instantiated", async () => {
+        expect(wrapper.name()).toMatch("EditGeneralInfo");
+        expect(wrapper.vm.status).toStrictEqual(status.status);
+        let countries = await wrapper.vm.getCountries();
+        expect(countries).toStrictEqual(countriesReady);
+    });
+
+    it("can react to type/registry change", async () => {
         expect(wrapper.vm.metaTemplate).toStrictEqual(recordStore.state.metaTemplate);
         wrapper.vm.metaTemplate.type = 'collection' ;
         expect(wrapper.vm.metaTemplate.status).toMatch("uncertain");
         wrapper.vm.metaTemplate.type = 'test' ;
-        graphStub.restore();
+    });
+
+    it("can post an edited record", async () => {
+        wrapper.vm.metaTemplate.type = {
+            id: 1
+        };
+        await wrapper.vm.editRecord();
+        expect($router.push).toHaveBeenCalledWith({"path": "/123"});
+        expect($router.push).toHaveBeenCalledTimes(1);
+        restStub.restore();
+        restStub.returns(new Error("Im an error"));
+        await wrapper.vm.editRecord();
+        expect($router.push).toHaveBeenCalledTimes(1);
     })
 });

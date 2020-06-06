907c8f117c17184ab6b6ee1f7d7800eb6150b697
hosein_mirian <hosein.mirian@gmail.com>
2020-06-06 16:50:39 +0100

    arm component is a test for mapActions

---
+++ src/components/Records-new/Arm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-btn
+      style="position: fixed;left: 30%;z-index: 3000;background: red;color: black"
+      @click="callAction"
+    >
+      Click
+    </v-btn>
+
+    <b>{{ countInside }}</b>
+  </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        name: "Arm",
+        props: {
+            countInside: {default: null, type: Number}
+        }, methods: {
+            ...mapActions('uiController', ["callAction"]),
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

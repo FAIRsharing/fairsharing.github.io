decb110104d84b7d488dd896db7b2ff24acac804
hosein.mirian <hosein.mirian@gmail.com>
2020-06-18 12:48:08 +0100

    changes in store and Record.vue

--- src/views/Records-new/Record.vue
+++ src/views/Records-new/Record.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-content>
+  <v-main>
     <h1 class="d-none">
       Content
     </h1>
@@ -534,7 +534,7 @@
     >
       {{ error }}
     </div>
-  </v-content>
+  </v-main>
 </template>
 
 <script>

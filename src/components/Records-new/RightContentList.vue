ff60cbd7fc75349e53c1594d2d5cbf3d43bce507
hosein.mirian <hosein.mirian@gmail.com>
2020-06-19 13:28:48 +0100

    added chips holder and filteredChips

--- src/components/Records-new/RightContentList.vue
+++ src/components/Records-new/RightContentList.vue
@@ -1,58 +1,58 @@
 <template>
-  <section>
-    <h1 class="d-none">
-      Records
-    </h1>
-    <!--Filtered Chips-->
-    <div style="background: lightblue;width: 100%;height: 50px;display: flex;justify-content: center;align-items: center">
-      place of filter Chip
-    </div>
+    <section>
+        <h1 class="d-none">
+            Records
+        </h1>
+        <!--Filtered Chips-->
+        <div class="d-flex align-content-center justify-content-center chips-holder">
+            <FiltersChip/>
+        </div>
 
-    <!--List Controller-->
-    <ListController
-      class="mt-2"
-      @ChangeListType="changeListType"
-    />
+        <!--List Controller-->
+        <ListController
+                class="mt-2"
+                @ChangeListType="changeListType"
+        />
 
-    <!--List Row-->
-    <div :class="['opacity-0-transition',{'opacity-1-transition':!isColumnList}]">
-      <article v-if="!isColumnList">
-        <v-skeleton-loader
-          class="mt-5"
-          :loading="loading"
-          type="card"
-        >
-          <h2 class="d-none">
-            Result
-          </h2>
-          <RecordsCardStack
-            v-for="record in records"
-            :key="'record_'+record.id"
-            :record="record"
-          />
-        </v-skeleton-loader>
-      </article>
-    </div>
+        <!--List Row-->
+        <div :class="['opacity-0-transition',{'opacity-1-transition':!isColumnList}]">
+            <article v-if="!isColumnList">
+                <v-skeleton-loader
+                        class="mt-5"
+                        :loading="loading"
+                        type="card"
+                >
+                    <h2 class="d-none">
+                        Result
+                    </h2>
+                    <RecordsCardStack
+                            v-for="record in records"
+                            :key="'record_'+record.id"
+                            :record="record"
+                    />
+                </v-skeleton-loader>
+            </article>
+        </div>
 
-    <div :class="['opacity-0-transition',{'opacity-1-transition':isColumnList}]">
-      <v-skeleton-loader
-        class="mt-5"
-        :loading="loading"
-        type="card"
-      >
-        <v-row v-show="isColumnList">
-          <RecordsCardColumn
-            v-for="record in records"
-            :key="'record_'+record.id"
-            :record="record"
-          />
-        </v-row>
-      </v-skeleton-loader>
-    </div>
+        <div :class="['opacity-0-transition',{'opacity-1-transition':isColumnList}]">
+            <v-skeleton-loader
+                    class="mt-5"
+                    :loading="loading"
+                    type="card"
+            >
+                <v-row v-show="isColumnList">
+                    <RecordsCardColumn
+                            v-for="record in records"
+                            :key="'record_'+record.id"
+                            :record="record"
+                    />
+                </v-row>
+            </v-skeleton-loader>
+        </div>
 
-    <!--List Controller-->
-    <!--        <ListController class="mt-2 " @ChangeListType="changeListType"></ListController>-->
-  </section>
+        <!--List Controller-->
+        <!--        <ListController class="mt-2 " @ChangeListType="changeListType"></ListController>-->
+    </section>
 </template>
 
 <script>
@@ -60,10 +60,11 @@
     import ListController from "./ListController";
     import RecordsCardColumn from "./RecordsCardColumn";
     import {mapState} from 'vuex'
+    import FiltersChip from "../Records-new/FilterChips";
 
     export default {
         name: "RightContentList",
-        components: {RecordsCardColumn, ListController, RecordsCardStack},
+        components: {FiltersChip, RecordsCardColumn, ListController, RecordsCardStack},
         data() {
             return {
                 isColumnList: false
@@ -79,3 +80,13 @@
         },
     }
 </script>
+<style lang="scss">
+    .chips-holder {
+        background: #f5f5f5;
+        height: 50px;
+        border: #dbdbdb dotted 2px;
+        border-radius: 10px;
+        -moz-border-radius: 10px;
+        -webkit-border-radius: 10px;
+    }
+</style>

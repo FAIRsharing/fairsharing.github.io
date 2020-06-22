c47fb421e5b8bae970ebdb8c682550db9ee39ba3
hosein.mirian <hosein.mirian@gmail.com>
2020-06-22 11:41:50 +0100

    updated the branch aligned with dev

--- src/components/Editor/EditKeywords.vue
+++ src/components/Editor/EditKeywords.vue
@@ -217,266 +217,255 @@
 </template>
 
 <script>
-    import { mapGetters, mapActions, mapState } from "vuex"
-    import GraphClient from "@/components/GraphClient/GraphClient.js"
-    import RestClient from "@/components/Client/RESTClient.js"
-    import domainsQuery from "@/components/GraphClient/queries/getDomains.json"
-    import taxonomiesQuery from "@/components/GraphClient/queries/getTaxonomies.json"
-    import subjectsQuery from "@/components/GraphClient/queries/getSubjects.json"
-    import userTagQuery from "@/components/GraphClient/queries/getUserDefinedTags.json"
-    import desc from './data/fieldsDescription.json'
-
-    let graphClient = new GraphClient();
-    let restClient = new RestClient();
-
-    export default {
-        name: "EditKeywords",
-        data(){
-            return {
-                fields: [
-                    "domains",
-                    "taxonomies",
-                    "subjects",
-                    "userDefinedTags"
-                ],
-                keywords: [],
-                headers: [
-                    {
-                        text: "Type of keyword",
-                        sortable: true,
-                        value: "type"
-                    },
-                    {
-                        text: "Name",
-                        sortable: true,
-                        value: "label"
-                    },
-                    {
-                        text: "Definition",
-                        sortable: false,
-                        value: "definitions",
-                        filterable: false
-                    },
-                    {
-                        text: "Alternative names",
-                        sortable: true,
-                        value: "synonyms"
-                    }
-                ],
-                selectedKeywords: [],
-                recordKeywords: [],
-                initialized: false,
-                search: "",
-                showTypes: [
-                    "domains",
-                    "subjects",
-                    "species",
-                    "userDefinedTags"
-                ],
-                colors: {
-                  "domains": "blue",
-                  "subjects": "orange",
-                  "species": "green",
-                  "taxonomies": "green",
-                  "userDefinedTags": "grey"
-                },
-                error: null
-            }
-        },
-        computed: {
-            ...mapGetters("record", ["getField"]),
-            ...mapState("users", ["user"]),
-            ...mapState("record", ["recordUpdate"]),
-            getKeywords(){
-                const _module = this;
-                let keywords = [];
-                _module.keywords.forEach(function(keyword){
-                  if (_module.showTypes.indexOf(keyword.type) > -1){
-                    keywords.push(keyword);
-                  }
-                });
-                return keywords;
-            },
-            getSelectedTerms(){
-              const _module = this;
-              let added = false;
-              let terms = {
-                domains: [],
-                subjects: [],
-                species: [],
-                userDefinedTags: []
-              };
-              if (_module.recordKeywords.length > 0){
-                added = true;
-                _module.recordKeywords.forEach(function(keyword){
-                  if (keyword.type === "taxonomies"){
-                    terms["species"].push(keyword)
-                  }
-                  else {
-                    terms[keyword.type].push(keyword)
-                  }
-
-                });
-              }
-              return (added) ? terms : null;
-            },
-            descriptions(){
-                return desc.descriptions;
-            }
-        },
-        watch: {
-            selectedKeywords: {
-                deep: true,
-                handler: function(after){
-                  const _module = this;
-                  if (_module.initialized){
-                    _module.recordKeywords = after;
-                  }
-                }
-            },
-            recordKeywords: {
-                deep: true,
-                handler: function(after){
-                  const _module = this;
-                  if (_module.initialized){
-                    _module.selectedKeywords = after;
-                  }
-                }
-          }
-        },
-        async mounted(){
-          await this.$nextTick(async function () {
-              let _module = this;
-              await _module.getData();
-              _module.initialized = true;
-          })
-        },
-        methods: {
-            ...mapActions("record", ["updateRecord"]),
-            getFields: function(){
-              let currentKeywords = [];
-              const _module = this;
-              _module.fields.forEach(function(field){
-                let values = _module.getField(field);
-                if (values){
-                  values.forEach(function(val){
-                    val.type = field;
-                    currentKeywords.push(val)
-                  })
-                }
-              });
-            return currentKeywords;
+  import { mapGetters, mapActions, mapState } from "vuex"
+  import GraphClient from "@/components/GraphClient/GraphClient.js"
+  import RestClient from "@/components/Client/RESTClient.js"
+  import domainsQuery from "@/components/GraphClient/queries/getDomains.json"
+  import taxonomiesQuery from "@/components/GraphClient/queries/getTaxonomies.json"
+  import subjectsQuery from "@/components/GraphClient/queries/getSubjects.json"
+  import userTagQuery from "@/components/GraphClient/queries/getUserDefinedTags.json"
+  import desc from './data/fieldsDescription.json'
+  let graphClient = new GraphClient();
+  let restClient = new RestClient();
+  export default {
+    name: "EditKeywords",
+    data(){
+      return {
+        fields: [
+          "domains",
+          "taxonomies",
+          "subjects",
+          "userDefinedTags"
+        ],
+        keywords: [],
+        headers: [
+          {
+            text: "Type of keyword",
+            sortable: true,
+            value: "type"
           },
-            getDomains: async function(){
-                let domains = await graphClient.executeQuery(domainsQuery);
-                domains = domains['searchDomains'].filter(obj => obj['inFairsharing'] === true);
-                return domains
-            },
-            getTaxonomies: async function(){
-                let taxonomies = await graphClient.executeQuery(taxonomiesQuery);
-                return taxonomies["searchTaxonomies"]
-            },
-            getSubjects: async function(){
-                let subjects = await graphClient.executeQuery(subjectsQuery);
-                return subjects["searchSubjects"]
-            },
-            getUserTags: async function(){
-                let userTag = await graphClient.executeQuery(userTagQuery);
-                return userTag["searchUserDefinedTags"]
-            },
-            getData: async function(){
-                const _module = this;
-                _module.addToKeywords(await _module.getDomains(), "domains");
-                _module.addToKeywords(await _module.getTaxonomies(), "species");
-                _module.addToKeywords(await _module.getSubjects(), "subjects");
-                _module.addToKeywords(await _module.getUserTags(), "userDefinedTags");
-                _module.recordKeywords = _module.getFields();
-                _module.recordKeywords.forEach(function(field){
-                    let keyword = _module.keywords.filter(obj => obj.label === field.label);
-                    _module.selectedKeywords.push(keyword[0]);
-                });
-                _module.keywords.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
-            },
-            addToKeywords(data, type){
-                const _module = this;
-                data.forEach(function(val){
-                    val.type = type;
-                    _module.keywords.push(val);
-                })
-            },
-            removeKeyword(keyword){
-              const _module = this;
-              _module.recordKeywords = _module.recordKeywords.filter(obj => obj.label !== keyword.label && obj.id !== keyword.id);
-            },
-            async submitRecord(){
-              const _module = this;
-              let preparedRecord = {
-                domain_ids: [],
-                subject_ids: [],
-                taxonomy_ids: [],
-                user_defined_tag_ids: []
-              };
-
-              const types = {
-                domains: "domain_ids",
-                subjects: "subject_ids",
-                taxonomies: "taxonomy_ids",
-                species: "taxonomy_ids",
-                userDefinedTags: "user_defined_tag_ids"
-              };
-
-              _module.recordKeywords.forEach(function(keyword){
-                  preparedRecord[types[keyword.type]].push(keyword.id)
-              });
-
-              let data = {
-                record: preparedRecord,
-                id: _module.$route.params.id,
-                token: _module.user().credentials.token
-              };
-
-              // POST THE DATA AND REACT THE RESPONSE
-              await _module.updateRecord(data);
-              if (!_module.recordUpdate.error){
-                let ID = _module.recordUpdate.id.data.id;
-                _module.$router.push({
-                  path: "/" + ID
-                })
-              }
-              else {
-                _module.error = _module.recordUpdate;
-              }
-            },
-            async createNewTerm(){
-              const _module = this;
-                let newTerm = await restClient.createNewUserDefinedTag(_module.search, _module.user().credentials.token);
-                let addTerm = {
-                    label: newTerm.label,
-                    id: newTerm.id,
-                    type: "userDefinedTags"
-                };
-                _module.selectedKeywords.push(addTerm);
-                _module.keywords.push(addTerm);
+          {
+            text: "Name",
+            sortable: true,
+            value: "label"
+          },
+          {
+            text: "Definition",
+            sortable: false,
+            value: "definitions",
+            filterable: false
+          },
+          {
+            text: "Alternative names",
+            sortable: true,
+            value: "synonyms"
+          }
+        ],
+        selectedKeywords: [],
+        recordKeywords: [],
+        initialized: false,
+        search: "",
+        showTypes: [
+          "domains",
+          "subjects",
+          "species",
+          "userDefinedTags"
+        ],
+        colors: {
+          "domains": "blue",
+          "subjects": "orange",
+          "species": "green",
+          "taxonomies": "green",
+          "userDefinedTags": "grey"
+        },
+        error: null
+      }
+    },
+    computed: {
+      ...mapGetters("record", ["getField"]),
+      ...mapState("users", ["user"]),
+      ...mapState("record", ["recordUpdate"]),
+      getKeywords(){
+        const _module = this;
+        let keywords = [];
+        _module.keywords.forEach(function(keyword){
+          if (_module.showTypes.indexOf(keyword.type) > -1){
+            keywords.push(keyword);
+          }
+        });
+        return keywords;
+      },
+      getSelectedTerms(){
+        const _module = this;
+        let added = false;
+        let terms = {
+          domains: [],
+          subjects: [],
+          species: [],
+          userDefinedTags: []
+        };
+        if (_module.recordKeywords.length > 0){
+          added = true;
+          _module.recordKeywords.forEach(function(keyword){
+            if (keyword.type === "taxonomies"){
+              terms["species"].push(keyword)
             }
+            else {
+              terms[keyword.type].push(keyword)
+            }
+          });
         }
+        return (added) ? terms : null;
+      },
+      descriptions(){
+        return desc.descriptions;
+      }
+    },
+    watch: {
+      selectedKeywords: {
+        deep: true,
+        handler: function(after){
+          const _module = this;
+          if (_module.initialized){
+            _module.recordKeywords = after;
+          }
+        }
+      },
+      recordKeywords: {
+        deep: true,
+        handler: function(after){
+          const _module = this;
+          if (_module.initialized){
+            _module.selectedKeywords = after;
+          }
+        }
+      }
+    },
+    async mounted(){
+      await this.$nextTick(async function () {
+        let _module = this;
+        await _module.getData();
+        _module.initialized = true;
+      })
+    },
+    methods: {
+      ...mapActions("record", ["updateRecord"]),
+      getFields: function(){
+        let currentKeywords = [];
+        const _module = this;
+        _module.fields.forEach(function(field){
+          let values = _module.getField(field);
+          if (values){
+            values.forEach(function(val){
+              val.type = field;
+              currentKeywords.push(val)
+            })
+          }
+        });
+        return currentKeywords;
+      },
+      getDomains: async function(){
+        let domains = await graphClient.executeQuery(domainsQuery);
+        domains = domains['searchDomains'].filter(obj => obj['inFairsharing'] === true);
+        return domains
+      },
+      getTaxonomies: async function(){
+        let taxonomies = await graphClient.executeQuery(taxonomiesQuery);
+        return taxonomies["searchTaxonomies"]
+      },
+      getSubjects: async function(){
+        let subjects = await graphClient.executeQuery(subjectsQuery);
+        return subjects["searchSubjects"]
+      },
+      getUserTags: async function(){
+        let userTag = await graphClient.executeQuery(userTagQuery);
+        return userTag["searchUserDefinedTags"]
+      },
+      getData: async function(){
+        const _module = this;
+        _module.addToKeywords(await _module.getDomains(), "domains");
+        _module.addToKeywords(await _module.getTaxonomies(), "species");
+        _module.addToKeywords(await _module.getSubjects(), "subjects");
+        _module.addToKeywords(await _module.getUserTags(), "userDefinedTags");
+        _module.recordKeywords = _module.getFields();
+        _module.recordKeywords.forEach(function(field){
+          let keyword = _module.keywords.filter(obj => obj.label === field.label);
+          _module.selectedKeywords.push(keyword[0]);
+        });
+        _module.keywords.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
+      },
+      addToKeywords(data, type){
+        const _module = this;
+        data.forEach(function(val){
+          val.type = type;
+          _module.keywords.push(val);
+        })
+      },
+      removeKeyword(keyword){
+        const _module = this;
+        _module.recordKeywords = _module.recordKeywords.filter(obj => obj.label !== keyword.label && obj.id !== keyword.id);
+      },
+      async submitRecord(){
+        const _module = this;
+        let preparedRecord = {
+          domain_ids: [],
+          subject_ids: [],
+          taxonomy_ids: [],
+          user_defined_tag_ids: []
+        };
+        const types = {
+          domains: "domain_ids",
+          subjects: "subject_ids",
+          taxonomies: "taxonomy_ids",
+          species: "taxonomy_ids",
+          userDefinedTags: "user_defined_tag_ids"
+        };
+        _module.recordKeywords.forEach(function(keyword){
+          preparedRecord[types[keyword.type]].push(keyword.id)
+        });
+        let data = {
+          record: preparedRecord,
+          id: _module.$route.params.id,
+          token: _module.user().credentials.token
+        };
+        // POST THE DATA AND REACT THE RESPONSE
+        await _module.updateRecord(data);
+        if (!_module.recordUpdate.error){
+          let ID = _module.recordUpdate.id.data.id;
+          _module.$router.push({
+            path: "/" + ID
+          })
+        }
+        else {
+          _module.error = _module.recordUpdate;
+        }
+      },
+      async createNewTerm(){
+        const _module = this;
+        let newTerm = await restClient.createNewUserDefinedTag(_module.search, _module.user().credentials.token);
+        let addTerm = {
+          label: newTerm.label,
+          id: newTerm.id,
+          type: "userDefinedTags"
+        };
+        _module.selectedKeywords.push(addTerm);
+        _module.keywords.push(addTerm);
+      }
     }
+  }
 </script>
 
 <style>
   table td.selectTerm{
     width: 70px;
   }
-
   table td.termType{
     width: 200px;
   }
-
   table td.termLabel{
     width: 250px;
   }
-
   table td.termSynonyms {
     max-width: 900px;
   }
-
 </style>

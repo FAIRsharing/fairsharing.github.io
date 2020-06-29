1d6c1dca6b35341a873ecc875f4d4856c4aab547
hosein.mirian <hosein.mirian@gmail.com>
2020-06-29 10:26:53 +0100

    start writing the test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
-  <v-app>
-    <template>
-      <div id="app">
-        <navbar-top />
-        <router-view class="py-5" />
-        <footer-bar />
-      </div>
-    </template>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-if="$vuetify.breakpoint.smAndDown"
+      v-model="UIGeneralStatus.drawerVisibilityState"
+      app
+      left
+      width="70%"
+    />
+
+    <transition name="fade">
+      <Header v-if="UIGeneralStatus.headerVisibilityState" />
+    </transition>
+    <router-view />
   </v-app>
 </template>
 
 <script>
-    import NavbarTop from "./components/Navigation/NavbarTop";
-    import FooterBar from "./components/Navigation/Footer";
+    import Header from "./components/IndividualComponents/Header";
+    import {mapState} from 'vuex';
 
     export default {
-        name: 'App',
-        components: {
-            NavbarTop,
-            FooterBar
-        }
-    };
+        components: {Header},
+        data: () => ({
+            hideOverflow: 'overflow-hidden'
+        }),
+        computed: {
+            ...mapState('uiController', ["UIGeneralStatus"]),
+        },
+        watch: {
+            UIGeneralStatus: {
+                handler(UIGeneralStatus) {
+                    this.toggleOverFlow(UIGeneralStatus.bodyOverflowState);
+                },
+                deep: true
+            },
+        },
+        created() {
+            /*
+                        this.$vuetify.theme.dark = true;
+                        console.log( this.$vuetify.theme);
+                         console.log( this.$vuetify.icons.values);
+                         console.log( this.$vuetify);
+                        console.log( this.$vuetify.breakpoint);
+                        console.log( this.$vuetify.breakpoint.width + ' '+this.$vuetify.breakpoint.height);
+            */
+        },
+        methods: {
+            toggleOverFlow: function (status) {
+                let root = document.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
+                status ? root.setAttribute('class', this.hideOverflow) : root.removeAttribute('class');
+            },
+        },
+    }
 </script>
 
-<style>
+<style lang="scss">
     #app {
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         min-height: 99vh;
     }
+
+    html, body {
+        height: 100%;
+    }
+
+    .footer-content {
+
+    }
+
+    .overflow-hidden {
+        overflow: hidden !important;
+    }
+
+
 </style>

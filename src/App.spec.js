1f0aafd92e2b600474612fd617a7b7f179d1e34e
hosein.mirian <hosein.mirian@gmail.com>
2020-06-29 13:28:47 +0100

    header test completed plus some cleanup

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,19 +1,49 @@
 import {shallowMount, createLocalVue} from "@vue/test-utils";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import Vuetify from "vuetify"
+
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
+
+let $route = { path: "/123/edit", params: {id: 123} };
 const router = new VueRouter();
+// const $router = { push: jest.fn() };
 
 
 describe("App.vue", () => {
+    let wrapper;
+    const vuetify = new Vuetify();
+
     it("can be instantiated", () => {
         const title = "App";
-        const wrapper = shallowMount(App, {
+        wrapper = shallowMount(App, {
             localVue,
-            router
+            router,
+            vuetify,
+            mocks: {$route, vuetify:{breakpoint:{smAndDown:false}}}
         });
         expect(wrapper.name()).toMatch(title);
-    })
+    });
+
+
+    it("can check the toggleOverFlow", () => {
+        wrapper.vm.toggleOverFlow(false);
+        expect(wrapper.root).toBe(null);
+    });
+
+    /*   it("can check watcher", () => {
+           console.log(wrapper.vm.UIGeneralStatus)
+
+           wrapper.vm.UIGeneralStatus.recordType = {
+               name: "collection"
+           };
+           expect(wrapper.vm.models).toStrictEqual({
+               recordType: {name: "collection"},
+               recordStatus: "uncertain"
+           });
+    });
+   */
+
 });

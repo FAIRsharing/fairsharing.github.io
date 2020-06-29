980eba45e8c848b282376d146e0a81b5c4a2e358
hosein.mirian <hosein.mirian@gmail.com>
2020-06-29 10:26:53 +0100

    start writing the test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 /*  import base */
 import Vue from "vue";
-// import App from "./App.vue";
-import App from "./App2.vue";
+import App from "./App.vue";
 import VueMeta from "vue-meta";
 import vuetify from './plugins/vuetify'
 

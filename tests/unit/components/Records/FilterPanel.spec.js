064a93db8113e60d8d466d25e1a68788e562f24a
hosein.mirian <hosein.mirian@gmail.com>
2020-07-01 14:58:22 +0100

    completed FilterPanel Test

---
+++ tests/unit/components/Records/FilterPanel.spec.js
@@ -0,0 +1,45 @@
+import {createLocalVue, shallowMount} from "@vue/test-utils";
+import Vuex from "vuex";
+import Vuetify from "vuetify"
+import FilterPanel from "@/components/Records/FilterPanel.vue"
+import searchFiltersStore from "@/store/searchFilters.js";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+const vuetify = new Vuetify();
+
+let $route = {
+    name: "search",
+    query: {}
+};
+
+const $router = {
+    push: jest.fn(),
+};
+
+const $store = new Vuex.Store({
+    modules: {
+        searchFilters: searchFiltersStore,
+    }
+});
+
+describe("FilterPanel.vue", function () {
+    let wrapper;
+
+    wrapper = shallowMount(FilterPanel, {
+        localVue,
+        vuetify,
+        mocks: {$store, $router, $route}
+    });
+
+    it("can check setup function", () => {
+        $store.state.searchFilters.filters.push({filterName: 'filterNameTest', filterLabel: 'filterLabelTest'});
+        wrapper.vm.setPanel();
+        wrapper.vm.createIndexForFilters();
+    });
+
+    it("can check resetPanel", () => {
+        wrapper.vm.resetPanel();
+        expect(wrapper.vm.panel).toStrictEqual([])
+    })
+});

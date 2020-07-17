f7062f25ee4afddf46f144e5036b0b53cc8ab064
hosein.mirian <hosein.mirian@gmail.com>
2020-07-17 13:18:44 +0100

    uiController tests are 100 percent covered

---
+++ tests/unit/store/uiController.spec.js
@@ -0,0 +1,64 @@
+import {actions, mutations} from "@/store/uiController.js"
+
+describe('Mutations & Actions', () => {
+
+    let state = {};
+
+    beforeEach(() => {
+        state = {
+            scrollStatus: false,
+            UIGeneralStatus: {
+                bodyOverflowState: false,
+                drawerVisibilityState: false,
+                headerVisibilityState: false,
+            },
+        };
+    });
+
+    it("can check the setGeneralUIAttributesAction action", () => {
+
+        let fakeObject = {
+            bodyOverflowState: false,
+            drawerVisibilityState: false,
+            headerVisibilityState: true,
+        }
+        actions.commit = jest.fn();
+        actions.setGeneralUIAttributesAction(state, fakeObject);
+        expect(actions.commit).toHaveBeenCalledTimes(1);
+
+    });
+
+    it("can check the setScrollStatus action", () => {
+
+        let fakeState = true;
+        actions.commit = jest.fn();
+        actions.setScrollStatus(state, fakeState);
+        expect(actions.commit).toHaveBeenCalledTimes(1);
+
+    });
+
+    it("can check the setScrollStatus Mutations", () => {
+
+        let fakeState = true;
+        mutations.setScrollStatus(state, fakeState);
+        expect(state.scrollStatus).toBe(true);
+
+    });
+
+    it("can check the setUIStatus Mutations", () => {
+
+        const fakeUIStateObject_caseA = {
+            bodyOverflowState: false,
+            drawerVisibilityState: false,
+            headerVisibilityState: true,
+        };
+        mutations.setUIStatus(state, fakeUIStateObject_caseA);
+        expect(state.UIGeneralStatus).toStrictEqual(fakeUIStateObject_caseA);
+
+        const fakeUIStateObject_caseB = {}
+        mutations.setUIStatus(state, fakeUIStateObject_caseB);
+        expect(state.UIGeneralStatus).toStrictEqual(fakeUIStateObject_caseA);
+
+    });
+
+});

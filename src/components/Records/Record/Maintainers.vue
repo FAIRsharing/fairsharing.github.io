cdddfa56c93160c856774d5547d7bdc98ed0aa07
Milo Thurston <knirirr@gmail.com>
2020-07-24 14:18:38 +0100

    Added "none found" field if some values are not present.

--- src/components/Records/Record/Maintainers.vue
+++ src/components/Records/Record/Maintainers.vue
@@ -6,6 +6,19 @@
     elevation="1"
   >
     <SectionTitle title="Maintainers" />
+    <v-card
+      v-if="!getField('maintainers').length"
+      class="pr-2 pl-4 pt-1 pb-2 d-flex flex-column"
+      :class="index === 0 ? 'mt-4':'mt-2'"
+      flat
+      outlined
+    >
+      <div class="d-flex mt-2 ">
+        <p class="ma-0">
+          None found.
+        </p>
+      </div>
+    </v-card>
     <!--Contact-->
     <v-card
       v-for="(maintainer, index) in getField('maintainers')"

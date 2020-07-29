8a431c5fb7a08b9bc4d0d7c8706213548ded4b7b
hosein.mirian <hosein.mirian@gmail.com>
2020-07-29 20:53:07 +0100

    no data available added in Records.vue as well as if one result is available fixed the scroll fixed #355

--- src/components/Records/Search/Output/SearchOutput.vue
+++ src/components/Records/Search/Output/SearchOutput.vue
@@ -15,7 +15,10 @@
     />
 
     <!--List Row-->
-    <div :class="['opacity-0-transition',{'opacity-1-transition':!isColumnList}]">
+    <div
+      v-if="records.length"
+      :class="['opacity-0-transition',{'opacity-1-transition':!isColumnList}]"
+    >
       <article v-if="!isColumnList">
         <v-skeleton-loader
           class="mt-5"
@@ -33,15 +36,31 @@
         </v-skeleton-loader>
       </article>
     </div>
+    <div
+      v-else-if="records.length<1 && !loading"
+      class="no-data-found"
+    >
+      <v-alert
+        color="gray"
+        icon="info"
+      >
+        No Data Found!
+      </v-alert>
+    </div>
 
     <!-- Card view -->
-    <div :class="['opacity-0-transition',{'opacity-1-transition':isColumnList}]">
+    <div
+      v-if="records.length"
+      :class="['opacity-0-transition',{'opacity-1-transition':isColumnList}]"
+    >
       <v-skeleton-loader
         class="mt-5"
         :loading="loading"
         type="image"
       >
-        <v-row v-show="isColumnList">
+        <v-row
+          v-show="isColumnList"
+        >
           <RecordsCardColumn
             v-for="record in records"
             :key="'record_'+record.id"
@@ -54,37 +73,45 @@
 </template>
 
 <script>
-    import RecordsCardStack from "./RecordsCardStack";
-    import ListController from "../Header/ListController";
-    import RecordsCardColumn from "./RecordsCardColumn";
-    import {mapState} from 'vuex'
-    import FilterChips from "../Header/FilterChips";
+import RecordsCardStack from "./RecordsCardStack";
+import ListController from "../Header/ListController";
+import RecordsCardColumn from "./RecordsCardColumn";
+import {mapState} from 'vuex'
+import FilterChips from "../Header/FilterChips";
 
-    export default {
-        name: "SearchOutput",
-        components: {FilterChips, RecordsCardColumn, ListController, RecordsCardStack},
-        data() {
-            return {
-                isColumnList: false
-            }
-        },
-        computed: {
-            ...mapState('records', ["records", "hits", "loading"])
-        },
-        methods: {
-            changeListType: function (listType) {
-                this.isColumnList = listType;
-            },
-        },
+export default {
+  name: "SearchOutput",
+  components: {FilterChips, RecordsCardColumn, ListController, RecordsCardStack},
+  data() {
+    return {
+      isColumnList: false
     }
+  },
+  computed: {
+    ...mapState('records', ["records", "hits", "loading"])
+  },
+  methods: {
+    changeListType: function (listType) {
+      this.isColumnList = listType;
+    },
+  },
+}
 </script>
 <style lang="scss">
-    .chips-holder {
-        background: #f5f5f5;
-        min-height: 50px;
-        border: #dbdbdb dotted 2px;
-        border-radius: 10px;
-        -moz-border-radius: 10px;
-        -webkit-border-radius: 10px;
-    }
+.chips-holder {
+  background: #f5f5f5;
+  min-height: 50px;
+  border: #dbdbdb dotted 2px;
+  border-radius: 10px;
+  -moz-border-radius: 10px;
+  -webkit-border-radius: 10px;
+}
+
+.no-data-found {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+}
 </style>

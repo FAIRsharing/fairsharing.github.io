7ee20f800e2c462500dd1e380a55f6e6121d4b1b
Milo Thurston <knirirr@gmail.com>
2020-07-30 12:37:04 +0100

    Cleaned up debugging strings, comments etc.

--- src/utils/currentParameterMixin.js
+++ src/utils/currentParameterMixin.js
@@ -2,14 +2,12 @@ export default {
     computed: {
         currentParameter(){
             const currentQuery = this.$route.query;
-            //console.log("QUERY: " + JSON.stringify(currentQuery));
             const fieldName = this.item.filterName;
             let currentParam = {};
             currentParam[fieldName] = null;
             if (currentQuery[fieldName]){
                 currentParam[fieldName] = currentQuery[fieldName];
             }
-            //console.log("PARAM: " + JSON.stringify(currentParam));
             return currentParam;
         }
     }

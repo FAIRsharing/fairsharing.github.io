af0d1a68c64edb29ad30185bee4e7dcf6953dca6
hosein.mirian <hosein.mirian@gmail.com>
2020-08-07 12:14:19 +0100

    completed task, making filterChips sticky

---
+++ src/utils/filterChipsUtils.js
@@ -0,0 +1,33 @@
+const filterChipsUtils = {
+    computed: {
+        getChips: function () {
+            let output = [];
+            const parameters = this.$route.query;
+            const ignoredFields = [
+                "page",
+                "orderBy",
+                "searchAnd"
+            ];
+            Object.keys(parameters).forEach(function (paramName) {
+                if (ignoredFields.indexOf(paramName) === -1) {
+                    let param = parameters[paramName];
+                    if (param.indexOf(",") > -1) {
+                        param = param.split(",")
+                    } else {
+                        param = [param];
+                    }
+                    param.forEach(function (val) {
+                        output.push({
+                            paramName: paramName,
+                            paramVal: val
+                        });
+                    })
+
+                }
+            });
+            return output;
+        }
+    },
+}
+
+export default filterChipsUtils;

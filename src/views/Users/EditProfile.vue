cd841a7d5bb936bf3fab41632b199266d5ca9e94
Terazus <batistadominique@hotmail.com>
2020-08-10 04:14:00 -0700

    styled edit profile form

--- src/views/Users/EditProfile.vue
+++ src/views/Users/EditProfile.vue
@@ -1,73 +1,100 @@
 <template>
-  <div class="container-fluid">
-    <v-card class="my-10">
-      <v-card-title>
-        <h2> Edit your profile</h2>
-      </v-card-title>
-
-      <v-card-text
-        v-if="user()"
-        class="body-1"
+  <v-container>
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        sm="12"
+        md="4"
+        lg="4"
+        xl="4"
       >
-        <v-form v-model="valid">
-          <v-container>
-            <!-- message -->
-            <v-row class="mb-5" v-if="messages().updateProfile.message">
-              <v-col cols="12">
-                <div
-                  :class="{'alert-danger': messages().updateProfile.error, 'alert-success': !messages().updateProfile.error}"
-                  class="alert"
-                >
-                  {{ messages().updateProfile.message }}
-                </div>
-              </v-col>
-            </v-row>
+        <v-card>
+          <v-card-title class="primary white--text">
+            <h2> Edit your profile</h2>
+          </v-card-title>
 
-            <!-- FIELDS -->
-            <v-row>
-              <v-col
-                v-for="(field, fieldKey) in fields"
-                :id="'edit_' + field.name"
-                :key="'edit_field_' + fieldKey"
-                cols="12 py-0"
-              >
-                <div
-                  v-if="field.type !== 'checkbox'"
+          <v-card-text
+            v-if="user()"
+            class="pt-3"
+          >
+            <v-form v-model="valid">
+              <v-container>
+                <!-- message -->
+                <v-row
+                  v-if="messages().updateProfile.message"
+                  class="mb-5"
                 >
-                  <v-text-field
-                    v-model="formData[field.name]"
-                    :label="field.label"
-                    outlined
-                    :type="field.type"
-                    :disabled="field.disabled"
-                  />
-                </div>
-                <div v-if="field.type === 'checkbox'">
-                  <v-checkbox
-                    v-model="formData[field.name]"
-                    :label="field.label"
-                  />
-                </div>
-              </v-col>
-            </v-row>
+                  <v-col cols="12">
+                    <v-alert
+                      v-if="messages().updateProfile.error"
+                      type="error"
+                    >
+                      <ul>
+                        <li
+                          v-for="(error, errorName, index) in messages().updateProfile.message "
+                          :key="'error_'+index"
+                        >
+                          {{ errorName }}: {{ error[0] }}
+                        </li>
+                      </ul>
+                    </v-alert>
+                    <v-alert
+                      v-else
+                      type="success"
+                    >
+                      {{ messages().updateProfile.message }}
+                    </v-alert>
+                  </v-col>
+                </v-row>
 
-            <!-- ACTIONS -->
-            <v-row>
-              <v-col cols="12">
-                <v-btn
-                  color="success"
-                  class="mr-4"
-                  @click="updateProfile()"
-                >
-                  Update profile
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </div>
+                <!-- FIELDS -->
+                <v-row>
+                  <v-col
+                    v-for="(field, fieldKey) in fields"
+                    :id="'edit_' + field.name"
+                    :key="'edit_field_' + fieldKey"
+                    cols="12 py-0"
+                  >
+                    <div
+                      v-if="field.type !== 'checkbox'"
+                    >
+                      <v-text-field
+                        v-model="formData[field.name]"
+                        :label="field.label"
+                        outlined
+                        :type="field.type"
+                        :disabled="field.disabled"
+                      />
+                    </div>
+                    <div v-if="field.type === 'checkbox'">
+                      <v-checkbox
+                        v-model="formData[field.name]"
+                        :label="field.label"
+                      />
+                    </div>
+                  </v-col>
+                </v-row>
+
+                <!-- ACTIONS -->
+                <v-row>
+                  <v-col cols="12">
+                    <v-btn
+                      color="success"
+                      class="mr-4"
+                      :loading="loading"
+                      @click="updateProfile()"
+                    >
+                      Update profile
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -123,7 +150,8 @@
                   hint: null,
                   type: "checkbox"
                 },
-              ]
+              ],
+              loading: false
             }
         },
         computed: {
@@ -145,7 +173,9 @@
         methods: {
           ...mapActions('users', ['getUserMeta', "updateUser"]),
           updateProfile: async function(){
+            this.loading = true;
             await this.updateUser(this.formData);
+            this.loading = false;
           }
         },
 

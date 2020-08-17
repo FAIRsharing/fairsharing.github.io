d4eb216fc011ae381e626ec6df76c1095f7cd061
hosein.mirian <hosein.mirian@gmail.com>
2020-08-17 14:02:33 +0100

    added if else to show none found based on condition

--- src/components/Records/Record/GeneralInfo.vue
+++ src/components/Records/Record/GeneralInfo.vue
@@ -38,23 +38,29 @@
         <div class="d-flex flex-column mt-2  ml-sm-6 ml-lg-8">
           <div class="d-flex flex-row mb-2 align-center">
             <h3>{{ currentRecord['fairsharingRecord'].name }}</h3>
-            <b class="ml-2">({{ currentRecord['fairsharingRecord'].abbreviation }})</b>
-            <NoneFound :string-field="currentRecord['fairsharingRecord'].abbreviation" />
-            <span
-              v-if="!currentRecord['fairsharingRecord'].abbreviation"
+            <b
+              v-if="currentRecord['fairsharingRecord'].abbreviation"
               class="ml-2"
-            >None.</span>
+            >({{ currentRecord['fairsharingRecord'].abbreviation }})</b>
+            <none-found
+              v-else
+              :string-field="currentRecord['fairsharingRecord'].abbreviation"
+            />
           </div>
           <div class="d-flex align-center">
             <h3 class="mr-1">
               doi:
             </h3>
             <a
+              v-if="currentRecord['fairsharingRecord'].doi"
               :href="currentRecord['fairsharingRecord'].doi"
             >
               {{ currentRecord['fairsharingRecord'].doi }}
             </a>
-            <NoneFound :string-field="currentRecord['fairsharingRecord'].doi" />
+            <NoneFound
+              v-else
+              :string-field="currentRecord['fairsharingRecord'].doi"
+            />
           </div>
         </div>
       </v-col>
@@ -73,8 +79,13 @@
       <!--fairsharingRecord.year_creation-->
       <div class="d-flex">
         <b class="mr-2">Year of Creation:</b>
-        <p>{{ currentRecord['fairsharingRecord'].metadata.year_creation }}</p>
-        <NoneFound :string-field="String(currentRecord['fairsharingRecord'].metadata.year_creation)" />
+        <p v-if="currentRecord['fairsharingRecord'].metadata.year_creation">
+          {{ currentRecord['fairsharingRecord'].metadata.year_creation }}
+        </p>
+        <NoneFound
+          v-else
+          :string-field="currentRecord['fairsharingRecord'].metadata.year_creation"
+        />
       </div>
       <!--Registry-->
       <div class="d-flex">
@@ -90,15 +101,22 @@
       <div class="d-flex">
         <b class="mr-2 mb-4">Home Page:</b>
         <a
+          v-if="currentRecord['fairsharingRecord'].homepage"
           :href="currentRecord['fairsharingRecord'].homepage"
           target="_blank"
         >{{ currentRecord['fairsharingRecord'].homepage }}</a>
-        <NoneFound :string-field="currentRecord['fairsharingRecord'].homepage" />
+        <NoneFound
+          v-else
+          :string-field="currentRecord['fairsharingRecord'].homepage"
+        />
       </div>
       <!--Developed Countries -->
       <div class="d-flex flex-wrap">
         <b class="mr-2">Countries involved with this resource:</b>
-        <NoneFound :string-field="String(currentRecord['fairsharingRecord'].countries)" />
+        <NoneFound
+          v-if="!currentRecord['fairsharingRecord'].countries || currentRecord['fairsharingRecord'].countries[0].name==='All'"
+          :data-field="currentRecord['fairsharingRecord'].countries"
+        />
         <v-tooltip
           v-for="country in currentRecord['fairsharingRecord'].countries"
           :key="country.id"
@@ -123,46 +141,46 @@
 </template>
 
 <script>
-    import CountryFlag from 'vue-country-flag';
-    import {mapState} from 'vuex';
-    // TODO:
-    //import { mapState, mapGetters } from 'vuex';
+import CountryFlag from 'vue-country-flag';
+import {mapState} from 'vuex';
+// TODO:
+//import { mapState, mapGetters } from 'vuex';
 
-    import Ribbon from "@/components/Records/Shared/Ribbon";
-    import SectionTitle from '@/components/Records/Record/SectionTitle';
-    import RecordStatus from "@/components/Records/Shared/RecordStatus";
+import Ribbon from "@/components/Records/Shared/Ribbon";
+import SectionTitle from '@/components/Records/Record/SectionTitle';
+import RecordStatus from "@/components/Records/Shared/RecordStatus";
 
-    import stringUtils from '@/utils/stringUtils';
-    import NoneFound from "@/components/Records/Record/NoneFound";
+import stringUtils from '@/utils/stringUtils';
+import NoneFound from "@/components/Records/Record/NoneFound";
 
 
-    export default {
-        name: "GeneralInfo",
-        components: {
-          NoneFound,
-            CountryFlag,
-            RecordStatus,
-            Ribbon,
-            SectionTitle
-        },
-        mixins: [stringUtils],
-        computed: {
-            ...mapState("record", ["currentRecord"])
-        }
-    }
+export default {
+  name: "GeneralInfo",
+  components: {
+    NoneFound,
+    CountryFlag,
+    RecordStatus,
+    Ribbon,
+    SectionTitle
+  },
+  mixins: [stringUtils],
+  computed: {
+    ...mapState("record", ["currentRecord"])
+  }
+}
 </script>
 
 <style scoped lang="scss">
-    a {
-        text-decoration: none;
+a {
+  text-decoration: none;
 
-        &:hover, &:focus {
-            text-decoration: underline;
-            outline: 0;
-        }
-    }
+  &:hover, &:focus {
+    text-decoration: underline;
+    outline: 0;
+  }
+}
 
-    .flag-mr {
-        margin-right: .29em;
-    }
+.flag-mr {
+  margin-right: .29em;
+}
 </style>

d8399b89283e862dbd2788fbdc3d6017eccf2a99
Milo Thurston <knirirr@gmail.com>
2020-09-07 15:28:29 +0100

    Initial implementation for #405.

---
+++ src/components/Records/Search/Header/HitCount.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="text-align: center;">
+    <p>Displaying {{x}}  to {{y}} of {{ hits }}.</p>
+  </div>
+</template>
+
+<script>
+    import {mapState} from "vuex";
+    export default {
+        name: "HitCount",
+        computed: {
+          ...mapState("records", ["hits", "perPage", "currentPage"]),
+          x: function () {
+            let _module = this;
+            return _module.currentPage * _module.perPage;
+          },
+          y: function () {
+            let _module = this;
+            return ((_module.currentPage + 1) * _module.perPage) -1;
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

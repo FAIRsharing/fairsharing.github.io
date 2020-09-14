0e58b4be4a98251f2dd64e7039ea00299e09bb61
Terazus <batistadominique@hotmail.com>
2020-09-14 06:33:29 -0700

    added color file

--- src/components/Records/Record/Keywords.vue
+++ src/components/Records/Record/Keywords.vue
@@ -6,8 +6,8 @@
     elevation="1"
   >
     <SectionTitle title="keywords" />
+    <!--
     <section>
-      <!--Taxonomies-->
       <div class="d-flex mt-4 flex-wrap">
         <b class="mr-2">Taxonomies:</b>
         <span
@@ -19,10 +19,10 @@
           v-for="item in getField('taxonomies')"
           :key="item.label"
           class="mr-2 mb-2 "
-          color="primary"
+          :color="colors['taxonomies']"
+          :text-color="colors['taxonomies']"
           label
           outlined
-          text-color="primary"
         >
           <v-icon left>
             mdi-label
@@ -30,7 +30,6 @@
           {{ item.label }}
         </v-chip>
       </div>
-      <!--Domains-->
       <div
         class="d-flex mt-2 flex-wrap"
       >
@@ -44,10 +43,10 @@
           v-for="item in getField('domains')"
           :key="item.label"
           class="mr-2 mb-2"
-          color="secondary"
+          :color="colors['domains']"
+          :text-color="colors['domains']"
           label
           outlined
-          text-color="secondary"
         >
           <v-icon left>
             mdi-label
@@ -55,7 +54,6 @@
           {{ item.label }}
         </v-chip>
       </div>
-      <!--Subjects-->
       <div
         class="d-flex mt-2 flex-wrap"
       >
@@ -69,10 +67,10 @@
           v-for="item in getField('subjects')"
           :key="item.label"
           class="mr-2 mb-2"
-          color="accent"
+          :color="colors['subjects']"
+          :text-color="colors['subjects']"
           label
           outlined
-          text-color="accent"
         >
           <v-icon left>
             mdi-label
@@ -80,7 +78,6 @@
           {{ item.label }}
         </v-chip>
       </div>
-      <!--UserDefinedTags-->
       <div
         class="d-flex mt-2 flex-wrap"
       >
@@ -94,10 +91,10 @@
           v-for="item in getField('userDefinedTags')"
           :key="item.label"
           class="mr-2 mb-2"
-          color="orange"
+          :color="colors['userDefinedTags']"
+          :text-color="colors['userDefinedTags']"
           label
           outlined
-          text-color="orange"
         >
           <v-icon left>
             mdi-label
@@ -106,12 +103,51 @@
         </v-chip>
       </div>
     </section>
+    -->
+    <v-container fluid>
+      <v-row
+        v-for="(section, sectionIndex) in sections"
+        :key="'keywordsSection_' + sectionIndex"
+        class="d-flex flex-wrap"
+      >
+        <v-col
+          cols="12"
+          class="keywordLabel px-0 mx-0"
+        >
+          <b>{{ labels[section].filterLabel }}: </b>
+          <span v-if="!getField(labels[section].filterName).length"> None. </span>
+        </v-col>
+
+        <v-chip
+          v-for="item in getField(labels[section].filterName)"
+          :key="item.label"
+          class="mr-2 mt-1"
+          :color="colors[labels[section].filterName]"
+          :text-color="colors[labels[section].filterName]"
+          label
+          outlined
+        >
+          <v-icon left>
+            mdi-label
+          </v-icon>
+          {{ item.label }}
+        </v-chip>
+        <v-col
+          v-if="section !== 'user_defined_tags'"
+          cols="12"
+        >
+          <v-divider class="mb-0 mt-4 py-0" />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
 <script>
     // TODO: Also needs user defined tags
     import { mapGetters } from 'vuex';
+    import colorsObject from "@/data/colorOptions.json"
+    import labelMapping from "@/components/Records/FiltersLabelMapping.json"
 
     import SectionTitle from '@/components/Records/Record/SectionTitle';
 
@@ -119,6 +155,18 @@
         name: "Keywords",
         components: {
             SectionTitle
+        },
+        data(){
+          return {
+            colors: colorsObject.colors,
+            labels: labelMapping.autocomplete,
+            sections: [
+              "taxonomies",
+              "domains",
+              "subjects",
+              "user_defined_tags"
+            ]
+          }
         },
         computed: {
             ...mapGetters("record", ["getField"])

4035d895edf0f3ccb2daf01b728e6d3a7286b2de
Terazus <batistadominique@hotmail.com>
2020-09-18 05:07:47 -0700

    fixed unit tests

--- src/views/Errors/404.vue
+++ src/views/Errors/404.vue
@@ -3,7 +3,9 @@
     <h1 class="text-center mt-5 blue--text">
       Error 404: <br>
     </h1>
-    <h3 class="text-center mb-5 blue--text"> {{ getSource }} couldn't be found ! </h3>
+    <h3 class="text-center mb-5 blue--text">
+      {{ getSource }} couldn't be found !
+    </h3>
   </div>
 </template>
 
@@ -12,7 +14,7 @@
         name: "Error404",
         computed: {
             getSource(){
-              return JSON.parse(this.$route.query.source)
+              return (this.$route.query.source) ? JSON.parse(this.$route.query.source) : '' + "Your page ";
             }
         }
     }

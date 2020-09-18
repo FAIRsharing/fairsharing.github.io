618bc31121890671a99587101b4d2c247b54a073
Terazus <batistadominique@hotmail.com>
2020-09-18 04:35:35 -0700

    made routerview at least 70vh

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,10 @@
       left
       width="70%"
     />
-
     <transition name="fade">
       <Header v-if="UIGeneralStatus.headerVisibilityState" />
     </transition>
-    <router-view />
+    <router-view style="min-height: 70vh" />
     <Footer />
   </v-app>
 </template>
@@ -42,7 +41,7 @@
         },
         methods: {
             toggleOverFlow: function (status) {
-                this._status = status
+                this._status = status;
                 this.root = document.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
                 this._status ? this.root.setAttribute('class', this.hideOverflow) : this.root.removeAttribute('class');
             },
@@ -59,9 +58,6 @@
 
     html, body {
         height: 100%;
-    }
-
-    .footer-content {
     }
 
     .overflow-hidden {

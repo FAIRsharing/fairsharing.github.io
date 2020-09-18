72e22c5d33c38ff682c3cc446589c3dcff5bbdb5
Terazus <batistadominique@hotmail.com>
2020-09-18 04:35:56 -0700

    added 404 page + redirection

--- src/views/Errors/404.vue
+++ src/views/Errors/404.vue
@@ -1,10 +1,19 @@
 <template>
-    $END$
+  <div>
+    <h1 class="text-center my-5 blue--text">
+      404: {{ getSource }} couldn't be found !
+    </h1>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "404"
+        name: "Error404",
+        computed: {
+            getSource(){
+              return this.$route.query.source
+            }
+        }
     }
 </script>
 

3c4f509aff17a205beeef45c1c8c2b431c1d20be
Terazus <batistadominique@hotmail.com>
2020-09-23 04:51:16 -0700

    fixed metainfo() test

--- tests/unit/views/Errors/404.spec.js
+++ tests/unit/views/Errors/404.spec.js
@@ -1,7 +1,9 @@
 import { createLocalVue, shallowMount } from "@vue/test-utils";
+import VueMeta from "vue-meta";
 import Error from "@/views/Errors/404.vue";
 
 const localVue = createLocalVue();
+localVue.use(VueMeta);
 
 let $route = {
     path: "/error/404",
@@ -22,8 +24,12 @@ describe("404 error page", () => {
         expect(wrapper.vm.getSource).toBe('http://localhost:8080/#/random/test')
     });
 
+    it("has it meta title dynamically set", () => {
+        expect(wrapper.vm.$meta().refresh().metaInfo.title).toBe("FAIRsharing | Not Found");
+    });
+
     it("can process no source", () => {
-       $route.query = {};
+        $route.query = {};
         wrapper = shallowMount(Error, {
             mocks: {$route},
             localVue

0fd7e1f25e0c01598e2e7b8cd36aad9cbae0eb34
Terazus <batistadominique@hotmail.com>
2020-11-10 04:00:16 -0800

    fixed codacy errors

--- tests/unit/components/Clients/GraphClient.spec.js
+++ tests/unit/components/Clients/GraphClient.spec.js
@@ -101,7 +101,7 @@ describe("GraphQL Client", function () {
 
     it("can correctly build a query string from a JSON", function () {
         const expectedOutput =
-            'searchFairsharingRecords(field1:true field2:"true" field3:["true","false"]){ aggregations currentPage perPage totalCount totalPages firstPage records{id type name abbreviation doi registry description domains{ label id definitions iri synonyms inFairsharing}subjects{ label id definitions iri synonyms}taxonomies{ label}userDefinedTags{ label}recordAssociations{ linkedRecord{name id registry } recordAssocLabel}reverseRecordAssociations{ fairsharingRecord{name id registry } recordAssocLabel}status isRecommended }}';;
+            'searchFairsharingRecords(field1:true field2:"true" field3:["true","false"]){ aggregations currentPage perPage totalCount totalPages firstPage records{id type name abbreviation doi registry description domains{ label id definitions iri synonyms inFairsharing}subjects{ label id definitions iri synonyms}taxonomies{ label}userDefinedTags{ label}recordAssociations{ linkedRecord{name id registry } recordAssocLabel}reverseRecordAssociations{ fairsharingRecord{name id registry } recordAssocLabel}status isRecommended }}';
         query.queryParam = {
             "field1": true,
             "field2": "true",

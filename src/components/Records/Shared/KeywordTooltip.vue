cff8f55e0ef10f1868f306b81b22f43ab378fa5f
Terazus <batistadominique@hotmail.com>
2020-11-10 03:54:57 -0800

    processing empty synonyms

--- src/components/Records/Shared/KeywordTooltip.vue
+++ src/components/Records/Shared/KeywordTooltip.vue
@@ -13,7 +13,12 @@
         <div><b class="mr-1">Name:</b> {{ keyword.label }}</div>
         <div><b class="mr-1">URL:</b> {{ keyword['iri'] }}</div>
         <div><b class="mr-1">Definition:</b> <i> {{ keyword.definitions[0] }} </i></div>
-        <div><b class="mr-1">Synonyms:</b> {{ processArray(keyword['synonyms']) }}</div>
+        <div>
+          <b
+            v-if="processArray(keyword['synonyms'])"
+            class="mr-1"
+          >Synonyms:</b> {{ processArray(keyword['synonyms']) }}
+        </div>
       </div>
     </v-tooltip>
   </div>
@@ -32,7 +37,8 @@
         },
         methods: {
           processArray(array){
-          return JSON.stringify(array)
+            if (array.length === 0) return null;
+            return JSON.stringify(array)
                   .replace(/,/g, ", ")
                   .replace(/"/g, "")
                   .replace("[", "")

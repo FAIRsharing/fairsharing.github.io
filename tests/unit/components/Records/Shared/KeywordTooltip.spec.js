a67c7d855cbca9dc4028e8541616673cac2f3220
Terazus <batistadominique@hotmail.com>
2020-11-10 03:54:57 -0800

    processing empty synonyms

--- tests/unit/components/Records/Shared/KeywordTooltip.spec.js
+++ tests/unit/components/Records/Shared/KeywordTooltip.spec.js
@@ -3,19 +3,22 @@ import KeywordTooltip from "@/components/Records/Shared/KeywordTooltip.vue"
 
 describe('KeywordTooltip.vue', () => {
     let wrapper;
-    wrapper = shallowMount(KeywordTooltip, {
-        propsData: {
-            keyword: {
-                label: 'ABC',
-                synonyms: ["abc", "def:test"],
-                definitions: ["first def"],
-                expandedNames: []
-            }
-        }
-    });
+    let keyword = {
+            label: 'ABC',
+            synonyms: ["abc", "def:test"],
+            definitions: ["first def"],
+            expandedNames: []
+    };
 
     it("can be mounted", () => {
+        wrapper = shallowMount(KeywordTooltip, { propsData: { keyword: keyword } });
         expect(wrapper.name()).toMatch("KeywordTooltip");
         expect(wrapper.vm.processArray(wrapper.vm.keyword.synonyms)).toBe("abc, def: test.")
     });
+
+    it ("can process empty synonyms", () => {
+        keyword.synonyms = [];
+        wrapper = shallowMount(KeywordTooltip, { propsData: { keyword: keyword  } });
+        expect(wrapper.vm.processArray(wrapper.vm.keyword.synonyms)).toBe(null);
+    })
 });

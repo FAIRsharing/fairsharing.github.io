ecd9fb2755557e9f7948440f9248bf6606517fa8
Terazus <batistadominique@hotmail.com>
2020-11-23 05:18:33 -0800

    can now update, create and delete orgaLinks

--- src/components/Editor/EditOrganisations.vue
+++ src/components/Editor/EditOrganisations.vue
@@ -1,6 +1,10 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <v-card id="editOrganisations">
-    <v-form ref="editOrganisations">
+    <!-- EDITING EXISTING RELATIONSHIPS -->
+    <v-form
+      ref="editOrganisations"
+      v-model="saving.validity"
+    >
       <v-card-title class="grey lighten-4 blue--text">
         <v-btn
           class="blue mr-4"
@@ -18,6 +22,20 @@
       </v-card-title>
       <v-card-text>
         <v-container fluid>
+          <v-row v-if="saving.errors">
+            <v-col cols-12>
+              <v-alert type="error">
+                {{ saving.errors.response.data }}
+              </v-alert>
+            </v-col>
+          </v-row>
+          <v-row v-if="saving.success">
+            <v-col cols-12>
+              <v-alert type="success">
+                Your record was successfully updated!
+              </v-alert>
+            </v-col>
+          </v-row>
           <v-row>
             <v-col
               v-for="(orga, orgaIndex) in currentOrganisations"
@@ -44,6 +62,7 @@
                         return-object
                         label="Select an organisation"
                         :search-input.sync="searches.organisations[orgaIndex]"
+                        :rules="[rules.isRequired()]"
                       >
                         <template slot="no-data">
                           <v-container>
@@ -71,7 +90,7 @@
                           <v-list-item-avatar>
                             <img
                               v-if="data.item['urlForLogo']"
-                              :src="data.item['urlForLogo']"
+                              :src="'https://api.fairsharing.org' + data.item['urlForLogo']"
                             >
                             <v-icon v-else>
                               fas fa-sitemap
@@ -97,11 +116,20 @@
                         :items="relations"
                         outlined
                         label="Select a type of relationship"
+                        :rules="[rules.isRequired()]"
                       >
                         <template v-slot:selection="data">
                           <v-chip class="blue white--text px-3 py-1">
                             {{ data.item }}
                           </v-chip>
+                        </template>
+                        <template v-slot:item="data">
+                          <v-list-item-content
+                            style="width:100%;height:100%"
+                            @click="cleanGrant(orgaIndex, data.item)"
+                          >
+                            {{ data.item }}
+                          </v-list-item-content>
                         </template>
                       </v-autocomplete>
                     </div>
@@ -116,6 +144,7 @@
                         outlined
                         return-object
                         label="Select an optional grant for funding organisations"
+                        :disabled="orga.relation !== 'funds'"
                       >
                         <template slot="no-data">
                           <v-container>
@@ -205,6 +234,8 @@
       <v-card-actions class="pb-5">
         <v-btn
           class="primary ml-4"
+          :loading="loading.saving"
+          :disabled="!saving.validity"
           @click="saveRelations"
         >
           Save Organisations and Grants
@@ -244,71 +275,89 @@
                 ref="newOrganisation"
                 v-model="menus.validity.newOrganisation"
               >
-                <v-text-field
-                  v-model="menus.data.name"
-                  label="Organisation Name"
-                  outlined
-                  :rules="[rules.isRequired()]"
-                />
-                <v-text-field
-                  v-model="menus.data.homepage"
-                  label="Organisation Homepage"
-                  outlined
-                  :rules="[rules.isRequired(), rules.isURL()]"
-                />
-                <v-autocomplete
-                  v-model="menus.data.organisation_type_ids"
-                  :items="organisationTypes"
-                  multiple
-                  outlined
-                  item-text="name"
-                  item-value="id"
-                  label="Select the organisation type(s)"
-                  :rules="[rules.isRequired()]"
-                />
-                <v-text-field
-                  v-model="menus.data.alternativeNames"
-                  label="Alternative names, comma separated"
-                  outlined
-                />
-                <v-file-input
-                  v-model="menus.data.logo"
-                  accept="image/png, image/jpeg"
-                  :rules="[rules.isImage()]"
-                  color="primary"
-                  label="File input"
-                  placeholder="Select a logo"
-                  outlined
-                  :show-size="1000"
-                  clearable
-                  chips
-                  disabled
-                />
-                <div class="orgaPreview" v-if="menus.validity.newOrganisation">
-                  <h6>Preview your new organisation</h6>
-                  <v-list-item
-                    three-line
-                    style="border: 1px dashed #ccc;"
-                  >
-                    <v-list-item-avatar>
-                      <img
-                        v-if="logoData"
-                        :src="logoData"
-                        alt="organisation logo"
-                      >
-                      <v-icon v-else>
-                        fas fa-sitemap
-                      </v-icon>
-                    </v-list-item-avatar>
-                    <v-list-item-content class="mb-2">
-                      <v-list-item-title> {{ menus.data.name }} </v-list-item-title>
-                      <v-list-item-subtitle> {{ menus.data.homepage }} </v-list-item-subtitle>
-                      <v-list-item-subtitle v-if="menus.data.organisation_type_ids">
-                        Types: {{ menus.data.organisation_type_ids.join(", ") }}
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </div>
+                <v-container fluid>
+                  <v-row>
+                    <v-col cols="6">
+                      <v-text-field
+                        v-model="menus.data.name"
+                        label="Organisation Name"
+                        outlined
+                        :rules="[rules.isRequired()]"
+                      />
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field
+                        v-model="menus.data.homepage"
+                        label="Organisation Homepage"
+                        outlined
+                        :rules="[rules.isRequired(), rules.isURL()]"
+                      />
+                    </v-col>
+                    <v-col cols="12">
+                      <v-autocomplete
+                        v-model="menus.data.organisation_type_ids"
+                        :items="organisationTypes"
+                        multiple
+                        outlined
+                        item-text="name"
+                        item-value="id"
+                        return-object
+                        label="Select the organisation type(s)"
+                        :rules="[rules.isRequired()]"
+                      />
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="menus.data.alternativeNames"
+                        label="Alternative names, comma separated"
+                        outlined
+                      />
+                    </v-col>
+                    <v-col cols="12">
+                      <v-file-input
+                        v-model="menus.data.logo"
+                        accept="image/png, image/jpeg"
+                        :rules="[rules.isImage()]"
+                        color="primary"
+                        label="File input"
+                        placeholder="Select a logo"
+                        outlined
+                        :show-size="1000"
+                        clearable
+                        chips
+                      />
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="menus.validity.newOrganisation">
+                    <v-col cols="12">
+                      <div class="orgaPreview">
+                        <h6>Preview your new organisation</h6>
+                        <v-list-item
+                          three-line
+                          style="border: 1px dashed #ccc;"
+                        >
+                          <v-list-item-avatar>
+                            <img
+                              v-if="logoData.data"
+                              :src="logoData.data"
+                              alt="organisation logo"
+                            >
+                            <v-icon v-else>
+                              fas fa-sitemap
+                            </v-icon>
+                          </v-list-item-avatar>
+                          <v-list-item-content class="mb-2">
+                            <v-list-item-title> {{ menus.data.name }} </v-list-item-title>
+                            <v-list-item-subtitle> {{ menus.data.homepage }} </v-list-item-subtitle>
+                            <v-list-item-subtitle v-if="menus.data.organisation_type_ids">
+                              Types: {{ menus.data.organisation_type_ids.map(obj => obj.name).join(", ") }}
+                            </v-list-item-subtitle>
+                          </v-list-item-content>
+                        </v-list-item>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-container>
               </v-form>
             </v-card-text>
             <v-card-actions>
@@ -331,6 +380,11 @@
         </v-row>
       </v-container>
     </v-dialog>
+
+    <!-- CREATE A NEW GRANT -->
+    <v-dialog
+      v-model="menus.visibility.newGrant"
+    />
   </v-card>
 </template>
 
@@ -363,7 +417,11 @@
             "undefined"
           ],
           grants: [],
-          logoData: null,
+          logoData: {
+            filename: null,
+            data: null,
+            content_type: null
+          },
           loading: {
             organisations: false,
             grants: false,
@@ -394,9 +452,14 @@
             target: null
           },
           rules: {
-            isRequired: function(){return isRequired()},
-            isURL: function(){ return isUrl()},
-            isImage: function(){ return isImage()}
+            isRequired: function(){return isRequired() },
+            isURL: function(){ return isUrl() },
+            isImage: function(){ return isImage() }
+          },
+          saving: {
+            errors: null,
+            validity: true,
+            success: false
           }
         }
       },
@@ -406,21 +469,33 @@
       },
       watch: {
         'menus.data.logo': function(){
-          let reader = new FileReader();
-          reader.addEventListener('load', async (event) => {
-              this.logoData = event.target['result'];
-          });
-          reader.readAsDataURL(this.menus.data.logo);
+          if (this.menus.data.logo) {
+            let reader = new FileReader();
+            reader.addEventListener('load', async (event) => {
+              this.logoData = {
+                filename: this.menus.data.logo.name,
+                data: event.target['result'],
+                content_type: this.menus.data.logo.type
+              };
+            });
+            reader.readAsDataURL(this.menus.data.logo);
+          }
+        },
+        'currentRecord.fairsharingRecord': async function(){
+          this.loading.organisations = true;
+          await this.getData();
+          this.loading.organisations = false;
         }
       },
       mounted(){
         this.$nextTick(async function () {
+          this.loading.organisations = true;
           await this.getData();
+          await this.getExistingOrganisations();
         });
       },
       methods: {
         async getData(){
-          this.loading.organisations = true;
           this.loading.grants = true;
           this.currentOrganisations = JSON.parse(JSON.stringify(this.currentRecord['fairsharingRecord']['organisationLinks']));
           this.initialOrganisations = JSON.parse(JSON.stringify(this.currentRecord['fairsharingRecord']['organisationLinks']));
@@ -429,8 +504,11 @@
           data = await client.executeQuery(getGrantsQuery);
           this.grants = data["searchGrants"];
           this.loading.grants = false;
-          getOrganisationsQuery.queryParam = {load: false};
-          data = await client.executeQuery(getOrganisationsQuery);
+        },
+        async getExistingOrganisations(){
+          this.loading.organisations = true;
+          getOrganisationsQuery.queryParam = {load: true};
+          let data = await client.executeQuery(getOrganisationsQuery);
           this.existingOrganisations = data["searchOrganisations"];
           this.loading.organisations = false;
         },
@@ -446,6 +524,11 @@
         },
         openOrganisationMenu(relationIndex, searchTerm){
           this.menus.visibility.newOrganisation = true;
+          this.logoData = {
+            filename: null,
+            data: null,
+            content_type: null
+          };
           this.menus.data = {
             name: searchTerm,
             homepage: null,
@@ -459,28 +542,18 @@
           this.menus.loading.newOrganisation = true;
           this.menus.errors.newOrganisation = false;
           let organisationInput = JSON.parse(JSON.stringify(this.menus.data));
-          let logo = this.menus.data.logo;
-          if (logo) {
-            let reader = new FileReader();
-            reader.addEventListener('load', async (event) => {
-              organisationInput.logo = {
-                filename: logo.name,
-                data: event.target['result'],
-                content_type: logo.type
-              };
-              await this.saveNewOrganisation(organisationInput);
-            });
-            reader.readAsDataURL(logo);
+          if (this.logoData.data){
+            organisationInput.logo = JSON.parse(JSON.stringify(this.logoData));
+            organisationInput.logo.data = organisationInput.logo.data.replace("data:image/png;base64,", "")
           }
-          else {
-            this.saveNewOrganisation(organisationInput);
-          }
+          this.saveNewOrganisation(organisationInput);
         },
         async saveNewOrganisation(organisation){
-          organisation.alternativeNames = this.menus.data.alternativeNames ? this.menus.data.alternativeNames.split(",") : null;
-          if (!organisation.alternativeNames) delete organisation.alternativeNames;
-          let data = await restClient.createOrganisation(organisation, this.user().credentials.token);
-          console.log(data);
+          let queryInput = JSON.parse(JSON.stringify(organisation));
+          queryInput.alternativeNames = this.menus.data.alternativeNames ? this.menus.data.alternativeNames.split(",") : null;
+          if (!queryInput.alternativeNames) delete queryInput.alternativeNames;
+          queryInput.organisation_type_ids = queryInput.organisation_type_ids.map(obj => obj.id);
+          let data = await restClient.createOrganisation(queryInput, this.user().credentials.token);
           if (data.error) {
             this.menus.errors.newOrganisation = data.error;
           }
@@ -489,8 +562,9 @@
               id: data.id,
               name: data.name,
               homepage: data.homepage,
-              types: organisation.organisation_type_ids,
-              alternativeNames: data['alternative_names']
+              types: organisation.organisation_type_ids.map(obj => obj.name),
+              alternativeNames: data['alternative_names'],
+              urlForLogo: data['url_for_logo']
             };
             this.currentOrganisations[this.menus.target].organisation = newOrganisation;
             Vue.set(this.existingOrganisations, this.existingOrganisations.length, newOrganisation);
@@ -501,18 +575,60 @@
         async createNewGrant(){},
         async saveNewGrant(){},
         async saveRelations(){
-          // let organisationsRelations = [];
-          this.currentOrganisations.forEach(relation => {
-            if (relation.id){
-              let oldRelation = this.initialOrganisations.filter(obj => obj.id = relation.id)[0];
-              if (!isEqual(oldRelation, relation)){
-                console.log("RELATION NEEDS CHANGE " + relation.id)
+          let _module = this;
+          this.saving.success = false;
+          _module.loading.saving = true;
+          _module.saving.errors = false;
+          _module.currentOrganisations.forEach(async relation => {
+            let query = {
+              fairsharing_record_id: _module.currentRecord['fairsharingRecord'].id,
+              organisation_id: relation.organisation.id,
+              relation: relation.relation,
+              grant_id: (relation.grant) ? relation.grant.id : null
+            };
+            let found = false;
+            this.initialOrganisations.forEach(async initialRelations => {
+              if (initialRelations.id === relation.id){
+                found = true;
+                if(!isEqual(relation, initialRelations)){
+                  let data = await restClient.updateOrganisationLink(query, relation.id, _module.user().credentials.token);
+                  if (data.error){
+                    _module.saving.errors = data.error;
+                  }
+                }
+              }
+            });
+            if (!found){
+              let data = await restClient.createOrganisationLink(query, _module.user().credentials.token);
+              if (data.error){
+                _module.saving.errors = data.error;
               }
             }
-            else {
-              console.log("RELATION NEEDS CREATION")
+          });
+          _module.initialOrganisations.forEach(async initialRelations => {
+            let found = false;
+            _module.currentOrganisations.forEach(relation => {
+              if (initialRelations.id === relation.id) {
+                found = true;
+              }
+            });
+            if (!found){
+              let data = await restClient.deleteOrganisationLink(initialRelations.id, this.user().credentials.token);
+              if (data.error){
+                _module.saving.errors = data.error;
+              }
             }
           });
+          if (!this.saving.errors){
+            this.saving.success = true;
+          }
+          _module.loading.saving = false;
+          window.scrollTo(0, 0);
+        },
+        cleanGrant(organisationIndex, relation){
+          if (relation !== 'funds'){
+            this.currentOrganisations[organisationIndex].grant = null;
+          }
         }
       }
     }

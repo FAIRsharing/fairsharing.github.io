9afb9d45ad9beaf1511aec7da0d3a9d56ed082d3
Dominique <batistadominique@hotmail.com>
2020-11-25 07:41:30 -0800

    Update rules.js
    remove typoe

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -10,7 +10,7 @@ export function hasValue(val){
  /**
  * Assess email patterns.
  * @returns {function(*): (boolean|string)}
- */7
+ */
 export function isEmail(){
     return value => {
         const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/

f6e317a6802b8533f237708f26f7cff1c440b61a
ramGranell <ragrator@gmail.com>
2020-11-30 13:04:46 +0000

    adding changes by the reviewers of the PR

--- src/views/Curators/Curator.vue
+++ src/views/Curators/Curator.vue
@@ -29,7 +29,7 @@
               Records awaiting approval
               <v-spacer />
               <v-text-field
-                v-model="search1"
+                v-model="searches.recordsAwaitingApproval"
                 label="Search"
                 color="white--text"
                 single-line
@@ -40,10 +40,10 @@
               :loading="loading"
               :headers="headers.approvalRequired"
               :items="approvalRequired"
-              :search="search1"
+              :search="searches.recordsAwaitingApproval"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
-              :sort-by="curator"
+              :sort-by="approvalRequired.curator"
               :sort-desc="false"
             >
               <template
@@ -102,7 +102,7 @@
               Pending Maintenance Requests
               <v-spacer />
               <v-text-field
-                v-model="search2"
+                v-model="searches.pendingMaintenanceRequests"
                 label="Search"
                 color="white--text"
                 single-line
@@ -113,7 +113,7 @@
               :loading="loading"
               :headers="headers.maintenanceRequests"
               :items="maintenanceRequests"
-              :search="search2"
+              :search="searches.pendingMaintenanceRequests"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
             />
@@ -128,7 +128,7 @@
               Records in curation
               <v-spacer />
               <v-text-field
-                v-model="search5"
+                v-model="searches.recordsInCuration"
                 label="Search"
                 color="white--text"
                 single-line
@@ -139,10 +139,10 @@
               :loading="loading"
               :headers="headers.recordsInCuration"
               :items="recordsInCuration"
-              :search="search5"
+              :search="searches.recordsInCuration"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
-              :sort-by="curator"
+              :sort-by="recordsInCuration.curator"
               :sort-desc="true"
             />
           </v-card-text>
@@ -156,7 +156,7 @@
               Records without dois
               <v-spacer />
               <v-text-field
-                v-model="search6"
+                v-model="searches.recordsWithoutDois"
                 label="Search"
                 color="white--text"
                 single-line
@@ -167,7 +167,7 @@
               :loading="loading"
               :headers="headers.recordsWithoutDois"
               :items="recordsWithoutDois"
-              :search="search6"
+              :search="searches.recordsWithoutDois"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
             />
@@ -182,7 +182,7 @@
               Hidden records
               <v-spacer />
               <v-text-field
-                v-model="search7"
+                v-model="searches.hiddenRecords"
                 label="Search"
                 color="white--text"
                 single-line
@@ -193,7 +193,7 @@
               :loading="loading"
               :headers="headers.hiddenRecords"
               :items="hiddenRecords"
-              :search="search7"
+              :search="searches.hiddenRecords"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
             />
@@ -208,7 +208,7 @@
               Records created by curators in the past week
               <v-spacer />
               <v-text-field
-                v-model="search3"
+                v-model="searches.recentCuratorCreations"
                 label="Search"
                 color="white--text"
                 single-line
@@ -219,7 +219,7 @@
               :loading="loading"
               :headers="headers.recordsCreatedCuratorsLastWeek"
               :items="recordsCreatedCuratorsLastWeek"
-              :search="search"
+              :search="searches.recentCuratorCreations"
               class="elevation-1"
               :footer-props="{'items-per-page-options': [10, 20, 30, 40, 50]}"
             />
@@ -364,15 +364,13 @@
             ]
           },
           searches: {
-        recordsAwaitingApproval: "",
-        pendingMaintenanceRequests: ""
-        ...
-    },,
-          search2: "",
-          search3: "",
-          search5: "",
-          search6: "",
-          search7: "",
+            recordsAwaitingApproval: "",
+            pendingMaintenanceRequests: "",
+            recentCuratorCreations: "",
+            recordsInCuration: "",
+            recordsWithoutDois: "",
+            hiddenRecords: ""
+          },
           loading: false
         }
       },
@@ -394,7 +392,7 @@
           }
           client.setHeader(this.user().credentials.token);
           let data = await client.executeQuery(getCurationRecords);
-          this.allDataCuration = data["curationSummary"];
+          this.allDataCuration = data.curationSummary;
           client.initalizeHeader();
           this.prepareData();
           this.loading = false;
@@ -403,70 +401,66 @@
       methods: {
           ...mapActions('users', ['getUser', 'setError']),
           prepareData(){
-            this.prepareApprovalRequired(_module.allDataCuration);
-            this.prepareMaintenanceRequests(_module.allDataCuration);
-            this.prepareRecordsInCuration(_module.allDataCuration);
-            this.prepareRecordsWithoutDois(_module.allDataCuration);
-            this.prepareHiddenRecords(_module.allDataCuration);
-            this.prepareRecordsCuratorCreationsLastWeek(_module.allDataCuration);
+            this.prepareApprovalRequired(this.allDataCuration);
+            this.prepareMaintenanceRequests(this.allDataCuration);
+            this.prepareRecordsInCuration(this.allDataCuration);
+            this.prepareRecordsWithoutDois(this.allDataCuration);
+            this.prepareHiddenRecords(this.allDataCuration);
+            this.prepareRecordsCuratorCreationsLastWeek(this.allDataCuration);
           },
           prepareApprovalRequired(dataCuration){
-            let _module = this;
-            let userRecords = dataCuration['approvalsRequired'];
+            let userRecords = dataCuration.approvalsRequired;
             userRecords.forEach(item => {
-              item['fairsharingRecords'].forEach(rec => {
+              item.fairsharingRecords.forEach(rec => {
                 let object = {};
                 object.updatedAt = rec.updatedAt;
-                object.curator = item['username'];
+                object.curator = item.username;
                 object.recordName = rec.name;
                 object.id = rec.id;
                 object.type = rec.type;
-                if (rec['lastEditor'] != null){
-                  object.lastEditor = rec['lastEditor'].username+' ('+rec['lastEditor'].id+')';
+                if (rec.lastEditor){
+                  object.lastEditor = rec.lastEditor.username+' ('+rec.lastEditor.id+')';
                 }else{
                   object.lastEditor = "unknown"
                 }
-
-                _module.approvalRequired.push(object);
+                this.approvalRequired.push(object);
               });
             });
           },
           prepareMaintenanceRequests(dataCuration){
-            let requests = dataCuration['pendingMaintenanceRequests'];
+            let requests = dataCuration.pendingMaintenanceRequests;
             requests.forEach(item => {
               let object = {};
               object.createdAt = item.createdAt;
-              object.recordNameID = item['fairsharingRecord'].name+' ('+item['fairsharingRecord'].id+')';
-              object.userNameID = item['user'].username+' ('+item['user'].id+')';
+              object.recordNameID = item.fairsharingRecord.name+' ('+item.fairsharingRecord.id+')';
+              object.userNameID = item.user.username+' ('+item.user.id+')';
               this.maintenanceRequests.push(object);
             });
           },
           prepareRecordsCuratorCreationsLastWeek(dataCuration){
-            let _module = this;
-            let records = dataCuration['recentCuratorCreations'];
+            let records = dataCuration.recentCuratorCreations;
             records.forEach(item => {
               let object = {};
               object.recordNameID = item.name+' ('+item.id+')';
               object.createdAt = item.createdAt;
-              if (item['creator'] != null){
-                object.creator = item['creator'].username +' ('+item['creator'].id+')';
+              if (item.creator){
+                object.creator = item.creator.username +' ('+item.creator.id+')';
               }else{
                 object.creator = "unknown"
               }
-              _module.recordsCreatedCuratorsLastWeek.push(object);
+              this.recordsCreatedCuratorsLastWeek.push(object);
             });
           },
           prepareRecordsInCuration(dataCuration){
-            let _module = this;
-            let userRecords = dataCuration['recordsInCuration'];
+            let userRecords = dataCuration.recordsInCuration;
             userRecords.forEach(item => {
-              item['fairsharingRecords'].forEach(rec => {
+              item.fairsharingRecords.forEach(rec => {
                 let object = {};
-                object.curator = item['username'];
+                object.curator = item.username;
                 object.recordNameID = rec.name+' ('+rec.id+')';
                 let numMaint = 0;
                 object.recordMaintainers = "none"
-                rec['maintainers'].forEach(main => {
+                rec.maintainers.forEach(main => {
                   if (numMaint > 0){
                     object.recordMaintainers += ', ' + main.username+' ('+main.id+')';
                   }else{
@@ -474,49 +468,47 @@
                   }
                   numMaint += 1;
                 });
-                _module.recordsInCuration.push(object);
+                this.recordsInCuration.push(object);
               });
             });
           },
           prepareRecordsWithoutDois(dataCuration){
-            let _module = this;
-            let records = dataCuration['recordsWithoutDois'];
+            let records = dataCuration.recordsWithoutDois;
             records.forEach(item => {
               let object = {};
               object.recordNameID = item.name+' ('+item.id+')';
               object.createdAt = item.createdAt;
               object.updatedAt = item.updatedAt;
-              if (item['creator'] != null){
-                object.creator = item['creator'].username +' ('+item['creator'].id+')';
-              }else{
-                object.creator = "unknown"
-              }
-              if (item['lastEditor'] != null){
-                object.lastEditor = item['lastEditor'].username +' ('+item['lastEditor'].id+')';
-              }else{
-                object.lastEditor = "unknown"
-              }
-              _module.recordsWithoutDois.push(object);
-            });
-          },
-          prepareHiddenRecords(dataCuration){
-            let _module = this;
-            let records = dataCuration['hiddenRecords'];
-            records.forEach(item => {
-              let object = {};
-              object.recordNameID = item.name+' ('+item.id+')';
-              object.createdAt = item.createdAt;
-              if (item.curator != null){
-                object.curator = item['curator'].username
-              }else{
-                object.curator = 'none'
-              }
-              if (!item.creator){
+              if (item.creator){
                 object.creator = item.creator.username +' ('+item.creator.id+')';
               }else{
                 object.creator = "unknown"
               }
-              _module.hiddenRecords.push(object);
+              if (item.lastEditor){
+                object.lastEditor = item.lastEditor.username +' ('+item.lastEditor.id+')';
+              }else{
+                object.lastEditor = "unknown"
+              }
+              this.recordsWithoutDois.push(object);
+            });
+          },
+          prepareHiddenRecords(dataCuration){
+            let records = dataCuration.hiddenRecords;
+            records.forEach(item => {
+              let object = {};
+              object.recordNameID = item.name+' ('+item.id+')';
+              object.createdAt = item.createdAt;
+              if (item.curator){
+                object.curator = item.curator.username
+              }else{
+                object.curator = 'none'
+              }
+              if (item.creator){
+                object.creator = item.creator.username +' ('+item.creator.id+')';
+              }else{
+                object.creator = "unknown"
+              }
+              this.hiddenRecords.push(object);
             });
           }
       }

add78836d07dc2f612606a5e1c49e164ab041a28
Hossein Mirian <hosein.mirian@gmail.com>
2020-12-04 14:39:56 +0000

    fixed minor issue with FilterChips test

--- tests/unit/components/Records/Search/Header/FilterChips.spec.js
+++ tests/unit/components/Records/Search/Header/FilterChips.spec.js
@@ -39,6 +39,12 @@ describe("FilterChips.vue", () => {
         wrapper.vm.$router.push = push;
         await wrapper.vm.removeParam("publications", "pub2");
         expect($router.push).toHaveBeenCalledTimes(1);
+        let query = wrapper.vm.buildNewQuery("grants", "ABCDEF");
+        expect(query).toStrictEqual({page: 1, publications: 'pub1,pub2,pub3'});
+        query = wrapper.vm.buildNewQuery("publications", "pub4");
+        expect(query).toStrictEqual({page: 1, grants: 'ABCDEF', publications: 'pub1,pub2,pub3'});
+        await wrapper.vm.removeParam("publications", "pub3");
+        expect($router.push).toHaveBeenCalledTimes(1);
         wrapper.vm.$route.query= {'notString':{}}
         expect(wrapper.vm.$route.query).toStrictEqual({"notString":{}});
     });

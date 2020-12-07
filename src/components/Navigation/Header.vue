bdd988ff4fe633e84ed3fce766c67a95de5e5d5c
Hossein Mirian <hosein.mirian@gmail.com>
2020-12-07 16:19:45 +0000

    fixed a minor bug when header and chipsHolder are overlapping

--- src/components/Navigation/Header.vue
+++ src/components/Navigation/Header.vue
@@ -1,7 +1,7 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <v-app-bar
     app
-    hide-on-scroll
+    :hide-on-scroll="UIGeneralStatus.headerVisibilityState"
     short
     height="100"
     max-height="100"

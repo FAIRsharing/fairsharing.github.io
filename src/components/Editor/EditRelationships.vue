b6b4610d1d9a0196b279c3f6c580a700ced1265d
Terazus <batistadominique@hotmail.com>
2020-12-15 05:37:57 -0800

    added GeneralInfo tests

--- src/components/Editor/EditRelationships.vue
+++ src/components/Editor/EditRelationships.vue
@@ -3,6 +3,23 @@
     <v-card-title>
       Edit Relationships
     </v-card-title>
+    <v-card-text>
+      <b> TODO: </b>
+      <v-list>
+        <v-list-item>
+          -> Other elements of data access
+        </v-list-item>
+        <v-list-item>
+          -> Support information
+        </v-list-item>
+        <v-list-item>
+          -> Relationships
+        </v-list-item>
+        <v-list-item>
+          -> Type specific fields (tools, ...)
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 

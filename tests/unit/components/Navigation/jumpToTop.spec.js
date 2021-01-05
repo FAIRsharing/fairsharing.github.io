53e6d381be424aba434b11ed268b86d1678f7945
Hossein Mirian <hosein.mirian@gmail.com>
2021-01-05 12:41:29 +0000

    Add vueScrollTo directive to pass the test fully

--- tests/unit/components/Navigation/jumpToTop.spec.js
+++ tests/unit/components/Navigation/jumpToTop.spec.js
@@ -1,12 +1,19 @@
-import {shallowMount} from "@vue/test-utils";
+import {createLocalVue, shallowMount} from "@vue/test-utils";
 import Vuetify from "vuetify"
 import jumpToTop from "@/components/Navigation/jumpToTop.vue"
 const vuetify = new Vuetify();
+import VueScrollTo from "vue-scrollto";
+
+const localVue = createLocalVue();
+localVue.use(VueScrollTo,{})
 
 describe("jumpToTop.vue", function () {
     let wrapper;
     wrapper = shallowMount(jumpToTop, {
         vuetify,
+        directives: {
+            scrollTo() { /* stub */ }
+        }
     });
 
     it("can be instantiated", () => {

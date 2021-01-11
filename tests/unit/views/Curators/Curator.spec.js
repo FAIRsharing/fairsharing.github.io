5594735b3c418c2203ae3668b4ceed72135387af
ramGranell <ragrator@gmail.com>
2021-01-11 17:29:55 +0000

    tests fail in server

--- tests/unit/views/Curators/Curator.spec.js
+++ tests/unit/views/Curators/Curator.spec.js
@@ -7,7 +7,6 @@ import GraphClient from "@/components/GraphClient/GraphClient.js"
 import usersStore from "@/store/users";
 import Curator from "@/views/Curators/Curator.vue"
 import dataDahboard from "../../../fixtures/curationDashboardData.json"
-import MaintenanceRequest from "@/components/Curators/MaintenanceRequests.vue"
 
 
 const localVue = createLocalVue();

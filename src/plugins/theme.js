afadab31724a3f4f38bbe69243b3d2a9e497c452
Hossein Mirian <hosein.mirian@gmail.com>
2021-01-13 12:17:25 +0000

    Indent code

--- src/plugins/theme.js
+++ src/plugins/theme.js
@@ -1,17 +1,15 @@
 export default {
+    //-- new legacy color palette
     primary: '#27aae1',
     secondary: '#253442',
-    accent:'#006055',
-    accent2:'#E67E22',
-    accent3:'#6B1E1E',
-    // All keys will generate theme styles,
-    // Here we add a custom `tertiary` color
-    // Here we add a custom `tertiary` color
+    accent: '#006055',
+    accent2: '#E67E22',
+    accent3: '#6B1E1E',
     dev_color: '#CB9221',
     uncertain_color: '#ADA3A3',
     ready_color: '#599C0F',
     deprecated_color: '#707070',
-    //subjects domains tax tags color
+    //-- subjects domains tax tags color
     subject_color: '#E67E22',
     domain_color: '#6B1E1E',
     taxonomic_color: '#006055',

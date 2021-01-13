c22957a83ce63bed184c351e3b238ad80a5ff30c
Hossein Mirian <hosein.mirian@gmail.com>
2021-01-13 11:58:17 +0000

    Add tests after refactoring plus completing stringUtils tests

--- tests/unit/components/Records/Search/Output/RecordsCardStack.spec.js
+++ tests/unit/components/Records/Search/Output/RecordsCardStack.spec.js
@@ -24,13 +24,6 @@ describe("RecordsCardStack.vue", function () {
         expect(wrapper.name()).toMatch("RecordsCardStack");
     });
 
-    it("can check getButtonLabel function in CardStack", () => {
-        let returnedValue = wrapper.vm.getButtonLabel('taxonomies');
-        expect(returnedValue).toBe('species');
-        returnedValue = wrapper.vm.getButtonLabel('userDefinedTags');
-        expect(returnedValue).toBe('tags');
-    });
-
     it("can generate correct link depending on doi presence", () => {
         expect(wrapper.vm.getRecordLink(wrapper.vm.record)).toEqual(wrapper.vm.record.id);
         let doi = 'FAIRsharing.wibble';

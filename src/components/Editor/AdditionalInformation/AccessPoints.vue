fcb8f39257f0c26c542212ff4839eae7f66509c9
Milo Thurston <knirirr@gmail.com>
2021-01-26 16:26:50 +0000

    Basis of AccessPoints added #575.

---
+++ src/components/Editor/AdditionalInformation/AccessPoints.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card
+    height="100%"
+    class="flexCard"
+  >
+    <v-card-title>
+      Access Points
+    </v-card-title>
+    <v-card-text>
+      Some info. here about Access Points.
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import {mapGetters, mapState} from "vuex"
+import { isRequired, isUrl } from "@/utils/rules.js"
+
+export default {
+  name: "AccessPoints",
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters("record"),
+    section(){
+      return this.getSection('additionalInformation');
+    },
+    initialFields(){
+      return this.getSection("additionalInformation").initialData
+    },
+    currentFields(){
+      return this.getSection("additionalInformation").data
+    },
+    message(){
+      let error = this.getSection("additionalInfomation").error;
+      return {
+        error: error,
+        value: this.getSection("generalInformation").message,
+        type: function(){if (error){return "error"} else {return "success"}}
+      };
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

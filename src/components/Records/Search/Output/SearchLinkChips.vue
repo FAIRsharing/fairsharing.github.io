418f475b4e9377156051f69f24b60a80b8be5c7b
Hossein Mirian <hosein.mirian@gmail.com>
2021-01-28 13:29:22 +0000

    Fix eslint issues with master branch

--- src/components/Records/Search/Output/SearchLinkChips.vue
+++ src/components/Records/Search/Output/SearchLinkChips.vue
@@ -21,11 +21,21 @@
           {{ chip.label }}
         </div>
       </v-chip>
-      <v-chip disabled outlined label v-if="remainTagCount!==0 && remainTagCount!==1">
-        {{ `+${remainTagCount} more tags`}}
+      <v-chip
+        v-if="remainTagCount!==0 && remainTagCount!==1"
+        disabled
+        outlined
+        label
+      >
+        {{ `+${remainTagCount} more tags` }}
       </v-chip>
-      <v-chip disabled outlined label v-else-if="remainTagCount===1">
-        {{ `one more tag`}}
+      <v-chip
+        v-else-if="remainTagCount===1"
+        disabled
+        outlined
+        label
+      >
+        {{ `one more tag` }}
       </v-chip>
     </v-chip-group>
   </section>

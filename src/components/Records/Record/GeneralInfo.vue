85eaa1ae09881a2799a9f66c4bc4aeace3287cb0
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-01 16:41:15 +0000

    Add some css styling plus improvement in Recrord.vue

--- src/components/Records/Record/GeneralInfo.vue
+++ src/components/Records/Record/GeneralInfo.vue
@@ -2,6 +2,7 @@
   <v-card
     class="pa-4 d-flex flex-column"
     outlined
+    color="bg_record_card"
     tile
     elevation="3"
   >
@@ -9,173 +10,77 @@
     <SectionTitle title="General Information" />
 
     <!-- Title and DOI -->
-    <v-row
-      no-gutters
-      class="mb-2"
-    >
-      <v-col
-        cols="4"
-        sm="2"
-        md="2"
-        lg="1"
-        xl="1"
-        class="d-flex flex-row align-center mt-4 "
-      >
-        <record-status :record="currentRecord['fairsharingRecord']" />
-      </v-col>
-      <v-col
-        class="d-flex flex-column justify-center"
-        cols="12"
-        sm="10"
-        md="10"
-        lg="11"
-        xl="11"
-      >
-        <div class="d-flex flex-column mt-2  ml-sm-6 ml-lg-8">
-          <div class="d-flex flex-row mb-2 align-center">
-            <h3>{{ currentRecord['fairsharingRecord'].name }}</h3>
-            <b
-              v-if="currentRecord['fairsharingRecord'].abbreviation"
-              class="ml-2"
-            >({{ currentRecord['fairsharingRecord'].abbreviation }})</b>
-          </div>
-          <div class="d-flex align-center">
-            <h3 class="mr-1">
-              doi:
-            </h3>
-            <a
-              v-if="currentRecord['fairsharingRecord'].doi"
-              :href="generateDoiLink(currentRecord['fairsharingRecord'].doi)"
-              target="_blank"
-            >
-              {{ currentRecord['fairsharingRecord'].doi }}
-            </a>
-            <NoneFound
-              v-else
-              :string-field="currentRecord['fairsharingRecord'].doi"
-            />
-          </div>
-        </div>
-      </v-col>
-    </v-row>
-
-    <section>
+    <DOITitle />
+    <!--  other data  -->
+    <div class="d-flex flex-column ml-2">
       <!--Type-->
-      <div class="d-flex">
-        <b class="mr-2">Type:</b>
-        <p>
-          {{ cleanString(currentRecord['fairsharingRecord'].type) | capitalize }}
-        </p>
-      </div>
+      <Type />
       <!--Year of Creation-->
-      <!--!! Attention need data model to be changed. must be sent by fairsharing Object like below!! -->
-      <!--fairsharingRecord.year_creation-->
-      <div class="d-flex">
-        <b class="mr-2">Year of Creation:</b>
-        <p v-if="currentRecord['fairsharingRecord'].metadata.year_creation">
-          {{ currentRecord['fairsharingRecord'].metadata.year_creation }}
-        </p>
-        <NoneFound
-          v-else
-          :string-field="currentRecord['fairsharingRecord'].metadata.year_creation"
-        />
-      </div>
+      <YearOfCreation />
       <!--Registry-->
-      <div class="d-flex">
-        <b class="mr-2">Registry:</b>
-        <p>{{ currentRecord['fairsharingRecord'].registry | capitalize }}</p>
-      </div>
+      <Registry />
       <!--Description-->
-      <div class="d-flex align-center mb-4">
-        <b class="mr-2">Description:</b>
-        <p class="mb-0">
-          {{ currentRecord['fairsharingRecord'].description | capitalize }}
-        </p>
-      </div>
-      <!--HomePage-->
-      <div class="d-flex">
-        <b class="mr-2 mb-4">Home Page:</b>
-        <a
-          v-if="currentRecord['fairsharingRecord'].homepage"
-          :href="currentRecord['fairsharingRecord'].homepage"
-          target="_blank"
-        >{{ currentRecord['fairsharingRecord'].homepage }}</a>
-        <NoneFound
-          v-else
-          :string-field="currentRecord['fairsharingRecord'].homepage"
-        />
-      </div>
-      <!--Developed Countries -->
-      <div class="d-flex flex-wrap">
-        <b class="mr-2">Countries involved with this resource:</b>
-        <NoneFound
-          v-if="!currentRecord['fairsharingRecord'].countries"
-          :data-field="currentRecord['fairsharingRecord'].countries"
-        />
-        <p
-          v-else-if="!currentRecord['fairsharingRecord'].countries.length"
-          class="my-0"
-        >
-          None found.
-        </p>
-        <v-tooltip
-          v-for="country in currentRecord['fairsharingRecord'].countries"
-          :key="country.id"
-          top
-        >
-          <template v-slot:activator="{ on }">
-            <v-sheet
-              class="mb-2 flag-mr"
-              v-on="on"
-            >
-              <country-flag
-                v-if="country.code"
-                :country="country.code"
-                size="big"
-              />
-              <div
-                v-else
-                class="warning"
-              >
-                country code undefined!
-              </div>
-            </v-sheet>
-          </template>
-          <span class="white--text">{{ country.name }}</span>
-        </v-tooltip>
-      </div>
-    </section>
+      <Description />
+      <!--Home Page-->
+      <HomePage />
+      <!--Developed Countries-->
+      <Countries />
+      <!--Maintainers-->
+      <Maintainers
+        :can-claim="canClaim"
+        @requestOwnership="callRequestOwnership"
+      />
+      <!--Keywords-->
+      <Keywords />
+      <!--How to cite & publication for record named Citations-->
+      <Citations />
+      <!--updates and creation date-->
+      <UpdateCreateDetail />
+    </div>
+    <section />
   </v-card>
 </template>
 
 <script>
-import CountryFlag from 'vue-country-flag';
-import {mapState} from 'vuex';
-// TODO:
-//import { mapState, mapGetters } from 'vuex';
-
 import SectionTitle from '@/components/Records/Record/SectionTitle';
-import RecordStatus from "@/components/Records/Shared/RecordStatus";
-
-import stringUtils from '@/utils/stringUtils';
-import NoneFound from "@/components/Records/Record/NoneFound";
+import Keywords from "@/components/Records/Record/GeneralInfo/Keywords";
+import DOITitle from "@/components/Records/Record/GeneralInfo/DOITitle";
+import Type from "@/components/Records/Record/GeneralInfo/Type";
+import YearOfCreation from "@/components/Records/Record/GeneralInfo/YearOfCreation";
+import Registry from "@/components/Records/Record/GeneralInfo/Registry";
+import Description from "@/components/Records/Record/GeneralInfo/Description";
+import HomePage from "@/components/Records/Record/GeneralInfo/HomePage";
+import Countries from "@/components/Records/Record/GeneralInfo/Countries";
+import Citations from "@/components/Records/Record/GeneralInfo/Citations";
+import UpdateCreateDetail from "@/components/Records/Record/GeneralInfo/UpdateCreateDetail";
+import Maintainers from "@/components/Records/Record/GeneralInfo/Maintainers";
 
 
 export default {
   name: "GeneralInfo",
   components: {
-    NoneFound,
-    CountryFlag,
-    RecordStatus,
-    SectionTitle
+    Maintainers,
+    UpdateCreateDetail,
+    Citations,
+    Countries,
+    HomePage,
+    Description,
+    Registry,
+    YearOfCreation,
+    Type,
+    DOITitle,
+    Keywords,
+    SectionTitle,
   },
-  mixins: [stringUtils],
-  computed: {
-    ...mapState("record", ["currentRecord"])
+  props: {
+    canClaim: {
+      type: Boolean,
+      default: true
+    }
   },
   methods: {
-    generateDoiLink(doi) {
-      return `https://doi.org/${doi}`
+    callRequestOwnership() {
+      this.$emit('requestOwnership')
     }
   }
 }
@@ -189,9 +94,5 @@ a {
     text-decoration: underline;
     outline: 0;
   }
-}
-
-.flag-mr {
-  margin-right: .29em;
 }
 </style>

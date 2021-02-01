a3dd179f1e80db25a01008c8c706b507db4d357d
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-01 17:32:16 +0000

    Apply Minor change

--- src/utils/recordsCardUtils.js
+++ src/utils/recordsCardUtils.js
@@ -77,6 +77,7 @@ const recordsCardUtils = {
                         record[node].remainTagCount++;
                     }
                 });
+                return true;
             }
             else {
                 return false;

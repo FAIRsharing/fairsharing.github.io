26ff8283fe80333cf3e52a68c62662a0dbc25f91
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-02 12:46:29 +0000

    Add underline effect for links in sass file

--- src/components/Records/Record/GeneralInfo.vue
+++ src/components/Records/Record/GeneralInfo.vue
@@ -85,14 +85,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-a {
-  text-decoration: none;
-
-  &:hover, &:focus {
-    text-decoration: underline;
-    outline: 0;
-  }
-}
-</style>

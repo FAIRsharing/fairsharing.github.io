4c0fa80f5454f2bfde804c71cfac6560a2ced270
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-04 16:36:10 +0000

    Update test

--- tests/unit/components/Navigation/Footer.spec.js
+++ tests/unit/components/Navigation/Footer.spec.js
@@ -1,7 +1,9 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils"
 import VueRouter from "vue-router"
 import Footer from "@/components/Navigation/Footer.vue"
+import Vuetify from 'vuetify'
 
+const vuetify = new Vuetify();
 const localVue = createLocalVue();
 localVue.use(VueRouter);
 const router = new VueRouter();
@@ -11,6 +13,7 @@ describe("Footer.vue", () => {
     it("can be instantiated", () => {
         const wrapper = shallowMount(Footer, {
             localVue,
+            vuetify,
             router
         });
         expect(wrapper.name()).toMatch("Footer");

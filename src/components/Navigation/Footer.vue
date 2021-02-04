b57d33058df1433d0ad61c10a28d10d6bc258aea
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-04 12:20:52 +0000

    Add margin

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -8,7 +8,7 @@
       xsm="12"
       class="flex-column align-center d-flex"
     >
-      <h4>
+      <h4 class="min-width-200 mb-2">
         Developed by
       </h4>
       <ul>
@@ -29,7 +29,7 @@
         </li>
         <li>
           <a
-              class="underline-effect"
+            class="underline-effect"
             href="http://www.ox.ac.uk"
             target="_blank"
           >University of Oxford</a>
@@ -43,12 +43,15 @@
       sm="12"
       class="flex-column align-center d-flex"
     >
-      <h4 id="contact-info">
+      <h4 class="min-width-200 mb-2">
         Contact Us
       </h4>
       <ul>
         <li>
-          <a href="mailto:contact@fairsharing.org?subject=FAIRsharing Issue" class="underline-effect"><i
+          <a
+            href="mailto:contact@fairsharing.org?subject=FAIRsharing Issue"
+            class="underline-effect"
+          ><i
             class="fa fa-envelope"
           />
             Email us for
@@ -82,35 +85,53 @@
       sm="12"
       class="flex-column align-center d-flex"
     >
-      <h4 id="about-info">
+      <h4 class="min-width-200 mb-2">
         About FAIRsharing
       </h4>
       <ul>
         <li>
           <a
-            class="underline-effect"
             id="tour-link"
+            class="underline-effect"
             href="#"
           >Take a Tour</a>
         </li>
         <li>
-          <a href="/communities" class="underline-effect">Community</a>
+          <a
+            href="/communities"
+            class="underline-effect"
+          >Community</a>
         </li>
         <li><a href="/stakeholders">Stakeholders</a></li>
         <li>
-          <a href="/timeline" class="underline-effect">Timeline</a>
+          <a
+            href="/timeline"
+            class="underline-effect"
+          >Timeline</a>
         </li>
         <li>
-          <a href="/licence" class="underline-effect">API and data licence</a>
+          <a
+            href="/licence"
+            class="underline-effect"
+          >API and data licence</a>
         </li>
         <li>
-          <a href="/terms" class="underline-effect">Terms of use</a>
+          <a
+            href="/terms"
+            class="underline-effect"
+          >Terms of use</a>
         </li>
         <li>
-          <a href="/educational/#faq10-1" class="underline-effect">How to cite us</a>
+          <a
+            href="/educational/#faq10-1"
+            class="underline-effect"
+          >How to cite us</a>
         </li>
         <li>
-          <a href="/privacy" class="underline-effect">Privacy policy</a>
+          <a
+            href="/privacy"
+            class="underline-effect"
+          >Privacy policy</a>
         </li>
       </ul>
     </v-col>
@@ -127,7 +148,10 @@
       >Creative Commons
         by
         Share Alike 4.0 International</a>
-      <a href="/licence" class="underline-effect"><img
+      <a
+        href="/licence"
+        class="underline-effect"
+      ><img
         src="https://fairsharing.org/static/img/home/FAIRsharingCC-BY-SA.png"
       ></a>
     </v-row>

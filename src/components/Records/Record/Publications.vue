0da4b6e13b57a1ffa8c39cc9597b989601601656
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-17 11:06:43 +0000

    Remove icons from publications

--- src/components/Records/Record/Publications.vue
+++ src/components/Records/Record/Publications.vue
@@ -27,24 +27,7 @@
           elevation="3"
         >
           <!-- title, url -->
-          <div class="d-flex mt-2 ">
-            <v-tooltip left>
-              <template #activator="{ on }">
-                <v-sheet
-                  class="mb-2 flag-mr"
-                  v-on="on"
-                >
-                  <v-icon
-                    color="secondary"
-                    class="mr-2"
-                  >
-                    fas fa-font
-                  </v-icon>
-                </v-sheet>
-              </template>
-
-              <span>Title</span>
-            </v-tooltip>
+          <div class="d-flex">
             <p
               v-if="publication.url"
               class="ma-0"
@@ -65,24 +48,8 @@
           <!-- Journals -->
           <div
             v-if="publication.journal"
-            class="d-flex mt-2 "
+            class="d-flex"
           >
-            <v-tooltip left>
-              <template #activator="{ on }">
-                <v-sheet
-                  class="mb-2 flag-mr"
-                  v-on="on"
-                >
-                  <v-icon
-                    color="secondary"
-                    class="mr-2"
-                  >
-                    fas fa-book
-                  </v-icon>
-                </v-sheet>
-              </template>
-              <span>Journal</span>
-            </v-tooltip>
             <p class="ma-0">
               {{ publication.journal }}<span v-if="publication.year">, {{ publication.year }}</span>
             </p>
@@ -91,24 +58,8 @@
           <!-- authors -->
           <div
             v-if="publication.authors"
-            class="d-flex mt-2 "
+            class="d-flex"
           >
-            <v-tooltip left>
-              <template #activator="{ on }">
-                <v-sheet
-                  class="mb-2 flag-mr"
-                  v-on="on"
-                >
-                  <v-icon
-                    color="secondary"
-                    class="mr-2"
-                  >
-                    fas fa-people-arrows
-                  </v-icon>
-                </v-sheet>
-              </template>
-              <span>Authors</span>
-            </v-tooltip>
             <p class="ma-0">
               {{ prettifyList(publication.authors) }}
             </p>
@@ -117,24 +68,8 @@
           <!-- doi -->
           <div
             v-if="checkLinkValue(publication.doi)"
-            class="d-flex mt-2 "
+            class="d-flex"
           >
-            <v-tooltip left>
-              <template #activator="{ on }">
-                <v-sheet
-                  class="mb-2 flag-mr"
-                  v-on="on"
-                >
-                  <v-icon
-                    color="secondary"
-                    class="mr-2"
-                  >
-                    fas fa-file-alt
-                  </v-icon>
-                </v-sheet>
-              </template>
-              <span>DOI</span>
-            </v-tooltip>
             <p class="ma-0">
               <a
                 :href="'https://doi.org/' + publication.doi"
@@ -150,22 +85,6 @@
             v-if="checkLinkValue(publication.pubmedId)"
             class="d-flex mt-2 "
           >
-            <v-tooltip left>
-              <template #activator="{ on }">
-                <v-sheet
-                  class="mb-2 flag-mr"
-                  v-on="on"
-                >
-                  <v-icon
-                    color="secondary"
-                    class="mr-2"
-                  >
-                    fas fa-file-medical
-                  </v-icon>
-                </v-sheet>
-              </template>
-              <span>PubMed ID</span>
-            </v-tooltip>
             <p class="ma-0">
               <a
                 :href="'https://pubmed.ncbi.nlm.nih.gov/' + publication.pubmedId"

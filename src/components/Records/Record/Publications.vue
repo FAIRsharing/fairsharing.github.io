c74a9fd6191162b497c67a0e343c0f6a02b5bbbf
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-17 11:11:00 +0000

    Add animation hover effect to card

--- src/components/Records/Record/Publications.vue
+++ src/components/Records/Record/Publications.vue
@@ -20,7 +20,7 @@
         xl="3"
       >
         <v-card
-          class="pa-4 d-flex flex-column"
+          class="pa-4 d-flex flex-column v-card-hover"
           outlined
           color="white"
           tile
@@ -130,6 +130,31 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+
+  &:hover, &:focus {
+    text-decoration: underline;
+    outline: 0;
+  }
+}
+
+.v-card-hover {
+  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  transition: box-shadow .4s linear;
+  -webkit-transition: box-shadow .4s linear;
+  -moz-transition: box-shadow .4s linear;
+  -o-transition: box-shadow .4s linear;
+}
+
+.v-card-hover:hover {
+  transform: scale(1.004);
+  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+}
 
 </style>

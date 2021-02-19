3e6ba8220d7c9bebcd2fe2b20955b4f63d900684
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-19 12:39:39 +0000

    Initialize Collections

---
+++ src/components/Records/Record/Collections.vue
@@ -0,0 +1,105 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-card
+    class="pa-4 d-flex flex-column"
+    outlined
+    color="bg_record_card"
+    tile
+    elevation="3"
+  >
+    <SectionTitle
+      title="Collections"
+    />
+    <div class="d-flex flex-column ml-2 min-height-40">
+      <v-card
+        class="pa-4 mt-4 d-flex flex-column"
+        outlined
+        color="white"
+        tile
+        elevation="3"
+      >
+        <v-tabs
+          v-model="tab"
+          background-color="transparent"
+          color="basil"
+          grow
+        >
+          <v-tab
+            v-for="item in items"
+            :key="item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </v-card>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import SectionTitle from '@/components/Records/Record/SectionTitle';
+import {mapGetters} from "vuex";
+export default {
+  name: "Collections",
+  components: {
+    SectionTitle,
+  },
+  data:()=>{
+    return {
+      tab: null,
+      items: [
+        'In collections', 'In Recommendation',
+      ],
+    }
+  },
+  computed: {
+    ...mapGetters("record", ["getField"]),
+  }
+}
+</script>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+
+  &:hover, &:focus {
+    text-decoration: underline;
+    outline: 0;
+  }
+}
+
+.icon-container {
+  position: absolute;
+  top: -45px;
+  background: white;
+  border: #b3b3b3 dotted 3px;
+  border-radius: 50% !important;
+  -moz-border-radius: 50% !important;
+  -webkit-border-radius: 50% !important;
+  width: 85px;
+  height: 85px;
+  cursor: help;
+}
+
+.card-title-customize {
+  position: absolute;
+  top: 5px;
+  left: 120px
+}
+
+.v-card-hover {
+  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
+  transition: box-shadow .4s linear;
+  -webkit-transition: box-shadow .4s linear;
+  -moz-transition: box-shadow .4s linear;
+  -o-transition: box-shadow .4s linear;
+}
+
+.v-card-hover:hover {
+  transform: scale(1.004);
+  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
+}
+</style>

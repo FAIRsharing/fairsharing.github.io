10c1e09db10f128f0639f4833ea5e7f70ea8c1f9
Hossein Mirian <hosein.mirian@gmail.com>
2021-02-22 13:41:35 +0000

    Create an empty tab for test

--- src/components/Records/Record/Collections.vue
+++ src/components/Records/Record/Collections.vue
@@ -10,52 +10,47 @@
       title="Collections"
     />
     <div class="d-flex flex-column ml-2 min-height-40">
-      <v-card
-        class="pa-4 mt-4 d-flex flex-column"
-        outlined
-        color="white"
-        tile
-        elevation="3"
+      <v-tabs
+        v-model="tabsData.selectedTab"
+        background-color="transparent"
+        grow
+        color="accent3"
+        slider-color="accent3"
+        class="my-5"
       >
-        <v-tabs
-          v-model="tabsData.selectedTab"
-          background-color="transparent"
-          grow
-          color="accent3"
-          slider-color="accent3"
-          class="mb-5"
+        <v-tab
+          v-for="(tabName,tabIndex) in Object.keys(tabsData.tabs)"
+          :key="tabName+'_'+tabIndex"
         >
-          <v-tab
-            v-for="(tabName,tabIndex) in Object.keys(tabsData.tabs)"
-            :key="tabName+'_'+tabIndex"
+          {{ cleanString(tabName) }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items
+        v-model="tabsData.selectedTab"
+        class="transparent"
+      >
+        <v-tab-item
+          v-for="(tabItem,tabItemIndex) in tabsData.tabs"
+          :key="tabItem+'_'+tabItemIndex"
+        >
+          <v-virtual-scroll
+            :items="tabItem"
+            height="400"
+            item-height="130"
+            class="ma-4 overflow-x-hidden"
           >
-            {{ cleanString(tabName) }}
-          </v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tabsData.selectedTab">
-          <v-tab-item
-            v-for="(tabItem,tabItemIndex) in tabsData.tabs"
-            :key="tabItem+'_'+tabItemIndex"
-          >
-            <v-virtual-scroll
-              :items="tabItem"
-              height="400"
-              item-height="130"
-              class="ma-4 overflow-x-hidden"
-            >
-              <template #default="{ item }">
-                <v-card
-                  class="pa-4 d-flex flex-column v-card-hover mx-2 height-120"
-                  flat
-                  outlined
-                >
-                  {{ item.name }}
-                </v-card>
-              </template>
-            </v-virtual-scroll>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-card>
+            <template #default="{ item }">
+              <v-card
+                class="pa-4 d-flex flex-column v-card-hover mx-2 height-120"
+                flat
+                outlined
+              >
+                {{ item.name }}
+              </v-card>
+            </template>
+          </v-virtual-scroll>
+        </v-tab-item>
+      </v-tabs-items>
     </div>
   </v-card>
 </template>
@@ -75,22 +70,16 @@ export default {
       tabsData: {
         selectedTab:'',
         tabs: {
-          in_collections: [{id: 1, name: 'a name 1 ask asdkajsdhaskjdhasd'}, {id: 2, name: 'a name 2'}],
+          in_collections: [{id: 1, name: 'a name 1 ask someone'}, {id: 2, name: 'a name 2'}],
           in_recommendations: [
             {id: 1, name: 'a name 1'}, {id: 2, name: 'a name 2'}, {id: 3, name: 'a name 3'}, {id: 4, name: 'a name 4'}, {id: 5, name: 'a name 5'},{id: 6, name: 'a name 6'},
-          ]
+          ],
+          empty_tab: [],
         }
       },
-      tab: null,
     }
   },
   computed: {
-    items () {
-      return []
-    },
-    length () {
-      return 12
-    },
     ...mapGetters("record", ["getField"]),
   }
 }

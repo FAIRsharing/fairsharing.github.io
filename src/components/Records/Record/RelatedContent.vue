62c6c46de2aece4cd852515285e630ea7b643196
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-01 13:05:19 +0000

    Refactor redundant codes into mixin

--- src/components/Records/Record/RelatedContent.vue
+++ src/components/Records/Record/RelatedContent.vue
@@ -97,13 +97,14 @@ import SectionTitle from '@/components/Records/Record/SectionTitle';
 import {mapState} from "vuex";
 import stringUtils from "@/utils/stringUtils"
 import RecordStatus from "@/components/Records/Shared/RecordStatus";
+import recordTabUtils from "@/utils/recordTabUtils";
 export default {
   name: "RelatedContent",
   components: {
     RecordStatus,
     SectionTitle,
   },
-  mixins:[stringUtils],
+  mixins:[stringUtils,recordTabUtils],
   data: () => {
     return {
       selectedValues: null,
@@ -119,40 +120,6 @@ export default {
   },
   computed: {
     ...mapState("record", ["currentRecord"]),
-    /** Fetch content related to each tab and feed search autocomplete*/
-    getValues() {
-      let selectedTabKey = Object.keys(this.tabsData.tabs)
-      return this.tabsData.tabs[selectedTabKey[this.tabsData.selectedTab]].data;
-    },
-    tabsDataExist() {
-      let inactiveTabs = true
-      for (const [key] of Object.entries(this.filterList)) {
-        if (this.filterList[key].data.length >= 1) {
-          inactiveTabs = false
-          break
-        }
-      }
-      return inactiveTabs
-    },
-    filterList() {
-      const _module = this
-      // here I deep copied object so the references are gone and my object is a new object with unique reference
-      const output = JSON.parse(JSON.stringify(_module.tabsData.tabs));
-
-      if (this.selectedValues !== null && this.selectedValues !== "" && this.selectedValues !== undefined) {
-        let foundItem = output[Object.keys(_module.tabsData.tabs)[_module.tabsData.selectedTab]].data.find(item => item.name === _module.selectedValues)
-        if (foundItem) {
-          output[Object.keys(output)[_module.tabsData.selectedTab]].data = []
-          output[Object.keys(output)[_module.tabsData.selectedTab]].data.push(foundItem)
-        }
-        return output
-      }
-      return output
-    }
-  },
-  beforeMount() {
-    this.prepareTabsData();
-    this.getFirstActiveTab();
   },
   methods: {
     /** Dynamically sets data for each tabs based on the data received from recordAssociations and reverseAssociations*/
@@ -163,47 +130,10 @@ export default {
           _module.tabsData.tabs[tabName].data = _module.prepareAssociations(_module.currentRecord['fairsharingRecord'].recordAssociations, _module.currentRecord['fairsharingRecord']['reverseRecordAssociations'])
               .filter(item => _module.tabsData.tabs[tabName].relation.includes(item.recordAssocLabel) && item.registry === _module.tabsData.tabs[tabName].registry)
         })
-      }
-      else {
+      } else {
         return false
       }
     },
-    /** Combines associations and reserveAssociations into a single array and prepare the data for the search table */
-    prepareAssociations(associations, reverseAssociations) {
-      let _module = this;
-      let recordAssociations = []
-      let joinedArrays = associations.concat(reverseAssociations);
-      const properties = ['fairsharingRecord', 'linkedRecord'];
-
-      joinedArrays.forEach(item => {
-        let object = {};
-        properties.forEach(prop => {
-          if (Object.prototype.hasOwnProperty.call(item, prop)) {
-            object.recordAssocLabel = _module.cleanString(item.recordAssocLabel);
-            object.id = item[prop].id;
-            object.registry = item[prop].registry;
-            object.name = item[prop].name;
-            object.subject = _module.currentRecord['fairsharingRecord'].name;
-            object.type = item[prop].type;
-          }
-        });
-        recordAssociations.push(object);
-      });
-      return recordAssociations;
-    },
-    /** active the very first tab that contains at least one item */
-    getFirstActiveTab() {
-      let firstActiveTabIndex = 0;
-      let index = -1
-      for (const [key] of Object.entries(this.filterList)) {
-        index++
-        if (this.filterList[key].data.length >= 1) {
-          firstActiveTabIndex = index
-          break
-        }
-      }
-      this.tabsData.selectedTab = firstActiveTabIndex
-    }
   }
 }
 </script>

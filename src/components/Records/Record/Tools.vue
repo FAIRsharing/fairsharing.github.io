2aa2b9a0e93a9f80de61a37a8e2a228446321f49
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-16 15:46:29 +0000

    Move shared css code in main.scss

--- src/components/Records/Record/Tools.vue
+++ src/components/Records/Record/Tools.vue
@@ -68,41 +68,10 @@ export default {
 <style scoped lang="scss">
 a {
   text-decoration: none;
+
   &:hover, &:focus {
     text-decoration: underline;
     outline: 0;
   }
-}
-.icon-container {
-  position: absolute;
-  top: -45px;
-  background: white;
-  border: #b3b3b3 dotted 3px;
-  border-radius: 50%!important;
-  -moz-border-radius: 50%!important;
-  -webkit-border-radius: 50%!important;
-  width: 85px;
-  height: 85px;
-  cursor: help;
-}
-.card-title-customize {
-  position: absolute;
-  top: 5px;
-  left: 120px
-}
-.v-card-hover {
-  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
-  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
-  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0) 0 1px 7px 0 !important;
-  transition: box-shadow .4s linear;
-  -webkit-transition: box-shadow .4s linear;
-  -moz-transition: box-shadow .4s linear;
-  -o-transition: box-shadow .4s linear;
-}
-.v-card-hover:hover {
-  transform: scale(1.004);
-  -webkit-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
-  -moz-box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
-  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 0, rgba(0, 0, 0, 0.2) 0 1px 7px 0 !important;
 }
 </style>

0f478adb988975cd9866131f2c1ffe17f60cd13d
Terazus <batistadominique@hotmail.com>
2021-03-17 06:48:31 -0700

    initial commit'

--- src/components/Editor/AdditionalInformation/EditAdditionalInfo.vue
+++ src/components/Editor/AdditionalInformation/EditAdditionalInfo.vue
@@ -3,156 +3,106 @@
     id="editAdditionalInfo"
     ref="editAdditionalInfo"
   >
-    <v-card
-      v-if="initialized"
-      class="delayed-transition"
-    >
-      <v-card-title class="grey lighten-4 blue--text">
+    <v-card>
+      {{allowedFields}}
+      <v-card-title class="blue white--text">
         Edit Additional Information
       </v-card-title>
-
-      <Alerts target="additionalInformation" />
-
-      <v-row
-        v-for="(field, index) in allowedFields"
-        :key="'selected_' + index"
-      >
-        <v-col
-          cols="12"
-          class="pa-6"
+      <v-card-text>
+        <v-container
+          fluid
         >
-          <!-- Dynamically loads components -->
-          <component
-            :is="componentMapping[field]"
-            @update-counts="updateCounts"
-          />
-        </v-col>
-      </v-row>
-
-      <v-card-text />
-      <v-card-actions>
-        <v-btn
-          class="primary"
-          :disabled="false"
-          :loading="loading"
-          @click="saveRecord(false)"
-        >
-          Save and continue
-        </v-btn>
-        <v-btn
-          :disabled="false"
-          :loading="loading"
-          class="primary"
-          @click="saveRecord(true)"
-        >
-          Save and exit
-        </v-btn>
-      </v-card-actions>
+          <v-row>
+            <v-col
+              v-for="(field, fieldName, fieldIndex) in allowedFields.properties"
+              :key="'field_' + fieldIndex"
+              cols="3"
+            >
+              <div v-if="field.type === 'object'">
+                <b class="mr-2"> {{ cleanString(fieldName) }}: </b>
+                {{fields[fieldName]}}
+                <div
+                  v-for="prop in field.properties"
+                  :key="prop.description"
+                >
+                  {{ prop }}
+                </div>
+              </div>
+              <div v-else-if="field.type === 'string'">
+                <v-text-field
+                  v-if="!field.enum"
+                  :value="fields[fieldName]"
+                  :label="cleanString(fieldName)"
+                  outlined
+                  @input="setField($event, fieldName)"
+                />
+                <v-autocomplete
+                  v-else
+                  :value="fields[fieldName]"
+                  :label="cleanString(fieldName)"
+                  :items="field.enum"
+                  outlined
+                  @input="setField($event, fieldName)"
+                />
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+        <!--
+          <div v-else-if="field.type === 'array'">
+            <b class="mr-2"> {{ cleanString(fieldName) }}: </b>
+            {{ field }}
+          </div>
+          -->
+      </v-card-text>
     </v-card>
-    <v-fade-transition>
-      <v-overlay
-        v-if="!initialized"
-        :absolute="false"
-        opacity="0.8"
-      >
-        <loaders />
-      </v-overlay>
-    </v-fade-transition>
   </v-form>
 </template>
 
 <script>
-import {mapActions, mapGetters, mapState} from "vuex";
-import Alerts from "@/components/Editor/Alerts";
-import AccessPoints from "@/components/Editor/AdditionalInformation/AccessPoints";
-import AssociatedTools from "@/components/Editor/AdditionalInformation/AssociatedTools";
-import Loaders from "@/components/Navigation/Loaders";
+import {mapActions, mapGetters, mapState, mapMutations} from "vuex";
 import RestClient from "@/components/Client/RESTClient.js"
+import stringUtils from '@/utils/stringUtils'
 
 let restClient = new RestClient();
 
 export default {
   name: "EditAdditionalInfo",
-  components: { Alerts, AccessPoints, AssociatedTools, Loaders },
+  components: { },
+  mixins: [ stringUtils ],
   data() {
     return {
       initialized: false,
       loading: true,
-      allowedFields: [],
-      componentMapping: {
-        "access_points": "AccessPoints",
-        "associated_tools": "AssociatedTools"
-      },
-      counts: {
-        access_points: 0,
-        associated_tools: 0
-      }
+      allowedFields: []
     }
   },
   computed: {
     ...mapGetters("record", ["getSection"]),
     ...mapState("users", ["user"]),
     fields() {
+      console.log(this.getSection("additionalInformation").data);
       return this.getSection("additionalInformation").data
     }
   },
   mounted(){
     this.$nextTick(async () => {
-      //this.allowedFields = await this.getFieldNames();
-      // TODO: Modify this when all sections have been completed.
-      this.allowedFields = [
-          'access_points',
-          'associated_tools'
-      ];
-      //console.log(JSON.stringify(this.allowedFields));
+      this.allowedFields = await this.getFieldNames();
       this.initialized = true;
       this.loading = false;
     });
   },
   methods: {
     ...mapActions("record", ["updateAdditionalInformation"]),
+    ...mapMutations("record", ["setAdditionalInformation"]),
     async getFieldNames() {
       return restClient.extraMetadataFields(
           this.fields.type,
           this.user().credentials.token
       );
     },
-    async saveRecord(redirect){
-      this.openEditor = false;
-      this.loading = true;
-      await this.updateAdditionalInformation({
-        token: this.user().credentials.token,
-        id: this.$route.params.id
-      });
-      this.loading = false;
-      if (!redirect) {
-        this.$scrollTo("#mainHeader");
-        this.$store.commit("record/setChanges", {
-          section: "additionalInformation",
-          value: 0
-        })
-      }
-      if (redirect && !this.getSection("additionalInformation").error){
-        await this.$router.push({path: '/' + this.$route.params.id})
-      }
-    },
-    updateCounts(count) {
-      /*
-       * Multiple sections may be updating the counts, with different means of calculating changes,
-       * so the updates to the page are all handled here.
-       */
-      let _module = this;
-      let changes = 0;
-      Object.keys(count).forEach( (key) => {
-        _module.counts[key] = count[key];
-      });
-      Object.keys(_module.counts).forEach( (key) => {
-        changes += _module.counts[key];
-      });
-      _module.$store.commit("record/setChanges", {
-        section: "additionalInformation",
-        value: changes
-      });
+    setField(fieldValue, fieldName) {
+      this.setAdditionalInformation({fieldName, fieldValue})
     }
   }
 }

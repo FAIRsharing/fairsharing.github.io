38a951143c6d087d627db80166acc868b159f3b9
ramGranell <ragrator@gmail.com>
2021-03-22 08:50:39 +0000

    first approach statistics

--- src/views/Stats/Statistics.vue
+++ src/views/Stats/Statistics.vue
@@ -1,17 +1,69 @@
 <template>
-  <div
-    id="loginPage"
-    class="login"
-  >
-    <h2>This will be the stat page</h2>
-  </div>
+  <section class="charts">
+    <h3>pie-basic</h3>
+    <vue-highcharts :options="pieOptions" ref="pieChart"></vue-highcharts>
+  </section>
 </template>
 
 <script>
     /** This component handles the statistic page
      *
      */
-    export default {
-        name: "Statistics"
-    }
+     //import FirstGraph from "@/components/StatsPage/FirstGraph.vue"
+     import Vue from "vue/dist/vue.js"
+     import VueHighcharts from 'highcharts-vue';
+
+
+     // pie chart data
+     Vue.use(VueHighcharts);
+     export const PieData = {
+       chart: {
+         type: "pie",
+         options3d: {
+           enabled: true,
+           alpha: 45
+         }
+       },
+       title: {
+         text: "Contents of Highsoft's weekly fruit delivery"
+       },
+       subtitle: {
+         text: "3D donut in Highcharts"
+       },
+       plotOptions: {
+         pie: {
+           innerSize: 100,
+           depth: 45
+         }
+       },
+       series: [
+         {
+           name: "Delivered amount",
+           data: [
+             ["Bananas", 8],
+             ["Kiwi", 3],
+             ["Mixed nuts", 1],
+             ["Oranges", 6],
+             ["Apples", 8],
+             ["Pears", 4],
+             ["Clementines", 4],
+             ["Reddish (bag)", 1],
+             ["Grapes (bunch)", 1]
+           ]
+         }
+       ]
+     };
+     export default {
+       name: "Statistics",
+       components: {
+         VueHighcharts
+       },
+       data() {
+         return {
+           pieOptions: PieData
+         };
+       },
+       mounted() {},
+       methods: {}
+     }
 </script>

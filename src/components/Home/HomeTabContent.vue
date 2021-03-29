216da61e171db0d1b0f478b56fb485a43988cacc
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-29 15:09:35 +0100

    Add HomeTabContent and refactored code

---
+++ src/components/Home/HomeTabContent.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-row class="my-4">
+    <v-col
+      cols="12"
+      sm="12"
+      lg="5"
+      class="d-flex justify-center"
+    >
+      <v-img
+        contain
+        :src="image"
+        :width="$vuetify.breakpoint.mdAndDown?'180':'300'"
+        :height="$vuetify.breakpoint.mdAndDown?'180':'300'"
+      />
+    </v-col>
+    <v-col
+      cols="12"
+      sm="12"
+      lg="7"
+      class="d-flex flex-column justify-center"
+    >
+      <h4 class="mb-2 text-left text-body-2 text-sm-h6 text-md-h5 font-weight-bold primary--text">
+        {{ header }}
+      </h4>
+      <p :class="['lato-font-medium font-weight-thin text-justify lato-text-sm',{'lato-text-md':$vuetify.breakpoint.mdAndUp}]">
+        {{ description }}
+      </p>
+      <router-link
+        class="text-left"
+        to=""
+      >
+        read more
+      </router-link>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "HomeTabContent",
+  props: {
+    header: {
+      type: String,
+      default: null
+    },
+    description: {
+      type: String,
+      default: null
+    },
+    image: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

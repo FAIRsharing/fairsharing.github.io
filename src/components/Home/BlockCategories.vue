69a5039b4323fb919369a6ff17d8caee39df49b8
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-30 13:40:30 +0100

    Add hover animation to BlockCategories card

--- src/components/Home/BlockCategories.vue
+++ src/components/Home/BlockCategories.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row class="block-category">
       <v-col
         cols="12"
         sm="12"
@@ -8,40 +8,25 @@
         lg="4"
       >
         <v-card
-          class="mx-auto"
-          max-width="300"
+          class="mx-auto block-category__card"
+          max-width="350"
         >
           <v-img
             class="white--text align-end"
             height="200px"
             src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
           >
-            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-title>Recommended</v-card-title>
           </v-img>
-
-          <v-card-subtitle class="pb-0">
-            Number 10
-          </v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
+          <v-card-text class="text--primary text-justify">
+            Standards and/or databases recommended by journal or funder data policies.
           </v-card-text>
-
           <v-card-actions>
             <v-btn
-              color="orange"
+              color="primary"
               text
             >
-              Share
-            </v-btn>
-
-            <v-btn
-              color="orange"
-              text
-            >
-              Explore
+              Find
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -53,40 +38,25 @@
         lg="4"
       >
         <v-card
-            class="mx-auto"
-            max-width="300"
+          class="mx-auto block-category__card"
+          max-width="350"
         >
           <v-img
-              class="white--text align-end"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
           >
-            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-title>Recommended</v-card-title>
           </v-img>
-
-          <v-card-subtitle class="pb-0">
-            Number 10
-          </v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
+          <v-card-text class="text--primary text-justify">
+            Standards and/or databases recommended by journal or funder data policies.
           </v-card-text>
-
           <v-card-actions>
             <v-btn
-                color="orange"
-                text
+              color="primary"
+              text
             >
-              Share
-            </v-btn>
-
-            <v-btn
-                color="orange"
-                text
-            >
-              Explore
+              Find
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -98,40 +68,25 @@
         lg="4"
       >
         <v-card
-            class="mx-auto"
-            max-width="300"
+          class="mx-auto block-category__card"
+          max-width="350"
         >
           <v-img
-              class="white--text align-end"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
           >
-            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-title>Recommended</v-card-title>
           </v-img>
-
-          <v-card-subtitle class="pb-0">
-            Number 10
-          </v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
+          <v-card-text class="text--primary text-justify">
+            Standards and/or databases recommended by journal or funder data policies.
           </v-card-text>
-
           <v-card-actions>
             <v-btn
-                color="orange"
-                text
+              color="primary"
+              text
             >
-              Share
-            </v-btn>
-
-            <v-btn
-                color="orange"
-                text
-            >
-              Explore
+              Find
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -146,6 +101,24 @@ name: "BlockCategories"
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.block-category {
+  &__card {
+    transition: all .2ms ease;
+    -webkit-transition:all .2s ease;
+    -moz-transition: all .2s ease;
+    -o-transition: all .2s ease;
+    box-shadow: 0 1rem 2rem rgba(black, .15)!important;
+    -webkit-box-shadow: 0 1rem 2rem rgba(black, .15)!important;
+    -moz-box-shadow: 0 1rem 2rem rgba(black, .15)!important;
+    -o-box-shadow: 0 1rem 2rem rgba(black, .15)!important;
+    &:hover {
+      transform: scale(1.05);
+      -moz-transform: scale(1.05);
+      -webkit-transform: scale(1.05);
+      -o-transform: scale(1.05);
+    }
+  }
+}
 </style>

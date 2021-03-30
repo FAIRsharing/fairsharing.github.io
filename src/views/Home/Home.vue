b8d8e0319ff83258cc91ab3845ec5b8a0c626ae3
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-30 21:14:02 +0100

    Add pattern for HomePage banner

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,23 +2,22 @@
   <v-main>
     <v-container
       fluid
-      class="text-center"
+      class="text-center pa-0"
     >
-      <div class="px-md-10">
-        <h1 class="text-body-1 text-sm-h6 my-4 my-sm-4 my-lg-8 text-md-h6 text-lg-h5 text-xl-h4 font-weight-black secondary--text">
+      <section
+        class="px-md-10 pa-5 "
+        :style="{ backgroundImage: 'linear-gradient(180deg, rgba(37, 52, 66, 1) 0%, rgba(39, 170, 225, 1) 200%),url(' + 'assets/Home/HeroBlock/pattern3.jpg',backgroundRepeat:'repeat',backgroundBlendMode:'multiply'}"
+      >
+        <h1 class="text-body-1 text-sm-h6 pt-2 text-md-h6 text-lg-h4 text-xl-h4 font-weight-black white--text">
           A curated, informative and educational resource on data and metadata standards, inter-related to databases and data policies.
         </h1>
-        <h2 class="text-body-2 text-sm-h5 my-8 my-sm-4 my-lg-8 text-md-h5 text-lg-h4 text-xl-h3 font-weight-medium">
-          How can we help?
-        </h2>
-        <h3 :class="['lato-font-medium my-sm-4 my-lg-8 my-8 primary--text px-10 font-weight-thin',{'lato-text-md':$vuetify.breakpoint.mdOnly,'lato-text-lg':$vuetify.breakpoint.lgAndUp,'lato-text-sm':$vuetify.breakpoint.smAndDown}]">
+        <h3 :class="['lato-font-medium my-sm-4 my-lg-4 my-4  primary--text px-1 font-weight-thin',{'lato-text-md':$vuetify.breakpoint.mdOnly,'lato-text-lg':$vuetify.breakpoint.lgAndUp,'lato-text-sm':$vuetify.breakpoint.smAndDown}]">
           We guide consumers to discover, select and use these resources with confidence, and producers to make their resource more discoverable, more widely adopted and cited.
         </h3>
-      </div>
+      </section>
 
       <v-tabs
         v-model="tabsData.selectedTab"
-        :grow="$vuetify.breakpoint.lgAndDown"
         :show-arrows="$vuetify.breakpoint.mdAndDown"
         centered
         slider-size="5"
@@ -106,9 +105,16 @@ import BlockCategories from "@/components/Home/BlockCategories";
         return {
           tabsData: {
             selectedTab: 0,
-            tabs: tabs
+            tabs: tabs,
+            backHeader:{
+
+            }
           }
         }
       }
     };
 </script>
+<style scoped lang="scss">
+section {
+}
+</style>

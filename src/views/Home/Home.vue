9b4a2c5ebb06c4560515c9cffdea6313f3628455
Hossein Mirian <hosein.mirian@gmail.com>
2021-03-30 22:05:33 +0100

    Update card background pattern

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,10 +5,10 @@
       class="text-center pa-0"
     >
       <section
-        class="px-md-10 pa-5 "
+        class="px-md-10 pa-5"
         :style="{ backgroundImage: 'linear-gradient(180deg, rgba(37, 52, 66, 1) 0%, rgba(39, 170, 225, 1) 200%),url(' + 'assets/Home/HeroBlock/pattern3.jpg',backgroundRepeat:'repeat',backgroundBlendMode:'multiply'}"
       >
-        <h1 class="text-body-1 text-sm-h6 pt-2 text-md-h6 text-lg-h4 text-xl-h4 font-weight-black white--text">
+        <h1 class="text-body-1 text-sm-h6 pt-2 text-md-h6 text-lg-h4 text-xl-h4 font-weight-medium white--text">
           A curated, informative and educational resource on data and metadata standards, inter-related to databases and data policies.
         </h1>
         <h3 :class="['lato-font-medium my-sm-4 my-lg-4 my-4  primary--text px-1 font-weight-thin',{'lato-text-md':$vuetify.breakpoint.mdOnly,'lato-text-lg':$vuetify.breakpoint.lgAndUp,'lato-text-sm':$vuetify.breakpoint.smAndDown}]">
@@ -114,7 +114,3 @@ import BlockCategories from "@/components/Home/BlockCategories";
       }
     };
 </script>
-<style scoped lang="scss">
-section {
-}
-</style>

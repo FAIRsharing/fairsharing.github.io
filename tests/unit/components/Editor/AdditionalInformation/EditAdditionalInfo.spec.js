35f76190a81dec3df343cfdba3554da5ccfbcb4b
Terazus <batistadominique@hotmail.com>
2021-04-06 06:51:37 -0700

    removed associatedTools components + started unit testing

---
+++ tests/unit/components/Editor/AdditionalInformation/EditAdditionalInfo.spec.js
@@ -0,0 +1,83 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import EditAdditionalInfo from "@/components/Editor/AdditionalInformation/EditAdditionalInfo.vue"
+import recordStore from "@/store/record.js";
+import userStore from "@/store/users.js";
+import editorStore from "@/store/editor.js"
+import VueRouter from "vue-router";
+import additionalInformationFixture from "@/../tests/fixtures/additionalInformation.json"
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const $store = new Vuex.Store({modules: {
+    record: recordStore,
+    users: userStore,
+    editor: editorStore
+}});
+$store.state.users.user = function(){return {credentials: {token: "123"}}};
+$store.state.record.sections.additionalInformation = {
+    data: additionalInformationFixture.data,
+    initialData: JSON.parse(JSON.stringify(additionalInformationFixture.data))
+};
+let $route = {params: {id: "123"}};
+const router = new VueRouter();
+const $router = { push: jest.fn() };
+const template = {
+    url: {
+        type: "string",
+        format: "uri",
+        minLength: 1,
+        description: "The URL at which the named data process can be found."
+    }
+};
+
+describe("Editor.vue", function() {
+    let wrapper;
+
+    it("can be mounted without allowed fields", () => {
+        wrapper = shallowMount(EditAdditionalInfo, {
+            localVue,
+            router,
+            mocks: {$store, $route, $router},
+            stubs: ['router-link']
+        });
+        expect(wrapper.name()).toMatch("EditAdditionalInfo");
+    });
+
+    it("can be mounted with allowed fields", () => {
+        $store.state.editor.allowedFields = additionalInformationFixture.schema;
+        wrapper = shallowMount(EditAdditionalInfo, {
+            localVue,
+            router,
+            mocks: {$store, $route, $router},
+            stubs: ['router-link']
+        });
+        expect(wrapper.name()).toMatch("EditAdditionalInfo");
+    });
+
+    it("can show/hide an overlay", () => {
+        wrapper = shallowMount(EditAdditionalInfo, {
+            localVue,
+            router,
+            mocks: {$store, $route, $router},
+            stubs: ['router-link']
+        });
+        wrapper.vm.showOverlay(1, "abc", {schema: "schema"}, template);
+        expect(wrapper.vm.overlay).toStrictEqual({
+            show: true,
+            id: 1,
+            fieldName: "abc",
+            template: template,
+            fields: {schema: "schema"}
+        });
+        wrapper.vm.hideOverlay();
+        expect(wrapper.vm.overlay).toStrictEqual({
+            show: false,
+            id: null,
+            fieldName: null,
+            fields: null,
+            template: null
+        })
+    });
+});

e8f222432f5b650f743d60494be9a76492467113
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-07 10:40:48 +0100

    test fixed

--- tests/unit/store/searchFilters.spec.js
+++ tests/unit/store/searchFilters.spec.js
@@ -100,7 +100,7 @@ describe('searchFilters store methods', () => {
         expect(actions.commit).toHaveBeenCalledWith("searchFilters/setLoadingStatus", true);
         expect(actions.commit).toHaveBeenCalledWith("searchFilters/setFilterButtons");
         expect(actions.commit).toHaveBeenCalledWith("searchFilters/setLoadingStatus", false);
-        expect(actions.commit).toHaveBeenCalledTimes(6);
+        expect(actions.commit).toHaveBeenCalledTimes(7);
         stub.restore();
     });
 

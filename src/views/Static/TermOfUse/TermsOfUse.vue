0c0474caf753be769ba4730247535d2210520764
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-08 18:34:40 +0100

    Add terms of use page

--- src/views/Static/TermOfUse/TermsOfUse.vue
+++ src/views/Static/TermOfUse/TermsOfUse.vue
@@ -1,5 +1,49 @@
 <template>
-  <h2>This will be the Terms of Use page</h2>
+  <main class="pa-5">
+    <h1 class="text-h4 mb-2">
+      Acceptable Use Policy and Conditions of Use
+    </h1>
+    <p class="text-h6">
+      This Acceptable Use Policy and Conditions of Use (“AUP”) defines the rules and conditions that govern your access to and use (including transmission, processing, and storage of data) of the resources and services (“Services”) as granted by FAIRsharing for the purpose of finding and identifying the appropriate record types for use.
+    </p>
+    <ul>
+      <li>
+        You shall only use the Services in a manner consistent with the purposes and limitations described above; you shall show consideration towards other users including by not causing harm to the Services; you have an obligation to collaborate in the resolution of issues arising
+      </li>
+      <li>
+        from your use of the Services.
+      </li>
+      <li>
+        You shall only use the Services for lawful purposes and not breach, attempt to breach, nor circumvent administrative or security controls.
+      </li>
+      <li>
+        You shall respect intellectual property and confidentiality agreements.
+      </li>
+      <li>
+        You shall protect your access credentials (e.g. passwords, private keys or multi-factor tokens); no intentional sharing is permitted.
+      </li>
+      <li>
+        You shall keep your registered information correct and up to date.
+      </li>
+      <li>
+        You shall promptly report known or suspected security breaches, credential compromise, or misuse to the security contact stated below; and report any compromised credentials to the relevant issuing authorities.
+      </li>
+      <li>
+        Your personal data will be processed in accordance with the privacy statements referenced below.
+      </li>
+      <li>
+        Your use of the Services may be restricted or suspended, for administrative, operational, or security reasons, without prior notice and without compensation.
+      </li>
+      <li>
+        If you violate these rules, you may be liable for the consequences, which may include your account being suspended and a report being made to your home organisation or to law Enforcement.
+      </li>
+    </ul>
+    <ul class="font-weight-bold mt-5">
+      <li>The administrative and security contact for this AUP is: contact@fairsharing.</li>
+      <li>The privacy statements (e.g. Privacy Notices) are located at: https://fairsharing.org/privacy.</li>
+      <li>API use and data License statements are located at: https://fairsharing.org/licence.</li>
+    </ul>
+  </main>
 </template>
 
 <script>
@@ -15,5 +59,7 @@
 </script>
 
 <style scoped>
-
+li {
+  margin-bottom: 5px;
+}
 </style>

70adce020371ba80a668929a9d27ad4bd492f673
ramGranell <ragrator@gmail.com>
2021-04-09 18:37:11 +0100

    adding eight charts

--- src/views/Stats/Statistics.vue
+++ src/views/Stats/Statistics.vue
@@ -1,98 +1,344 @@
 <template>
-  <v-container fluid>
-    <v-card-title
-      primary-title
-      class="justify-center"
-    >
-      <div>
-        <h3
-          class="headline text--accent-2"
+  <v-main>
+    <v-container fluid>
+      <v-card-title
+        primary-title
+        class="justify-center"
+      >
+        <div>
+          <h3
+            class="headline text--accent-2"
+          >
+            Summary Statistics (under construction)
+          </h3>
+        </div>
+      </v-card-title>
+      <v-row
+        align="center"
+        justify="space-around"
+      >
+        <v-btn
+          depressed
+          :outlined="activeChart !== 0"
+          color="primary"
+          @click="chartSelection(0)"
         >
-          Summary Statistics (under construction)
-        </h3>
-      </div>
-    </v-card-title>
-    <v-row
-      align="center"
-      justify="space-around"
-    >
-      <v-btn
-        depressed
-        :outlined="activeChart !== 0"
-        color="primary"
-        @click="chartSelection(0)"
-      >
-        All
-      </v-btn>
-      <v-btn
-        depressed
-        :outlined="activeChart !== 1"
-        color="primary"
-        @click="chartSelection(1)"
-      >
-        Standards
-      </v-btn>
-      <v-btn
-        depressed
-        :outlined="activeChart !== 2"
-        color="primary"
-        @click="chartSelection(2)"
-      >
-        Databases
-      </v-btn>
-      <v-btn
-        depressed
-        :outlined="activeChart !== 3"
-        color="primary"
-        @click="chartSelection(3)"
-      >
-        Policies
-      </v-btn>
-    </v-row>
-    <v-divider
-      class="mx-4"
-      vertical
-    />
-    <v-row
-      v-if="activeChart === 0"
-      no-gutters
-    >
-      <v-col
-        md="12"
-        sm="12"
-        lg="6"
-      >
-        <v-card
-          v-if="optionChartPie1"
-          class="pa-2"
-          outlined
-          tile
+          All
+        </v-btn>
+        <v-btn
+          depressed
+          :outlined="activeChart !== 1"
+          color="primary"
+          @click="chartSelection(1)"
         >
-          <highcharts
-            ref="chartComponent1"
-            :options="optionChartPie1"
-          />
-        </v-card>
-      </v-col>
-      <v-col
-        md="12"
-        sm="12"
-        lg="6"
-      >
-        <v-card
-          v-if="optionChartBars1"
-          class="pa-2"
-          outlined
-          tile
+          Standards
+        </v-btn>
+        <v-btn
+          depressed
+          :outlined="activeChart !== 2"
+          color="primary"
+          @click="chartSelection(2)"
         >
-          <highcharts
-            ref="chartComponent2"
-            :options="optionChartBars1"
-          />
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          Databases
+        </v-btn>
+        <v-btn
+          depressed
+          :outlined="activeChart !== 3"
+          color="primary"
+          @click="chartSelection(3)"
+        >
+          Policies
+        </v-btn>
+      </v-row>
+      <v-divider
+        class="mx-4"
+        vertical
+      />
+      <template v-if="activeChart === 0">
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartRegistries"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartRegistries"
+                :options="chartRegistries"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsStand"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsStand"
+                :options="chartSubjectsStand"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsDB"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsDB"
+                :options="chartSubjectsDB"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsPol"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsPol"
+                :options="chartSubjectsPol"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesAll"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesAll"
+                :options="chartCountriesAll"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesStand"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesStand"
+                :options="chartCountriesStand"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesDB"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesDB"
+                :options="chartCountriesDB"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesPol"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesPol"
+                :options="chartCountriesPol"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-else-if="activeChart === 1">
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsStand"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsStand2"
+                :options="chartSubjectsStand"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesStand"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesStand2"
+                :options="chartCountriesStand"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-else-if="activeChart === 2">
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsDB"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsDB2"
+                :options="chartSubjectsDB"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesDB"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesDB2"
+                :options="chartCountriesDB"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-else-if="activeChart === 3">
+        <v-row
+          no-gutters
+        >
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartSubjectsPol"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartSubjectsPol2"
+                :options="chartSubjectsPol"
+              />
+            </v-card>
+          </v-col>
+          <v-col
+            md="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              v-if="chartCountriesPol"
+              class="pa-2"
+              outlined
+              tile
+            >
+              <highcharts
+                ref="chartCountriesPol2"
+                :options="chartCountriesPol"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+    </v-container>
+    <v-fade-transition>
+      <v-overlay
+        v-if="isLoading"
+        :absolute="false"
+        opacity="0.8"
+      >
+        <Loaders />
+      </v-overlay>
+    </v-fade-transition>
+  </v-main>
 </template>
 
 <script>
@@ -103,7 +349,8 @@
      import GraphClient from "@/components/GraphClient/GraphClient.js"
      import getFilters from "@/components/GraphClient/queries/getFilters.json"
      import Highcharts from 'highcharts'
-     import Pie from 'highcharts/modules/variable-pie'
+     import Loaders from "@/components/Navigation/Loaders"
+     import {cloneDeep} from 'lodash'
 
 
      const client = new GraphClient();
@@ -119,16 +366,21 @@
      });
 
      // pie chart data
-     Pie (Highcharts);
+     //Pie (Highcharts);
 
      export default {
        name: "Statistics",
        components: {
-         highcharts: Chart
+         highcharts: Chart,
+         Loaders
        },
        data () {
            return {
+             isLoading: false,
              allDataStats: null,
+             allDataStatsStand: null,
+             allDataStatsPol: null,
+             allDataStatsDB: null,
              activeChart : 0,
              optionChartPie:{
                chart:{
@@ -212,27 +464,53 @@
                 },
                 series: []
              },
-             optionChartPie1: null,
-             optionChartBars1: null
+             chartRegistries: null,
+             chartSubjectsStand: null,
+             chartSubjectsDB: null,
+             chartSubjectsPol: null,
+             chartCountriesStand: null,
+             chartCountriesDB: null,
+             chartCountriesPol: null,
+             chartCountriesAll: null
          }
        },
        async mounted() {
-         this.$nextTick(async function () {
-
+         this.$nextTick(async () => {
+           this.isLoading = true;
            let data = await client.executeQuery(getFilters);
            this.allDataStats = data.searchFairsharingRecords.aggregations;
+
+           //console.log(this.allDataStats);
+           let QueryTwo = getFilters;
+           let filter ={};
+           filter['fairsharingRegistry']='Standard';
+           QueryTwo['queryParam'] =filter;
+           let dataStand;
+           dataStand = await client.executeQuery(getFilters);
+           this.allDataStatsStand = dataStand.searchFairsharingRecords.aggregations;
+
+           filter['fairsharingRegistry']='Database';
+           QueryTwo['queryParam'] =filter;
+           let dataDB;
+           dataDB = await client.executeQuery(getFilters);
+           this.allDataStatsDB = dataDB.searchFairsharingRecords.aggregations;
+
+           filter['fairsharingRegistry']='Policy';
+           QueryTwo['queryParam'] =filter;
+           let dataPol;
+           dataPol = await client.executeQuery(getFilters);
+           this.allDataStatsPol = dataPol.searchFairsharingRecords.aggregations;
+
+           //console.log(this.allDataStatsStand);
            this.prepareData();
-           //console.log(this.allDataStats);
-           //let QueryTwo = getFilters;
-           //let filter ={};
-           //filter['fairsharingRegistry']='Database';
-           //QueryTwo['queryParam'] =filter;
-           //data = await client.executeQuery(getFilters);
-           //this.allDataStats = data.searchFairsharingRecords.aggregations;
-           //console.log(this.allDataStats);
+           this.isLoading = false;
          })
          //console.log(this.allDataStats.fairsharing_registry.buckets)
          //console.log(this.allDataStats.countries.buckets);
+       },
+       beforeDestroy() {
+         this.chartRegistries = null;
+         delete this.chartRegistries;
        },
        methods: {
          chartSelection(id) {
@@ -240,12 +518,25 @@
          },
          prepareData(){
            this.prepareRegistryCounts(this.allDataStats);
-           this.prepareRecordCountry(this.allDataStats);
+           this.chartSubjectsStand = cloneDeep(this.optionChartBars);
+           this.prepareSubject(this.allDataStatsStand, this.chartSubjectsStand, "standard");
+           this.chartSubjectsDB = cloneDeep(this.optionChartBars);
+           this.prepareSubject(this.allDataStatsDB, this.chartSubjectsDB, "database");
+           this.chartSubjectsPol = cloneDeep(this.optionChartBars);
+           this.prepareSubject(this.allDataStatsPol, this.chartSubjectsPol, "policy");
+           this.chartCountriesStand = cloneDeep(this.optionChartBars);
+           this.prepareRecordCountry(this.allDataStatsStand, this.chartCountriesStand,"standard");
+           this.chartCountriesDB = cloneDeep(this.optionChartBars);
+           this.prepareRecordCountry(this.allDataStatsDB, this.chartCountriesDB,"database");
+           this.chartCountriesPol = cloneDeep(this.optionChartBars);
+           this.prepareRecordCountry(this.allDataStatsPol, this.chartCountriesPol,"policy");
+           this.chartCountriesAll = cloneDeep(this.optionChartBars);
+           this.prepareRecordCountry(this.allDataStats, this.chartCountriesAll,"record");
          },
          prepareRegistryCounts(data){
-           this.optionChartPie1 = this.optionChartPie;
-           this.optionChartPie1.title.text= "Content divided by type";
-           this.optionChartPie1.series[0].data = [];
+           this.chartRegistries = cloneDeep(this.optionChartPie);
+           this.chartRegistries.title.text= "Content divided by type";
+           this.chartRegistries.series[0].data = [];
            let regBucket = data.fairsharing_registry.buckets;
            let nameMap = { policy:"Policies", standard:"Standards", database:"Databases", collection:"Collections" };
            regBucket.forEach(item => {
@@ -254,14 +545,19 @@
                y: item.doc_count,
                url: '/#/search?fairsharingRegistry='+item.key
              };//window.location.hostname+
-             this.optionChartPie1.series[0].data.push(vectItem);
+             this.chartRegistries.series[0].data.push(vectItem);
            });
          },
-         prepareRecordCountry(data){
-           this.optionChartBars1 = this.optionChartBars;
-           this.optionChartBars1.title.text= "Top 10 countries by content"
-           this.optionChartBars1.yAxis.title.text = 'Number of records'
-           this.optionChartBars1.series = [];
+         prepareRecordCountry(data,chartField,text){
+           let searchText = "";
+           if ( text === 'record' ){
+             chartField.title.text = "Top 10 countries by content";
+           }else{
+             chartField.title.text = "Top 10 "+text+ " producing countries";
+             searchText = "&fairsharingRegistry="+text;
+           }
+           chartField.yAxis.title.text = 'Number of '+text+'s';
+           chartField.series = [];
            let totRecords = data.countries.doc_count;
            let regBucket = data.countries.buckets.slice(0,10);
            let nameMap = { "united states of america":"USA",
@@ -282,7 +578,7 @@
                point: {
                    events: {
                      click: function() {
-                       location.href = '/#/search?countries='+item.key;
+                       location.href = '/#/search?countries='+item.key+searchText;
                      }
                    }
                },
@@ -296,9 +592,54 @@
              };
              vectItem.data.push(datItem);
              varX +=1;
-             this.optionChartBars1.series.push(vectItem);
+             chartField.series.push(vectItem);
+           });
+         },
+         prepareSubject(data,chartField,text){
+           let textPlural = "";
+           if (text === "policy"){
+             textPlural = "policies";
+           }else{
+             textPlural = text+"s";
+           }
+           chartField.title.text= "Top 10 ontologies subjects covered by "+textPlural;
+           chartField.yAxis.title.text = 'Number of '+textPlural;
+           chartField.series = [];
+           let totRecords = data.subjects.doc_count;
+           let regBucket = data.subjects.buckets.slice(0,10);
+           let varX = 0;
+           let nameC = "";
+           regBucket.forEach(item => {
+             //console.log(item.key);
+             let words = item.key.split(" ");
+             for (let i = 0; i < words.length; i++) {
+               words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+             }
+             nameC =words.join(" ");
+             let vectItem = {
+               name: nameC,
+               cursor: 'pointer',
+               point: {
+                   events: {
+                     click: function() {
+                       location.href = '/#/search?fairsharingRegistry='+text+'&subjects='+item.key;
+                     }
+                   }
+               },
+               data: []
+             };
+             let datItem = {
+               name: "",
+               x: varX,
+               y: item.doc_count,
+               z: 100*item.doc_count/totRecords
+             };
+             vectItem.data.push(datItem);
+             varX +=1;
+             chartField.series.push(vectItem);
            });
          }
+
       }
     }
   </script>

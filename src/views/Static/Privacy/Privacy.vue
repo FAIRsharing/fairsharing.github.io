79a9dbd57ed498aaf72e93b2b6a33d88f67e8d54
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-12 15:00:18 +0100

    Privacy Completed

--- src/views/Static/Privacy/Privacy.vue
+++ src/views/Static/Privacy/Privacy.vue
@@ -1,19 +1,53 @@
 <template>
-  <h2>This will be the privacy page</h2>
+  <main class="pa-5 mt-5 mb-10">
+    <v-expansion-panels
+      v-model="allOpen"
+      multiple
+      hover
+      accordion
+    >
+      <v-expansion-panel
+        v-for="(item,i) in privacyData.bullet_points"
+        :key="i"
+      >
+        <v-expansion-panel-header>
+          <h3>{{ item.title }}</h3>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p
+            class="lato-font-medium"
+            v-html="item.content"
+          />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </main>
 </template>
 
 <script>
-    /** This component handles the sign-up/register page
-     * @memberOf Static
-     * @name Privacy
-     * @type module
-     * @instance
-     * */
-    export default {
-        name: "Privacy"
+import {privacy} from '@/data/PrivacyPolicyData.json'
+export default {
+  name: "Privacy",
+  data: () => {
+    return {
+      privacyData: privacy,
+      panel:[]
     }
+  },
+  computed:{
+    allOpen () {
+      const _module = this;
+      _module.panel = [...Array(_module.privacyData['bullet_points'].length).keys()].map((k, i) => i)
+      return _module.panel;
+    }
+  }
+}
 </script>
 
 <style scoped>
-
+p {
+  white-space: pre-wrap;
+  /* make the line of paragraph to break when reaching /n in a string */
+  line-height: 1.6;
+}
 </style>

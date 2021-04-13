5f1edfed06298994859181aa57c6c0e193cc1b76
Terazus <batistadominique@hotmail.com>
2021-04-13 09:04:35 -0700

    fully working nawdrawer

--- src/components/Navigation/NavigationDrawer.vue
+++ src/components/Navigation/NavigationDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-10 px-5">
+  <div class="py-10 px-5 d-flex flex-column" style="height:100%">
     <div
       v-for="(button, index) in buttons"
       :key="'button_' + index"
@@ -14,11 +14,55 @@
         <span :class="['white--text',{'primary--text':!button.active}]">{{ button.name }}</span>
       </v-btn>
     </div>
+    <div style="flex-grow: 1">
+      <v-btn
+        v-if="!user().isLoggedIn"
+        color="accent3 white--text"
+        class="mr-1 mt-10"
+        dark
+        width="100%"
+        @click="goToLogin()"
+      >
+        Login
+        <v-icon
+          class="ml-1"
+          small
+        >
+          fa fa-sign-in-alt
+        </v-icon>
+      </v-btn>
+      <v-btn
+        v-else
+        class="mr-1 mt-10 accent3 pl-2"
+        to="/accounts/profile"
+        width="100%"
+      >
+        <v-avatar>
+          <v-icon
+            dark
+            color="white"
+          >
+            fa-user-circle
+          </v-icon>
+        </v-avatar>
+        <span class="white--text ellipse-150">{{ user().credentials.username }}</span>
+      </v-btn>
+    </div>
+    <div>
+      <router-link to="/">
+        <v-img
+          src="@/assets/fairsharing-logo.svg"
+          height="70"
+          contain
+        />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
+  import {mapState} from 'vuex'
+  export default {
         name: "NavigationDrawer",
         data(){
             return {
@@ -63,6 +107,7 @@
             }
         },
         computed: {
+            ...mapState('users', ["user"]),
             route(){
                 return {
                     path: this.$route.path,
@@ -96,13 +141,23 @@
                     })
                 }
             },
+            async goToLogin(){
+              if (this.$route.path !== '/accounts/login') {
+                await this.$router.push({
+                  path: "/accounts/login",
+                  query: {
+                    goTo: this.$route.fullPath
+                  }
+                })
+              }
+            },
             makeActiveButton(val){
                 this.buttons.forEach(button => {
                     button.active =
                         button.path === val.path.replace("/", "")
                         && button.query['fairsharingRegistry'] === val.query['fairsharingRegistry'];
                 });
-            }
+            },
         }
     }
 </script>

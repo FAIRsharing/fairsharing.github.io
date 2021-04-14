1026ad42c425123a35757ea3946d3ed5082322d8
ramGranell <ragrator@gmail.com>
2021-04-14 15:39:35 +0100

    repaired the bug and test created to see the special case

--- src/components/Curators/RecordsAwaitingApproval.vue
+++ src/components/Curators/RecordsAwaitingApproval.vue
@@ -159,9 +159,14 @@
             </td>
             <td>
               {{ props.item.createdAt }},
-              <a :href="'#/users/' + props.item.idCreator">
-                {{ props.item.creator }} 
-              </a>
+              <div v-if="props.item.creator === 'unknown'">
+                {{ props.item.creator }}
+              </div>
+              <div v-else>
+                <a :href="'#/users/' + props.item.idCreator">
+                  {{ props.item.creator }}
+                </a>
+              </div>
             </td>
           </tr>
         </template>

3937f0fd968c4b376ed2bfdfa27dbd88e69483e6
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-15 12:22:32 +0100

    License initialized

--- src/views/Static/License/License.vue
+++ src/views/Static/License/License.vue
@@ -1,14 +1,26 @@
 <template>
-  <h2>This will be the API/License page</h2>
+  <main class="pa-5 mt-5 mb-10">
+    <!--  main_title -->
+    <h1 class="text-h4 text-xl-h3 mb-2 mb-xl-6">
+      API and data License
+    </h1>
+
+    <!--  main_title_2 -->
+    <h2 class="text-h4 text-xl-h3 mb-2 mb-xl-6">
+      some subtitle
+    </h2>
+
+    <!-- eslint-disable vue/no-v-html -->
+    <!--
+    <p
+        :class="['mb-8 lato-font-medium lato-text-md',{'lato-text-lg':$vuetify.breakpoint.xlOnly }]"
+        v-html="stakeholdersData.main_subtitle"
+    />
+-->
+  </main>
 </template>
 
 <script>
-    /** This component handles the sign-up/register page
-     * @memberOf Static
-     * @name License
-     * @type module
-     * @instance
-     * */
     export default {
         name: "License"
     }

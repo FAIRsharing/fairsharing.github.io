34d0b3f8232e8ee1e160496b1523d627f2797188
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-16 17:41:42 +0100

    format v-html data

--- src/views/Static/License/License.vue
+++ src/views/Static/License/License.vue
@@ -1,26 +1,87 @@
 <template>
   <main class="pa-15 mb-10">
     <!--  main_title -->
-    <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
-      Data sharing license and attribution, via the web and API
+    <h1 class="text-h4 text-xl-h3 mb-2 mb-xl-6">
+      FAIRsharing API
     </h1>
+
+    <h2 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      Introduction
+    </h2>
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      The new FAIRsharing application has a REST API which offers the opportunity to both query and modify data. It's accessible to any user with an account on the system for showing basic information and for modifying any record you've created or maintain. To query more detailed information from it, please email <a
+        href="contact@fairsharing.org"
+        target="_blank"
+      >contact@fairsharing.org</a> to discuss your requirements.
+    </p>
+
+    <h2 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      Creating an account
+    </h2>
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      You can create an account by going to the <a
+        href="https://fairsharing.org"
+        class="underline-effect"
+      >FAIRsharing site</a>, clicking the "login" button and then choosing "create new account". You can also create an account by signing in with ORCID, Twitter or Github, but if you do you'll need to reset your password (click on the "Forgot your password?" link) before you can use the API.
+    </p>
+
+    <h2 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      Using the API
+    </h2>
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      The base URL of the API is <mark>https://api.fairsharing.org</mark>; paths below may only give the route rather than the full URL, e.g. <mark>/fairsharing_records/123</mark>.
+    </p>
+
+    <h2 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      Logging in
+    </h2>
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      You will need to POST the following to <mark>https://api.fairsharing.org/users/sign_in</mark>:
+    </p>
+    <pre
+      class="mt-2 mb-8"
+      v-text="jsonData.login()"
+    />
 
     <p
       :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
     >
-      Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution and Share-alike (CC-BY-SA) International 4.0 license.
+      A successful response will return something like the following:
     </p>
-    <p
-      :class="['mb-0 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-    >
-      In addition, we request that any data taken from FAIRsharing, either by the API, scraping the site or other means, is attributed and cited appropriately. We request that a link and the FAIRsharing logo is presented next to the data taken from FAIRsharing.
-    </p>
-    <v-img
-      src="@/assets/fairsharing-logo.svg"
-      height="70"
-      contain
-      class="text-left pa-0 d-inline-block"
+    <pre
+      class="mt-2 mb-8"
+      v-text="jsonData.loginResponse()"
     />
+
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      In addition to the general information about your account there's a "jwt" field. This is a JSON web token which is valid until the expiry time given, and you'll need this for any further interactions with the API. The way it should be used is to send headers with each request, as follows:
+    </p>
+    <pre
+      class="mt-2 mb-8"
+      v-text="jsonData.jwt()"
+    />
+
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      Here are some examples of both logging in and quering a record. Each of these queries is for a single FAIRsharing Record with ID of 1.
+    </p>
+    <pre
+      class="mt-2 mb-8"
+      v-html="jsonData.cURL()"
+    />
+
+
     <p
       :class="['mb-0 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
     >
@@ -129,16 +190,67 @@
 </template>
 
 <script>
-    export default {
-        name: "License"
+export default {
+  name: "License",
+  data: () => {
+    return {
+      jsonData: {
+        login: () => {
+          return JSON.stringify({
+            "user":
+                {
+                  "login": "your_username",
+                  "password": "your_password"
+                }
+          }, undefined, 3)
+        },
+        jwt: () => {
+          return JSON.stringify({
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer your_jwt_goes_here"
+          }, undefined, 3)
+        },
+        loginResponse: () => {
+          return JSON.stringify({
+            "success": true,
+            "jwt": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxYjhjMmMwMS05MjAsg3LTQxOGUtOTNiNS04NjNjNDE1NWZmM302LCJzdWIiOiI2NzMiLCJzY3AiOiJ1c2VyIiwiYXVkIjpudWxsLCJpYXQiOjE2MTQzMzk1MTMsImV4cCI6MTYxNDQyNTkxM30.QzTDIeDCvHc49lLbztxB5roGtIUuOTlRIOLFmiy2M6g",
+            "username": "joe_bloggs",
+            "id": 123,
+            "role": "user",
+            "profile_type": "publisher",
+            "watched_records": [
+              1,
+            ],
+            "is_curator": false,
+            "is_special_curator": false,
+            "expiry": 1614425000,
+            "message": "Authentication successful"
+          }, undefined, 3)
+        },
+        cURL: () => {
+          return 'curl --location --request POST \'https://api.fairsharing.org/users/sign_in\' \n' +
+              '--header \'Accept: application/json\' \n' +
+              '--header \'Content-Type: application/json\' \n' +
+              '--data-raw \'{"user": {"login":"your_username","password":"your_secret_password"} }\'\n' +
+              '\n' +
+              'curl --location --request GET \'https://api.fairsharing.org/fairsharing_records/1\' \n' +
+              '--header \'Accept: application/json\' \n' +
+              '--header \'Content-Type: application/json\' \n' +
+              '--header \'Authorization: Bearer your_jwt_goes_here\' '
+        }
+
+      }
     }
+  }
+}
 </script>
 
 <style scoped>
 pre {
   padding: 9px;
   margin: 0 0 10px;
-  font-size: 15px;
+  font-size: 1rem;
   font-family: monospace;
   word-break: break-all;
   word-wrap: break-word;

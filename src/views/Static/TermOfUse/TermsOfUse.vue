9e6f67b413cb77085bc3c1bc4df087447371f169
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-20 13:18:43 +0100

    Harmonize termsofUse fonts and sizer

--- src/views/Static/TermOfUse/TermsOfUse.vue
+++ src/views/Static/TermOfUse/TermsOfUse.vue
@@ -1,11 +1,13 @@
 <template>
-  <main class="pa-5 mt-5 mb-10">
+  <main class="pa-15 mb-10">
     <!--  title  -->
-    <h1 class="text-h4 mb-2">
+    <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
       {{ TermsOfUse.title }}
     </h1>
     <!--  subtitle  -->
-    <p class="text-h6 mb-8">
+    <p
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
       {{ TermsOfUse.subTitle }}
     </p>
 
@@ -16,19 +18,23 @@
         :key="index"
         class="mb-4"
       >
-        <span class="mr-2">{{ index+1 }}-</span>
         {{ bulletPoint }}
       </li>
     </ul>
 
     <!--  contact points  -->
     <div>
-      <p class="d-inline-block mr-2">
+      <p
+        :class="['mb-4 lato-font-medium d-inline-block mr-2 lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      >
         The administrative and security contact for this AUP is:
       </p><a :href="`mailto:${TermsOfUse.admin_contact}`">{{ TermsOfUse.admin_contact }}</a>.
     </div>
+
     <div>
-      <p class="d-inline-block mr-2">
+      <p
+        :class="['mb-4 lato-font-medium d-inline-block mr-2 lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      >
         The privacy statements (e.g. Privacy Notices) are located at:
       </p><a
         class="underline-effect"
@@ -37,7 +43,9 @@
       >{{ TermsOfUse.privacy_statements }}</a>.
     </div>
     <div>
-      <p class="d-inline-block mr-2">
+      <p
+        :class="['mb-4 lato-font-medium d-inline-block mr-2 lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      >
         API use and data License statements are located at:
       </p><a
         class="underline-effect"
@@ -62,6 +70,19 @@ import {termsOfUse} from '@/data/TermsofUseData.json'
 
 <style scoped lang="scss">
 li {
-  margin-bottom: 5px;
+  line-height: 1.6;
 }
+
+li::before {
+  display: inline-block;
+  content: '';
+  -webkit-border-radius: 0.375rem;
+  border-radius: 0.375rem;
+  -moz-border-radius: 0.375rem;
+  height: 0.4rem;
+  width: 0.4rem;
+  margin-right: 0.5rem;
+  background-color: black;
+}
+
 </style>

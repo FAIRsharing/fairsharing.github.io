23c9fc8862f3215ab14f95112a3fb4d182cd41ae
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-22 16:43:09 +0100

    SearchCollection component structure initialized

---
+++ src/components/Records/Record/CollectionRecord/SearchCollection.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row
+    dense
+  >
+    <v-col
+      cols="12"
+      sm="12"
+      md="4"
+      lg="4"
+      xl="4"
+      style="background: gray"
+    >
+      left
+    </v-col>
+    <v-col
+      style="background: darkgray"
+      cols="12"
+      sm="12"
+      md="8"
+      lg="8"
+      xl="8"
+    >
+      right
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SearchCollection"
+}
+</script>
+
+<style scoped>
+
+</style>

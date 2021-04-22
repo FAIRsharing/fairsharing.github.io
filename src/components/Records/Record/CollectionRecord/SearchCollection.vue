8d76965b11081ec9231bf614a37aadc43178dfce
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-22 18:04:58 +0100

    Add more layout design to SearchCollection component

--- src/components/Records/Record/CollectionRecord/SearchCollection.vue
+++ src/components/Records/Record/CollectionRecord/SearchCollection.vue
@@ -13,21 +13,67 @@
       left
     </v-col>
     <v-col
-      style="background: darkgray"
       cols="12"
       sm="12"
       md="8"
       lg="8"
       xl="8"
     >
-      right
+      <!--List Row-->
+      <div :class="['opacity-0-transition',{'opacity-1-transition':!isColumnList}]">
+        <article v-if="!isColumnList">
+          <v-skeleton-loader
+            type="image"
+          >
+            <!-- StackCard view -->
+            <!-- need to add component   -->
+
+            <!--List Controller-->
+            <Pagination
+              :total-pages="5"
+            />
+            <!--    result number        -->
+            <p class="text-center mt-2">
+              Displaying {{ x }}  to {{ y }} of {{ hits }}.
+            </p>
+          </v-skeleton-loader>
+        </article>
+      </div>
+      <!-- ColumnCard view -->
+      <div :class="['opacity-0-transition',{'opacity-1-transition':isColumnList}]">
+        <article v-if="isColumnList">
+          <v-skeleton-loader
+            type="image"
+          >
+            <v-row>
+              <!-- need to add component   -->
+            </v-row>
+            <!--List Controller-->
+            <Pagination
+              :total-pages="5"
+            />
+          </v-skeleton-loader>
+        </article>
+      </div>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import Pagination from "@/components/Records/Search/Header/Pagination";
 export default {
-  name: "SearchCollection"
+  name: "SearchCollection",
+  components: {Pagination},
+  data() {
+    return {
+      isColumnList: false
+    }
+  },
+  methods:{
+    changeListType: function (listType) {
+      this.isColumnList = listType;
+    }
+  }
 }
 </script>
 

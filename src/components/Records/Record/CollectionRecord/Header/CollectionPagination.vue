6d45c304060e056a4b8b7a1fa98ff415ac0b05d1
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-26 18:22:36 +0100

    Pagination full functional

--- src/components/Records/Record/CollectionRecord/Header/CollectionPagination.vue
+++ src/components/Records/Record/CollectionRecord/Header/CollectionPagination.vue
@@ -3,6 +3,7 @@
     v-model="currentPageLocal"
     :length="totalPages"
     :total-visible="($vuetify.breakpoint.mdAndUp) ? 7 : 3"
+    @input="paginate"
   />
 </template>
 
@@ -15,57 +16,56 @@
      * @vue-data {Number} [currentQuery = null] - the current page number
      */
     export default {
-        name: "CollectionPagination",
-        data() {
-            return {
-                allowPaginate: true,
-                disable: false,
-                currentPageLocal:1
-            }
-        },
-        computed: {
-          ...mapState('collectionRecords', ["currentPage","totalPages"]),
-        },
-        watch: {
-          'currentPageLocal': async function (newPage) {
-            await this.paginate(newPage);
+      name: "CollectionPagination",
+      data() {
+        return {
+          allowPaginate: true,
+          disable: false,
+          currentPageLocal: 1
+        }
+      },
+      computed: {
+        ...mapState('collectionRecords', ["currentPage", "totalPages"]),
+      },
+      mounted() {
+        this.$nextTick(() => {
+          this.currentPageLocal = this.currentPage;
+        })
+      },
+      methods: {
+        ...mapActions('collectionRecords', ['paginateRecords', 'fetchRecords']),
+        /**
+         * Set the current query page parameter value to the given input for vueJs router
+         * @param {Number} pageNumber - the page to go to
+         */
+        paginate: async function (pageNumber) {
+          const _module = this;
+          if (this.allowPaginate) {
+            this.disableThrottle(this.disable);
+            await _module.paginateRecords(pageNumber);
+            await _module.fetchRecords();
           }
         },
-        methods: {
-          ...mapActions('collectionRecords', ['paginateRecords','fetchRecords']),
-          /**
-             * Set the current query page parameter value to the given input for vueJs router
-             * @param {Number} pageNumber - the page to go to
-             */
-            paginate: async function (pageNumber) {
-                const _module = this;
-                if (this.allowPaginate) {
-                        this.disableThrottle(this.disable);
-                        await _module.paginateRecords(pageNumber);
-                        await _module.fetchRecords();
-                }
-            },
-            /**
-             * Postpone the pagination buttons to be clickable
-             */
-            PaginatePermission: throttle(function () {
-                this.allowPaginate = true
-            }, 1200),
+        /**
+         * Postpone the pagination buttons to be clickable
+         */
+        PaginatePermission: throttle(function () {
+          this.allowPaginate = true
+        }, 1200),
 
-            /**
-             * Set the environment ready for testing or development
-             * @param {Boolean} disable - should disable or not the throttle for the next call
-             */
-            disableThrottle: function (disable) {
-                if (!disable) {
-                    this.allowPaginate = false;
-                    this.PaginatePermission();
-                }
-                else {
-                    this.allowPaginate = true;
-                }
-            }
-        },
+        /**
+         * Set the environment ready for testing or development
+         * @param {Boolean} disable - should disable or not the throttle for the next call
+         */
+        disableThrottle: function (disable) {
+          if (!disable) {
+            this.allowPaginate = false;
+            this.PaginatePermission();
+          } else {
+            this.allowPaginate = true;
+          }
+        }
+      },
     }
 </script>
 

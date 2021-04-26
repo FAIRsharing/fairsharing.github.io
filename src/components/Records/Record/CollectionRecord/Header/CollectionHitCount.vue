cde99bc6e25904d54a4b793c270f2f02b69b70b7
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-26 14:56:59 +0100

    Add CollectionHit component

---
+++ src/components/Records/Record/CollectionRecord/Header/CollectionHitCount.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    v-if="hits"
+    class="mt-2"
+  >
+    <p class="text-center">
+      Displaying {{ x }}  to {{ y }} of {{ hits }}.
+    </p>
+  </div>
+</template>
+
+<script>
+    import {mapState} from "vuex";
+    export default {
+        name: "CollectionHitCount",
+        computed: {
+          ...mapState("collectionRecords", ["hits", "perPage", "currentPage"]),
+          x: function () {
+            let _module = this;
+            return (_module.perPage * (_module.currentPage -1)) + 1;
+          },
+          y: function () {
+            let _module = this;
+            let max = ((_module.perPage * _module.currentPage) -1) + 1;
+            if (max > _module.hits) {
+              max = _module.hits;
+            }
+            return max;
+          }
+        }
+    }
+</script>

190bfca65545cdd2e2c268b9a88137a930475afd
ramGranell <ragrator@gmail.com>
2021-04-28 11:10:58 +0100

    adding suggested changes

--- src/components/Static/Statistics/BarChart.vue
+++ src/components/Static/Statistics/BarChart.vue
@@ -12,7 +12,6 @@
     import Highcharts from 'highcharts'
 
     const colourPalete = ['#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5'];
-    //['#50B432', '#ED561B', '#24CBE5', '#DDDF00', '#64E572', '#FF9655', '#FFF263',      '#6AF9C4']
     Highcharts.setOptions({
       lang: {
         thousandsSep: ','

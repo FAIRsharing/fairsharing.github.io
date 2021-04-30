1c5bd0b482ef2715491398a53c37bfc13c453d6f
Hossein Mirian <hosein.mirian@gmail.com>
2021-04-30 15:27:40 +0100

    Initialize searchCollection test

--- tests/unit/components/Records/Record/CollectionRecord/searchCollection.spec.js
+++ tests/unit/components/Records/Record/CollectionRecord/searchCollection.spec.js
@@ -2,24 +2,60 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import searchCollection from "@/components/Records/Record/CollectionRecord/SearchCollection"
 import Vuetify from "vuetify"
+import VueScrollTo from "vue-scrollto";
+import records from "@/store/recordSearch";
+import record from "@/store/recordData";
+import introspection from "@/store/introspector";
+import uiController from "@/store/uiController";
+import Client from "@/lib/GraphClient/GraphClient";
+import sinon from "sinon";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueScrollTo,{})
 const vuetify = new Vuetify();
 
+const $route = {
+    name: "Standards",
+    path: "standard",
+    query: {
+        fairsharingRegistry: "Standard",
+    }
+};
+const $store = new Vuex.Store({
+    modules: {
+        record: record,
+        records: records,
+        introspection: introspection,
+        uiController: uiController
+    },
+});
 
-describe("searchCollection.vue", function(){
-    let wrapper;
 
-    beforeEach(() => {
-        wrapper = shallowMount(searchCollection, {
+describe("SearchCollection.vue", function(){
+
+    let stub = sinon.stub(Client.prototype, "executeQuery");
+
+    beforeAll(() => {
+        stub.withArgs(sinon.match.object).returns({searchFairsharingRecords: {records: [1]}});
+    });
+
+    afterAll(() => {
+        Client.prototype.executeQuery.restore();
+    });
+
+    beforeEach(async () => {
+        wrapper = await shallowMount(searchCollection, {
+            mocks: {$route, $store},
             localVue,
             vuetify
         });
     });
 
+    let wrapper;
+
     it("can be instantiated", () => {
-        expect(wrapper.name()).toMatch("searchCollection");
+        expect(wrapper.name()).toMatch("SearchCollection");
     });
 
 });

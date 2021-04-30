b6355a482a15dc353aecfb35f8be2a7a5ad7501a
Terazus <batistadominique@hotmail.com>
2021-04-30 05:41:51 -0700

    TombStone page active

---
+++ src/views/Errors/Tombstone.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-alert type="warning">
+      This record does not exist anymore: {{ record.name }}.
+      The record with identifier <b>{{ record.doi }}</b> was invalid.
+    </v-alert>
+    <b class="pl-3">Description: </b> {{ record.description }}
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Tombstone",
+        props: {
+            record: {default: null, type: Object}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

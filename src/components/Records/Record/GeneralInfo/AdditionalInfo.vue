d0c9930316034052d8a66c1c59f4b8e1b7761d75
Hossein Mirian <hosein.mirian@gmail.com>
2021-05-13 17:14:57 +0100

    Dynamically checked available additional fields in AdditionalInfo component

--- src/components/Records/Record/GeneralInfo/AdditionalInfo.vue
+++ src/components/Records/Record/GeneralInfo/AdditionalInfo.vue
@@ -6,10 +6,13 @@
     tile
     elevation="3"
   >
-    <!-- General Info -->
-    <SectionTitle title="Additional Information" />
+    <!-- Additional Information -->
+    <SectionTitle
+      title="Additional Information"
+      :inactive-section="allowedFields.properties===undefined || allowedFields.properties===null"
+    />
     <div
-      v-if="getField('metadata').dataset_pid"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_pid"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset pid</b>
@@ -21,7 +24,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_citation"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_citation"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Citation</b>
@@ -33,7 +36,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_contact"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_contact"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Contact</b>
@@ -45,7 +48,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_versioning"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_contact"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Versioning</b>
@@ -57,7 +60,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_prepubreview"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_prepubreview"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Pre-Publication review</b>
@@ -69,7 +72,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').deprecation_reason"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.deprecation_reason"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Deprecation reason</b>
@@ -81,7 +84,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_preservation"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_preservation"
       class="d-flex flex-row mt-4 align-center min-height-40"
     >
       <b class="width-200">Dataset Preservation</b>
@@ -97,7 +100,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_curation"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_curation"
       class="pa-4 data-holder"
     >
       <b class="text-h6">Dataset Curation</b>
@@ -124,7 +127,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_metrics"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_metrics"
       class="pa-4 mt-4 data-holder"
     >
       <b class="text-h6">Dataset Metrics</b>
@@ -151,7 +154,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_deposition"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_deposition"
       class="pa-4 mt-4 data-holder"
     >
       <b class="text-h6">Dataset Deposition</b>
@@ -178,7 +181,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').data_access_condition"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.data_access_condition"
       class="pa-4 mt-4 data-holder"
     >
       <b class="text-h6">Data Access Condition</b>
@@ -205,7 +208,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').dataset_sustainability"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_sustainability"
       class="pa-4 mt-4 data-holder"
     >
       <b class="text-h6">Dataset Sustainability</b>
@@ -232,7 +235,7 @@
     </div>
 
     <div
-      v-if="getField('metadata').community_certification"
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.community_certification"
       class="pa-4 mt-4 data-holder"
     >
       <b class="text-h6">Community Certification</b>
@@ -257,11 +260,79 @@
         </div>
       </div>
     </div>
+
+    <div
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.community_certification"
+      class="pa-4 mt-4 data-holder"
+    >
+      <b class="text-h6">Community Certification</b>
+      <div class="d-flex flex-row align-center min-height-40">
+        <b class="width-200">URL</b>
+        <div class="d-flex full-width ml-md-12 ml-13">
+          <a
+            class="underline-effect"
+            :href="getField('metadata').community_certification.url"
+            target="_blank"
+          >
+            {{ getField('metadata').community_certification.url }}
+          </a>
+        </div>
+      </div>
+      <div class="d-flex flex-row align-center min-height-40">
+        <b class="width-200">Type</b>
+        <div class="d-flex full-width ml-md-12 ml-13">
+          <p class="ma-0">
+            {{ getField('metadata').community_certification.type }}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.cross_references"
+      class="pa-4 mt-4 data-holder"
+    >
+      <b class="text-h6">Cross References</b>
+      <div
+        v-for="(crossRefObj,index) in getField('metadata').cross_references"
+        :key="crossRefObj+''+index"
+      >
+        <div class="d-flex flex-row align-center min-height-40">
+          <b class="width-200">Name</b>
+          <div class="d-flex full-width ml-md-12 ml-13">
+            <p class="ma-0">
+              {{ crossRefObj.name }}
+            </p>
+          </div>
+        </div>
+        <div class="d-flex flex-row align-center min-height-40">
+          <b class="width-200">Portal</b>
+          <div class="d-flex full-width ml-md-12 ml-13">
+            <p class="ma-0">
+              {{ crossRefObj.portal }}
+            </p>
+          </div>
+        </div>
+        <div class="d-flex flex-row align-center min-height-40">
+          <b class="width-200">URL</b>
+          <div class="d-flex full-width ml-md-12 ml-13">
+            <a
+              class="underline-effect"
+              :href="crossRefObj.url"
+              target="_blank"
+            >
+              {{ crossRefObj.url }}
+            </a>
+          </div>
+        </div>
+        <v-divider v-if="getField('metadata').cross_references.length-1!==index" />
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
-import {mapGetters, mapState} from "vuex";
+import {mapActions, mapGetters, mapState} from "vuex";
 import SectionTitle from "@/components/Records/Record/SectionTitle";
 
 export default {
@@ -269,8 +340,31 @@ export default {
   components: {SectionTitle},
   computed: {
     ...mapState('record', ["currentRecord"]),
-    ...mapGetters("record", ["getField"]),
-  }
+    ...mapState('editor', ["allowedFields"]),
+    ...mapGetters("record", ["getField",'getRecordType']),
+    ...mapState('users', ['user']),
+    userToken() {
+      const _module = this;
+      return (_module.user().credentials) ? _module.user().credentials.token : null;
+    }
+  },
+  created() {
+
+  },
+  async mounted() {
+    this.$nextTick(async () => {
+      let userToken = this.userToken;
+      if (userToken) {
+        await this.getAllowedFields({
+          type: this.getRecordType,
+          token: userToken
+        })
+      }
+    })
+  },
+  methods: {
+    ...mapActions("editor", ["getAllowedFields"]),
+  },
 }
 </script>
 

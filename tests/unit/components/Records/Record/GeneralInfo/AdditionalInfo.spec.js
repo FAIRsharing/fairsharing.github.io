3aa4de14c5ad7795132c4989203e3185dbd61fe1
Hossein Mirian <hosein.mirian@gmail.com>
2021-05-17 12:41:07 +0100

    Add test for AdditionalInfo component

--- tests/unit/components/Records/Record/GeneralInfo/AdditionalInfo.spec.js
+++ tests/unit/components/Records/Record/GeneralInfo/AdditionalInfo.spec.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 import Record from "@/store/recordData.js"
 import AdditionalInfo from "@/components/Records/Record/GeneralInfo/AdditionalInfo"
 import Vuetify from "vuetify"
+import editorStore from "@/store/editor";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -10,12 +11,12 @@ const vuetify = new Vuetify();
 
 const $store = new Vuex.Store({
     modules: {
-        record:Record
+        record:Record,
+        editor: editorStore
     }});
 
 describe("AdditionalInfo.vue", function(){
     let wrapper;
-
     beforeEach(() => {
         wrapper = shallowMount(AdditionalInfo, {
             localVue,
@@ -26,6 +27,16 @@ describe("AdditionalInfo.vue", function(){
 
     it("can be initiated", () => {
         expect(wrapper.name()).toMatch("AdditionalInfo");
+    });
+
+    it("can check if data is available", () => {
+        let selectedNode = {item:[1,2]}
+        let retVal = wrapper.vm.checkDataAvailable(selectedNode)
+        expect(retVal).toEqual(true);
+        selectedNode = {item:[]}
+        wrapper.vm.checkDataAvailable(selectedNode)
+        retVal = wrapper.vm.checkDataAvailable(selectedNode)
+        expect(retVal).toEqual(false);
     });
 
 });

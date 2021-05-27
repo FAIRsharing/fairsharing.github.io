a7b6cfe03cacdd24e90c1494ce23d6053cbe3868
Hossein Mirian <hosein.mirian@gmail.com>
2021-05-27 14:46:39 +0100

    Refactor some part of additionalInfo

--- src/components/Records/Record/GeneralInfo/AdditionalInfo.vue
+++ src/components/Records/Record/GeneralInfo/AdditionalInfo.vue
@@ -9,77 +9,18 @@
   >
     <!-- Additional Information -->
     <SectionTitle title="Additional Information" />
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_pid && getField('metadata').dataset_pid==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset pid</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').dataset_pid }}
-        </p>
-      </div>
-    </div>
-
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_citation && getField('metadata').dataset_citation==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset Citation</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').dataset_citation }}
-        </p>
-      </div>
-    </div>
-
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_contacts && getField('metadata').dataset_contacts==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset Contacts</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').dataset_contacts }}
-        </p>
-      </div>
-    </div>
-
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_versioning && getField('metadata').dataset_versioning==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset Versioning</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').dataset_versioning }}
-        </p>
-      </div>
-    </div>
-
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_prepubreview && getField('metadata').dataset_prepubreview==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset Pre-Publication review</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').dataset_prepubreview }}
-        </p>
-      </div>
-    </div>
-
-    <div
-      v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.deprecation_reason && getField('metadata').deprecation_reason==='yes'"
-      class="d-flex flex-row mt-4 align-center min-height-40"
-    >
-      <b class="width-200">Dataset Deprecation reason</b>
-      <div class="d-flex full-width ml-md-12 ml-13">
-        <p class="ma-0">
-          {{ getField('metadata').deprecation_reason }}
-        </p>
-      </div>
-    </div>
+    <!--  dataset_pid  -->
+    <DatasetBoolean field-name="dataset_pid" />
+    <!--  dataset_citation  -->
+    <DatasetBoolean field-name="dataset_citation" />
+    <!--  dataset_contacts  -->
+    <DatasetBoolean field-name="dataset_contacts" />
+    <!--  dataset_versioning  -->
+    <DatasetBoolean field-name="dataset_versioning" />
+    <!--  dataset_versioning  -->
+    <DatasetBoolean field-name="dataset_prepubreview" />
+    <!--  dataset_versioning  -->
+    <DatasetBoolean field-name="deprecation_reason" />
 
     <div
       v-if="Object.keys(allowedFields).includes('properties') && allowedFields.properties.dataset_preservation && (getField('metadata').dataset_preservation!=='' && getField('metadata').dataset_preservation!==null && getField('metadata').dataset_preservation!==undefined)"
@@ -376,10 +317,11 @@
 <script>
 import {mapActions, mapGetters, mapState} from "vuex";
 import SectionTitle from "@/components/Records/Record/SectionTitle";
+import DatasetBoolean from "@/components/Records/Record/GeneralInfo/AdditionalInfo/Dataset_Boolean";
 
 export default {
   name: "AdditionalInfo",
-  components: {SectionTitle},
+  components: {DatasetBoolean, SectionTitle},
   data: () => {
     return {
       anyDataAvailable:[]
@@ -396,8 +338,8 @@ export default {
         type: this.getRecordType
       });
     })
-    const itemsArray = ['dataset_preservation','dataset_curation','dataset_metrics','dataset_deposition','data_access_condition','dataset_sustainability','community_certification','cos_top_guidelines','cross_references']
-    for (const key of itemsArray) {
+    const allAllowedTypes = ['dataset_preservation','dataset_curation','dataset_metrics','dataset_deposition','data_access_condition','dataset_sustainability','community_certification','cos_top_guidelines','cross_references']
+    for (const key of allAllowedTypes) {
       if (Object.prototype.hasOwnProperty.call(this.getField('metadata'), key)) {
         await this.checkDataAvailable(this.getField('metadata')[key]);
       }

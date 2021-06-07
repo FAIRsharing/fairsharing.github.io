c3614fdda3afcdbfb637c1db136d00fbc41ff979
Hossein Mirian <hosein.mirian@gmail.com>
2021-06-07 12:51:33 +0100

    Reorder cv list in generalInfo

--- src/components/Records/Record/GeneralInfo/Keywords.vue
+++ src/components/Records/Record/GeneralInfo/Keywords.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
+    <!--Domains-->
+    <div class="d-flex flex-row mt-4 min-height-40">
+      <b class="width-200">Domains</b>
+      <div class="d-flex full-width flex-wrap ml-md-12 ml-13">
+        <span v-if="!getField('domains').length">
+          None
+        </span>
+        <v-chip
+            v-for="item in getField('domains')"
+            :key="item.label"
+            class="mr-2 mb-2"
+            text-color="white"
+            :color="getChipColor(item)"
+        >
+          <KeywordTooltip
+              :keyword="item"
+              :full-width="true"
+          />
+        </v-chip>
+      </div>
+    </div>
+    <!--Subjects-->
+    <div class="d-flex flex-row mt-4 min-height-40">
+      <b class="width-200">Subjects</b>
+      <div class="d-flex full-width flex-wrap ml-md-12 ml-13">
+        <span v-if="!getField('subjects').length">
+          None
+        </span>
+        <v-chip
+            v-for="item in getField('subjects')"
+            :key="item.label"
+            class="mr-2 mb-2"
+            :color="getChipColor(item)"
+            text-color="white"
+        >
+          <KeywordTooltip
+              :keyword="item"
+              :full-width="true"
+          />
+        </v-chip>
+      </div>
+    </div>
     <!--Taxonomic Range-->
     <div class="d-flex flex-row mt-4 min-height-40">
       <b class="width-200">Taxonomic Range</b>
@@ -13,48 +55,6 @@
           class="mr-2 mb-2"
           :color="getChipColor(item)"
           text-color="white"
-        >
-          <KeywordTooltip
-            :keyword="item"
-            :full-width="true"
-          />
-        </v-chip>
-      </div>
-    </div>
-    <!--Subjects-->
-    <div class="d-flex flex-row mt-4 min-height-40">
-      <b class="width-200">Subjects</b>
-      <div class="d-flex full-width flex-wrap ml-md-12 ml-13">
-        <span v-if="!getField('subjects').length">
-          None
-        </span>
-        <v-chip
-          v-for="item in getField('subjects')"
-          :key="item.label"
-          class="mr-2 mb-2"
-          :color="getChipColor(item)"
-          text-color="white"
-        >
-          <KeywordTooltip
-            :keyword="item"
-            :full-width="true"
-          />
-        </v-chip>
-      </div>
-    </div>
-    <!--Domains-->
-    <div class="d-flex flex-row mt-4 min-height-40">
-      <b class="width-200">Domains</b>
-      <div class="d-flex full-width flex-wrap ml-md-12 ml-13">
-        <span v-if="!getField('domains').length">
-          None
-        </span>
-        <v-chip
-          v-for="item in getField('domains')"
-          :key="item.label"
-          class="mr-2 mb-2"
-          text-color="white"
-          :color="getChipColor(item)"
         >
           <KeywordTooltip
             :keyword="item"

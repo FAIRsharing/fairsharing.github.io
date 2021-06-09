65aa1ab9f405f874520b99f4460f4be5651a6477
Milo Thurston <knirirr@gmail.com>
2021-06-09 16:52:57 +0100

    Added countries list #917.

--- src/views/Organisations/Organisation.vue
+++ src/views/Organisations/Organisation.vue
@@ -4,7 +4,7 @@
     fluid
     class="standard grey lighten-3 pb-10"
   >
-    <div v-if="error">
+    <div v-if="error && !loading">
       <NotFound />
     </div>
     <v-row v-else>
@@ -159,6 +159,36 @@
                           >
                             {{ child.name }}
                           </router-link>
+                        </li>
+                      </ul>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <!-- countries -->
+                  <v-list-item
+                    :key="'organisation_countries'"
+                    :v-if="organisation.countries"
+                    class="body-1"
+                  >
+                    <v-list-item-content
+                      class="py-0 d-block"
+                    >
+                      <b class="blue--text">Associated countries: </b>
+                      <ul
+                        v-for="(country,index) in organisation.countries"
+                        :key="country.id"
+                      >
+                        <li
+                          v-if="country.name"
+                          class="ma-0 mr-2"
+                        >
+                          {{ ` ${country.name}${index!==organisation.countries.length-1?',':''}` }}
+                        </li>
+                        <li
+                          v-else
+                          class="warning"
+                        >
+                          country code undefined!
                         </li>
                       </ul>
                     </v-list-item-content>

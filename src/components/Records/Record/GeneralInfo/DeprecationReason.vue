b199671d1c925c12be93c1ca1a80ee86429e1156
Hossein Mirian <hosein.mirian@gmail.com>
2021-06-11 10:37:15 +0100

    Add deprecation reason in generalInfo

---
+++ src/components/Records/Record/GeneralInfo/DeprecationReason.vue
@@ -0,0 +1,23 @@
+<template>
+  <p
+    v-if="currentRecord.fairsharingRecord['deprecationReason']"
+    class="ml-2 mt-4 pb-0 red--text"
+  >
+    {{ currentRecord.fairsharingRecord["deprecationReason"] }}
+  </p>
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: "DeprecationReason",
+  computed: {
+    ...mapState('record', ["currentRecord"]),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

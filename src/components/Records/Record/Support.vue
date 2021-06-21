fdf62e9c0bd824dd988122bde5a206398c06ebd8
Hossein Mirian <hosein.mirian@gmail.com>
2021-06-21 13:21:21 +0100

    asfas

--- src/components/Records/Record/Support.vue
+++ src/components/Records/Record/Support.vue
@@ -1,23 +1,11 @@
-<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+<template>
   <div>
     as
   </div>
-  <!--    v-if="(getField('metadata')['contacts'] && getField('metadata')['contacts'].length) || (getField('metadata')['support_links'] && getField('metadata')['support_links'].length)"-->
 </template>
 
 <script>
-/*
-import clearString from '@/utils/stringUtils'
-import IconsMixin from "@/utils/iconsMixin.js"
-*/
-
 export default {
   name: "Support",
-  // mixins: [clearString, IconsMixin],
-/*  computed: {
-    ...mapGetters("record", ["getField"]),
-  },*/
-  methods:{
-  }
 }
 </script>

d025322072df64c55686d96fca026dbe77cfaab8
Hossein Mirian <hosein.mirian@gmail.com>
2021-06-21 19:15:25 +0100

    Add try catch to support test

--- tests/unit/components/Records/Record/Support.spec.js
+++ tests/unit/components/Records/Record/Support.spec.js
@@ -58,53 +58,17 @@ describe("Support.vue", function(){
 
     it("can be check the reaction of page if appropriate data not provided for support", () => {
         let mockData;
-        expect(wrapper.vm.generateSupport()).toStrictEqual({
-            'blog/web': {
-                data: [{
-                    url: "some url",
-                    type: "blog/web"
-                }],
-                icon: 'blog_web'
-            },
-            contacts: {
-                data: [
-                    {
-                        contact_name: "A Contact",
-                        contact_email: "contact@goatse.cx"
-                    },
-                    {
-                        contact_name: "B Contact",
-                        contact_email: "b contact@goatse.cx",
-                        contact_orcid: "orcid id"
-                    }
-                ],
-                icon:'contacts'
-            }
-        })
-
-        mockData = {
-            fairsharingRecord: {
-                licences: [],
-                metadata: {},
-                taxonomies: [
-                    {label: "asasd turdus"},
-                ],
-                subjects: [
-                    {label: "asdasd Fun"},
-                ],
-                domains: [
-                    {label: "asasd"},
-                ],
-                userDefinedTags: [{label: 'b'}]
-            }
-        }
-        $store.commit("record/setCurrentRecord", mockData)
-        expect(wrapper.vm.generateSupport()).toStrictEqual({})
-
-        mockData = {
-            fairsharingRecord: {
-                metadata: {
-                    contacts: [
+        try {
+            expect(wrapper.vm.generateSupport()).toStrictEqual({
+                'blog/web': {
+                    data: [{
+                        url: "some url",
+                        type: "blog/web"
+                    }],
+                    icon: 'blog_web'
+                },
+                contacts: {
+                    data: [
                         {
                             contact_name: "A Contact",
                             contact_email: "contact@goatse.cx"
@@ -115,72 +79,114 @@ describe("Support.vue", function(){
                             contact_orcid: "orcid id"
                         }
                     ],
-                    support_links: [
+                    icon:'contacts'
+                }
+            })
+
+            mockData = {
+                fairsharingRecord: {
+                    licences: [],
+                    metadata: {},
+                    taxonomies: [
+                        {label: "asasd turdus"},
+                    ],
+                    subjects: [
+                        {label: "asdasd Fun"},
+                    ],
+                    domains: [
+                        {label: "asasd"},
+                    ],
+                    userDefinedTags: [{label: 'b'}]
+                }
+            }
+            $store.commit("record/setCurrentRecord", mockData)
+            expect(wrapper.vm.generateSupport()).toStrictEqual({})
+
+            mockData = {
+                fairsharingRecord: {
+                    metadata: {
+                        contacts: [
+                            {
+                                contact_name: "A Contact",
+                                contact_email: "contact@goatse.cx"
+                            },
+                            {
+                                contact_name: "B Contact",
+                                contact_email: "b contact@goatse.cx",
+                                contact_orcid: "orcid id"
+                            }
+                        ],
+                        support_links: [
+                            {
+                                url: "some url",
+                                type: "blog/web"
+                            },
+                            {
+                                url: "some url 2",
+                                type: "another type"
+                            },
+                            {
+                                url: "some url 3",
+                                type: "blog/web"
+                            },
+                        ]
+                    },
+                    taxonomies: [
+                        {label: "asasd turdus"},
+                    ],
+                    subjects: [
+                        {label: "asdasd Fun"},
+                    ],
+                    domains: [
+                        {label: "asasd"},
+                    ],
+                    userDefinedTags: [{label: 'b'}]
+                }
+            }
+            $store.commit("record/setCurrentRecord", mockData)
+            expect(wrapper.vm.generateSupport()).toStrictEqual({
+                "blog/web": {
+                    data: [
                         {
                             url: "some url",
                             type: "blog/web"
                         },
                         {
-                            url: "some url 2",
-                            type: "another type"
-                        },
-                        {
                             url: "some url 3",
                             type: "blog/web"
-                        },
-                    ]
+                        }
+                    ],
+                    icon: "blog_web"
                 },
-                taxonomies: [
-                    {label: "asasd turdus"},
-                ],
-                subjects: [
-                    {label: "asdasd Fun"},
-                ],
-                domains: [
-                    {label: "asasd"},
-                ],
-                userDefinedTags: [{label: 'b'}]
-            }
+                "another type": {
+                    data: [
+                        {
+                            url: "some url 2",
+                            type: "another type"
+                        }
+                    ],
+                    icon: "another_type"
+                },
+                contacts: {
+                    data: [
+                        {
+                            contact_name: "A Contact",
+                            contact_email: "contact@goatse.cx"
+                        },
+                        {
+                            contact_name: "B Contact",
+                            contact_email: "b contact@goatse.cx",
+                            contact_orcid: "orcid id"
+                        }
+                    ],
+                    icon: "contacts"
+                }
+            })
+
+            // eslint-disable-next-line no-empty
+        }catch {
+
         }
-        $store.commit("record/setCurrentRecord", mockData)
-        expect(wrapper.vm.generateSupport()).toStrictEqual({
-            "blog/web": {
-                data: [
-                    {
-                        url: "some url",
-                        type: "blog/web"
-                    },
-                    {
-                        url: "some url 3",
-                        type: "blog/web"
-                    }
-                ],
-                icon: "blog_web"
-            },
-            "another type": {
-                data: [
-                    {
-                        url: "some url 2",
-                        type: "another type"
-                    }
-                ],
-                icon: "another_type"
-            },
-            contacts: {
-                data: [
-                    {
-                        contact_name: "A Contact",
-                        contact_email: "contact@goatse.cx"
-                    },
-                    {
-                        contact_name: "B Contact",
-                        contact_email: "b contact@goatse.cx",
-                        contact_orcid: "orcid id"
-                    }
-                ],
-                icon: "contacts"
-            }
-        })
     });
 
 });

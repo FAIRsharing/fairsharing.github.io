403d64ccdd1e6a9ba0b0698ca7850ea804694fef
Hossein Mirian <hosein.mirian@gmail.com>
2021-06-22 13:26:03 +0100

    Initialize Community page

--- src/views/Static/Community/Community.vue
+++ src/views/Static/Community/Community.vue
@@ -1,5 +1,10 @@
 <template>
-  <h2>Community</h2>
+  <main class="pa-15 mb-10">
+    <!--  main_title -->
+    <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      FAIRsharing is a community-driven resource with a growing number of users, adopters, collaborators and activities, all working to enable the FAIR Principles and to make Standards, Knowledgebases, Repositories and Data Policies FAIR.
+    </h1>
+  </main>
 </template>
 
 <script>

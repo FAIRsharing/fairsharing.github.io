eb1f8a960232631d61c9437527d74cdd7ce79be4
Milo Thurston <knirirr@gmail.com>
2021-06-25 16:46:28 +0100

    Completed redirections for #1099.

--- src/router/index.js
+++ src/router/index.js
@@ -86,6 +86,21 @@ let routes = [
             }
         }
     },
+    {
+        name: 'ontology',
+        path: '/ontology/:name',
+        redirect: to => {
+            if (to.params.name.toLowerCase() === 'srao') {
+                return { path: 'https://github.com/FAIRsharing/subject-ontology' }
+            }
+            else if (to.params.name.toLowerCase() === 'drao') {
+                return { path: 'https://github.com/FAIRsharing/domain-ontology' }
+            }
+            else {
+                return { path: '/' }
+            }
+        }
+    },
 
 
 

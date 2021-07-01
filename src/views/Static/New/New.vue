315f3659108653fe741615db021b9ae8efeca7e0
Hossein Mirian <hosein.mirian@gmail.com>
2021-07-01 12:40:06 +0100

    Add test for New component

--- src/views/Static/New/New.vue
+++ src/views/Static/New/New.vue
@@ -4,14 +4,14 @@
     <!--  Header  -->
     <h1
       class="text-h5 text-xl-h4 mb-2 mb-xl-6"
-      v-html="newData.header"
+      v-html="$sanitize(newData.header)"
     />
     <!--  subtitle  -->
     <p
       :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-      v-html="newData.firstDescription"
+      v-html="$sanitize(newData.firstDescription)"
     />
-    <!--    -->
+    <!--  BulletPoints  -->
     <div
       v-for="(item,index) in newData.description"
       :key="item.header+'_'+index"
@@ -25,20 +25,20 @@
       <p
         v-if="item.textBefore"
         :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="item.textBefore"
+        v-html="$sanitize(item.textBefore)"
       />
       <ul v-if="item.bulletPoints">
         <li
           v-for="(bulletPoint,bulletPoint_index) in item.bulletPoints"
           :key="bulletPoint_index"
           class="mb-2"
-          v-html="bulletPoint"
+          v-html="$sanitize(bulletPoint)"
         />
       </ul>
       <p
         v-if="item.textAfter"
         :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="item.textAfter"
+        v-html="$sanitize(item.textAfter)"
       />
     </div>
     <v-btn

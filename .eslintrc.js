79a5afbe1d037f5be3ac2fd5095abb023cd0083b
Milo Thurston <knirirr@gmail.com>
2021-07-26 14:10:31 +0100

    Trying to disable a specific line rather than modify .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === "production" ? "error" : "off",
-    'no-debugger': process.env.NODE_ENV === "production" ? "error" : "off",
-    'vue/valid-v-slot': ['error', { allowModifiers: true, }],
+    'no-debugger': process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parser: "vue-eslint-parser",
   parserOptions: {

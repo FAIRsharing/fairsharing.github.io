987eb31b341d989ae59e6a57ed80cbe4e8fb7c55
Hossein Mirian <hosein.mirian@gmail.com>
2021-07-27 15:50:48 +0100

    Add logos component for home page

---
+++ src/components/Home/CommunityCarousel.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="d-flex flex-column align-center">
+    <p class="lato-text-lg ma-0 text-body">
+      Our Community of Adopters and Collaborators
+    </p>
+    <v-container>
+      <v-carousel
+        v-model="model"
+        cycle
+        height="300"
+        hide-delimiter-background
+      >
+        <v-carousel-item
+          v-for="(logo, index) in logos"
+          :key="logo.image+'_'+index"
+        >
+          <v-sheet
+            color="white"
+            height="100%"
+          >
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-img
+                contain
+                :src="logo.image"
+                width="100"
+                height="100"
+              />
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import {blockLogos} from '@/data/homePageData.json'
+export default {
+  name: "CommunityCarousel",
+  data: () => ({
+    model: 0,
+    logos: blockLogos
+  })
+}
+</script>
+
+<style scoped>
+
+</style>

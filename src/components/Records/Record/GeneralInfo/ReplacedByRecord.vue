d2d0b920713e953cde6b066531d878da0109f886
hoseinmirian <hosein.mirian@gmail.com>
2021-08-20 13:00:21 +0100

    Apply Milo's comment, organized the code for better readablility

--- src/components/Records/Record/GeneralInfo/ReplacedByRecord.vue
+++ src/components/Records/Record/GeneralInfo/ReplacedByRecord.vue
@@ -1,23 +1,23 @@
 <template>
   <div
-    v-if="(!!currentRecord['fairsharingRecord'].reverseRecordAssociations.find(item=>item.recordAssocLabel==='deprecates') && currentRecord['fairsharingRecord'].reverseRecordAssociations) || (!!currentRecord['fairsharingRecord'].recordAssociations.find(item=>item.recordAssocLabel==='deprecates' && currentRecord['fairsharingRecord'].recordAssociations))"
+    v-if="anyAssociationExist"
     class="d-flex flex-row mt-4 min-height-40"
   >
     <b
-      v-if="!!currentRecord['fairsharingRecord'].reverseRecordAssociations.find(item=>item.recordAssocLabel==='deprecates')"
+      v-if="reverseRecordAssociationExist"
       class="width-200"
     >This record is replaced by: </b>
     <b
-      v-if="!!currentRecord['fairsharingRecord'].recordAssociations.find(item=>item.recordAssocLabel==='deprecates')"
+      v-if="recordAssociationExist"
       class="width-200"
     > This record replaces or incorporates the following deprecated resources: </b>
     
     <div
-      v-if="!!currentRecord['fairsharingRecord'].reverseRecordAssociations.find(item=>item.recordAssocLabel==='deprecates')"
+      v-if="reverseRecordAssociationExist"
       class="d-flex full-width flex-column flex-wrap ml-md-14 ml-13"
     >
       <router-link
-        v-for="(item,index) in currentRecord['fairsharingRecord'].reverseRecordAssociations.filter(obj=>obj.recordAssocLabel==='deprecates')"
+        v-for="(item,index) in getAllReverseRecordAssociations"
         :key="item.id+'_'+index"
         class="underline-effect"
         :to="'/'+item.fairsharingRecord.id"
@@ -27,11 +27,11 @@
     </div>
 
     <div
-      v-if="!!currentRecord['fairsharingRecord'].recordAssociations.find(item=>item.recordAssocLabel==='deprecates')"
+      v-if="recordAssociationExist"
       class="d-flex full-width flex-column flex-wrap ml-md-14 ml-13"
     >
       <router-link
-        v-for="(item,index) in currentRecord['fairsharingRecord'].recordAssociations.filter(obj=>obj.recordAssocLabel==='deprecates')"
+        v-for="(item,index) in getAllRecordAssociations"
         :key="item.id+'_'+index"
         class="underline-effect"
         :to="'/'+item.linkedRecord.id"
@@ -49,10 +49,23 @@ export default {
   name: "ReplacedByRecord",
   computed: {
     ...mapState("record", ["currentRecord"]),
+    anyAssociationExist() {
+      return (!!this.currentRecord['fairsharingRecord'].reverseRecordAssociations.find(item => item.recordAssocLabel === 'deprecates') && !!this.currentRecord['fairsharingRecord'].reverseRecordAssociations)
+          ||
+          (!!this.currentRecord['fairsharingRecord'].recordAssociations.find(item => item.recordAssocLabel === 'deprecates' && !!this.currentRecord['fairsharingRecord'].recordAssociations))
+    },
+    recordAssociationExist() {
+      return !!this.currentRecord['fairsharingRecord'].recordAssociations.find(item=>item.recordAssocLabel==='deprecates')
+    },
+    reverseRecordAssociationExist() {
+      return !!this.currentRecord['fairsharingRecord'].reverseRecordAssociations.find(item=>item.recordAssocLabel==='deprecates')
+    },
+    getAllRecordAssociations() {
+      return this.currentRecord['fairsharingRecord'].recordAssociations.filter(obj=>obj.recordAssocLabel==='deprecates')
+    },
+    getAllReverseRecordAssociations() {
+      return this.currentRecord['fairsharingRecord'].reverseRecordAssociations.filter(obj=>obj.recordAssocLabel==='deprecates')
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>

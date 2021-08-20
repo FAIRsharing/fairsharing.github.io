099e093ca9e6070034bde122fd5b8aec1b60b107
hoseinmirian <hosein.mirian@gmail.com>
2021-08-20 13:17:48 +0100

    Detached pagination of being coupled with records currentPage and replaced it by props for better usability

--- src/components/Records/Search/Header/ListController.vue
+++ src/components/Records/Search/Header/ListController.vue
@@ -48,7 +48,10 @@
         width="100%"
         type="list-item"
       >
-        <Pagination :total-pages="totalPages" />
+        <Pagination
+          :total-pages="totalPages"
+          :current-page="currentPage"
+        />
       </v-skeleton-loader>
       <div
         v-else
@@ -137,7 +140,7 @@
             }
         },
         computed: {
-            ...mapState('records', ["totalPages", "loading"]),
+            ...mapState('records', ["totalPages", "loading","currentPage"]),
         },
         methods: {
             changeListType: function (listType) {

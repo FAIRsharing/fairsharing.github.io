6c066304aee82c299ce8d962893e5a832c6df60d
hoseinmirian <hosein.mirian@gmail.com>
2021-09-07 12:05:37 +0100

    Add moment to make date human-friendly

--- src/components/Global/PublicMessages.vue
+++ src/components/Global/PublicMessages.vue
@@ -1,27 +1,33 @@
 <template>
   <div
     v-if="publicMessages.length && !loading"
-    class="my-2"
+    class="my-1"
   >
     <v-alert
       v-for="(messageObj,index) in publicMessages"
       :key="messageObj.message+'_'+index"
       dense
       type="warning"
-      class="mb-2 mx-2 flex-grow-1"
+      class="mb-0 mx-2 flex-grow-1"
     >
-      {{ `${messageObj.updatedAt}: ${messageObj.message} ` }}
+      {{ `${moment(messageObj.updatedAt)}: ${messageObj.message} ` }}
     </v-alert>
   </div>
 </template>
 
 <script>
 import {mapState} from "vuex";
+import moment from "moment";
 
 export default {
   name: "PublicMessages",
   computed: {
-    ...mapState('messages', ["publicMessages","loading"])
+    ...mapState('messages', ["publicMessages", "loading"])
+  },
+  methods: {
+    moment(date) {
+      return moment(date).format('dddd, MMMM Do YYYY, H:mm');
+    }
   }
 }
 </script>

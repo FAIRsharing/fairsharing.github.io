0e1e04aa1834f0c40f147f863d34b82b7cd81c69
hoseinmirian <hosein.mirian@gmail.com>
2021-09-08 11:01:41 +0100

    Search botton in home page works

--- tests/unit/components/Global/PublicMessages.spec.js
+++ tests/unit/components/Global/PublicMessages.spec.js
@@ -30,7 +30,7 @@ describe("PublicMessages.vue", () => {
 
     it("can be check moment method", () => {
         const momentifiedDate = wrapper.vm.moment('2021-08-26T14:24:46Z');
-        expect(momentifiedDate).toBe('Thursday, August 26th 2021, 14:24')
+        expect(momentifiedDate).toBe('Thursday, August 26th 2021, 15:24')
     });
 
 });

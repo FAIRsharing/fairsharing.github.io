a319b79bc12409ec39da673ce84617d56bae33ef
Milo Thurston <knirirr@gmail.com>
2021-09-15 13:19:46 +0100

    Added missing vue-gtag.

--- src/main.js
+++ src/main.js
@@ -33,6 +33,7 @@ Vue.directive('linkified', linkify)
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'vue-gtag'
 
 import 'vue-json-pretty/lib/styles.css'
 

3c0a5d9a077352d394ac88762ea25522a7000fae
Milo Thurston <knirirr@gmail.com>
2021-10-07 10:11:43 +0100

    More spaces/formatting.

--- src/views/Users/Login/LoginFailure.vue
+++ src/views/Users/Login/LoginFailure.vue
@@ -5,20 +5,18 @@
       Login Failure
     </h2>
     <p
-        :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
     >
       Sorry, login failed. This is an unexpected event that might relate to a problem with a remote
       login service. The message was: <b>{{ error.message }}</b>
     </p>
     <p
-        :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
     >
       If you need assistance please <a href="mailto:contact@fairsharing.org">contact us</a>
-       with the text of the message shown above, details of your account, the approximate time this error
+      with the text of the message shown above, details of your account, the approximate time this error
       occurred, and the steps you took to log in.
     </p>
-
-
   </main>
 </template>
 

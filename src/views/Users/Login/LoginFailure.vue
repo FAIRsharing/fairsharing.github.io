ff9340b6b7a3a707e94dfff068c9d8efe07aeb02
Milo Thurston <knirirr@gmail.com>
2021-10-07 10:07:42 +0100

    Improved the message if oauth login fails. #1255

--- src/views/Users/Login/LoginFailure.vue
+++ src/views/Users/Login/LoginFailure.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    YOU FAILED !
-  </div>
+  <main class="pa-15 mb-10">
+    <!--  title  -->
+    <h2 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+      Login Failure
+    </h2>
+    <p
+        :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      Sorry, login failed. This is an unexpected event that might relate to a problem with a remote
+      login service. The message was: <b>{{ error.message }}</b>
+    </p>
+    <p
+        :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+    >
+      If you need assistance please <a href="mailto:contact@fairsharing.org">contact us</a>
+       with the text of the message shown above, details of your account, the approximate time this error
+      occurred, and the steps you took to log in.
+    </p>
+
+
+  </main>
 </template>
 
 <script>
@@ -9,6 +27,11 @@
 
     export default {
         name: "LoginFailure",
+        data() {
+          return {
+            error: "unknown",
+          };
+        },
         computed: {},
         mounted() {
             this.parseParams();
@@ -17,12 +40,12 @@
             ...mapActions("users", ["setError"]),
             parseParams: function(){
                 let _module = this;
-                let error = JSON.parse(_module.$route.query.errors);
-                this.setError({
+                _module.error = JSON.parse(_module.$route.query.errors);
+                _module.setError({
                     field: "login",
-                    message: error
+                    message: _module.error
                 });
-                this.$router.push({
+                _module.$router.push({
                     path: "accounts/login"
                 })
             }

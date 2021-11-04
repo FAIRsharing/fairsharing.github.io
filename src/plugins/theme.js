c9635b6c874ca8345a659aa7cfda66973a1e3139
hoseinmirian <hosein.mirian@gmail.com>
2021-11-04 12:05:04 +0000

    Update ontology subject toplevel color

--- src/plugins/theme.js
+++ src/plugins/theme.js
@@ -11,9 +11,9 @@ export default {
     deprecated_color: '#707070',
     //-- subjects domains tax tags color
     subject_color: '#E67E22',
-    subject_topLevel_1:'#f4cd4c',
-    subject_topLevel_2:'#9c6c0d',
-    subject_topLevel_3:'#553a06',
+    subject_topLevel_1:'#a04545',
+    subject_topLevel_2:'#1ba393',
+    subject_topLevel_3:'#678bab',
     subject_topLevel_4:'#27aae1',
     domain_color: '#6B1E1E',
     taxonomic_color: '#006055',

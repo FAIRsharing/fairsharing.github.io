caf9bd07f423ec8b4b55ca94e1ed222f3262df10
hoseinmirian <hosein.mirian@gmail.com>
2021-11-12 14:54:52 +0000

    Add ontology browse button

--- src/components/Home/SearchBlock.vue
+++ src/components/Home/SearchBlock.vue
@@ -5,10 +5,26 @@
   >
     <v-container>
       <section class="d-flex flex-column justify-center">
-        <string-search
-          show-home-search
-          placeholder="search through all content"
-        />
+        <v-row>
+          <v-col
+            cols="6"
+            class="d-flex align-center"
+          >
+            <string-search
+              style="width: 100%"
+              show-home-search
+              placeholder="search through all content"
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-img
+              src="/assets/Home/BlockSearch/browser.svg"
+              height="400"
+              contain
+              alt="browse by subject"
+            />
+          </v-col>
+        </v-row>
       </section>
     </v-container>
   </v-container>
@@ -28,6 +44,6 @@ export default {
 }
 
 section {
-  height: 250px;
+  height: 400px;
 }
 </style>

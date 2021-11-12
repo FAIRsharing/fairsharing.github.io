0e802335d6ae46f27295f9c3b34eebaaf5964955
hoseinmirian <hosein.mirian@gmail.com>
2021-11-12 15:46:27 +0000

    Fully added browse by subject link to homepage

--- src/components/Home/SearchBlock.vue
+++ src/components/Home/SearchBlock.vue
@@ -7,7 +7,10 @@
       <section class="d-flex flex-column justify-center">
         <v-row>
           <v-col
-            cols="6"
+            cols="12"
+            sm="12"
+            md="12"
+            lg="6"
             class="d-flex align-center"
           >
             <string-search
@@ -15,14 +18,34 @@
               show-home-search
               placeholder="search through all content"
             />
+            <div class="vertical-divider" />
           </v-col>
-          <v-col cols="6">
+          <v-col
+            cols="12"
+            sm="12"
+            md="12"
+            lg="6"
+            class="d-flex justify-content-center align-center"
+          >
             <v-img
               src="/assets/Home/BlockSearch/browser.svg"
               height="400"
               contain
               alt="browse by subject"
             />
+            <v-btn
+              color="primary"
+              :class="['mt-1 mt-lg-1 ml-2',$vuetify.breakpoint.lgAndDown?'home-search-bt':'home-search-bt-xl']"
+              to="/browse/subject"
+            >
+              <v-icon
+                x-small
+                class="mr-1"
+              >
+                fas fa-search
+              </v-icon>
+              <span class="button-text-size">Browse by Subject</span>
+            </v-btn>
           </v-col>
         </v-row>
       </section>
@@ -44,6 +67,32 @@ export default {
 }
 
 section {
-  height: 400px;
+  max-height: 650px;
 }
+
+.vertical-divider {
+  width: .1rem;
+  height: 100%;
+  background: #809bb5;
+  margin-left: 10rem;
+}
+
+.home-search-bt {
+  height: 40px!important;
+  right: 0;
+  top: 0;
+  border-radius: unset;
+  -webkit-border-radius: unset;
+  -moz-border-radius: unset;
+}
+
+.home-search-bt-xl {
+  height: 50px!important;
+  right: 0;
+  top: 0;
+  border-radius: unset;
+  -webkit-border-radius: unset;
+  -moz-border-radius: unset;
+}
+
 </style>

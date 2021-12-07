b04531360c495be10effa6cf1d3d2730b5ee5810
hoseinmirian <hosein.mirian@gmail.com>
2021-12-07 01:47:02 +0000

    Add formData to send image

--- src/lib/UploadingServices/UploadFilesService.js
+++ src/lib/UploadingServices/UploadFilesService.js
@@ -4,17 +4,23 @@ let restClient = new RESTClient();
 
 class UploadFilesService {
 
+    constructor() {
+        this.formData = new FormData();
+    }
+
     async setFormData(data) {
-        this.formData = data;
+        this.formData.append("id",data.id)
+        this.formData.append("token",data.token)
         console.log(this.formData)
     }
 
     upload(file, onUploadProgress) {
-        //encode to base 64
-        file = btoa(file)
 
+        console.log(file)
+        this.formData.append("logo",file)
 
-        this.formData = {...this.formData, logo: file}
+        // //encode to base 64
+        // file = btoa(file)
         console.log(this.formData)
 
         return restClient.uploadImages(this.formData, {

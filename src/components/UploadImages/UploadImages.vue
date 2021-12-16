8c09e0f802b5ef1221e44f47d40443b2ed298dfd
hoseinmirian <hosein.mirian@gmail.com>
2021-12-16 18:43:29 +0000

    Make upload component props more generic so defaultImage accepts both string and array as list of images

--- src/components/UploadImages/UploadImages.vue
+++ src/components/UploadImages/UploadImages.vue
@@ -19,6 +19,7 @@
       </div>
     </div>
 
+    <!--  Upload  -->
     <v-row
       no-gutters
       justify="center"
@@ -64,16 +65,7 @@
       </v-col>
     </v-row>
 
-    <v-alert
-      v-if="message"
-      border="left"
-      color="teal"
-      outlined
-      class="multi-line"
-    >
-      {{ message }}
-    </v-alert>
-
+    <!--  Showing default single Image or list of Images  -->
     <v-card
       v-if="fileInfos.length > 0"
       class="mx-auto"
@@ -114,12 +106,11 @@ export default {
     multipleUpload: {type: Boolean, default: false},
     linearProgressBar: {type: Boolean, default: true},
     credentialInfo: {type: Object, default: null},
-    currentRecordLogo: {type: String, default: null},
+    initialImages: {type: [String , Array], default: null},
   },
   data() {
     return {
       selectedFiles: null,
-      message: "",
       fileInfos: [],
       loading: false,
       progressInfos: []
@@ -128,17 +119,22 @@ export default {
   async mounted() {
     // if the credential is needed for upload process then set the formData default credential data / can be ignored in case we did not need and included any credential prop
     await this.setFormCredential();
-    // set current record url-for-logo using props so this can be decoupled from the context / can be ignored if currentRecordLogo is not passed as prop.
+    // set current record url-for-logo using initialImages props so this can be decoupled from the context / can be ignored if initialImages is not passed as prop.
     this.setDefaultImageList();
   },
     methods: {
       setDefaultImageList() {
         const API_ENDPOINT = 'https://api.fairsharing.org';
-        if (this.currentRecordLogo) {
+        // if its an array as default images to show then map it with extra API endpoint/map part is tailored for this app can be changed on different situation
+        if (isArray(this.initialImages)) {
+          this.initialImages.map(imageItem => imageItem.url = API_ENDPOINT + imageItem.url)
+        }
+
+        if (this.initialImages) {
           let response = {
             data: {
               attributes: {
-                'url-for-logo': API_ENDPOINT + this.currentRecordLogo
+                'url-for-logo': API_ENDPOINT + this.initialImages
               }
             }
           }
@@ -165,7 +161,6 @@ export default {
       },
      async uploadFiles() {
       if (!this.selectedFiles) return
-      this.message = "";
       for (let i = 0; i < this.selectedFiles.length; i++) {
          await this.upload(i, this.selectedFiles[i]);
       }
@@ -181,6 +176,7 @@ export default {
         this.loading = false
     },
      updateImageList(response) {
+          // this part cannot be more decoupled and its tailored for fairsharing application
           // handling multiple images
         if (isArray(response.data.attributes['url-for-logo'])) {
           this.fileInfos = response.data.attributes['url-for-logo']

4346ed4deac1dca499d9e2b31b41534ef4610021
hoseinmirian <hosein.mirian@gmail.com>
2021-12-16 18:51:10 +0000

    Add more comments to clarify the UploadService class

--- src/lib/UploadingServices/UploadFilesService.js
+++ src/lib/UploadingServices/UploadFilesService.js
@@ -9,11 +9,13 @@ class UploadFilesService {
     }
 
     async setFormData(data) {
+        // can be passed or added any type of data we need to include before uploading the files
         this.formData.append("id",data.id)
         this.formData.append("token",data.token)
     }
 
     async upload(file, onUploadProgress) {
+        // tailored specially for uploading files and showing progress of upload
         this.formData.append("logo",file)
         return await restClient.uploadImages(this.formData,onUploadProgress);
     }

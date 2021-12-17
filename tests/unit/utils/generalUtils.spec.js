6bd7e517ef896d6e12a9b88a2ae4a866f981f316
hoseinmirian <hosein.mirian@gmail.com>
2021-12-17 14:27:50 +0000

    Add generalUtils test

---
+++ tests/unit/utils/generalUtils.spec.js
@@ -0,0 +1,21 @@
+import generalUtils, {LightenDarkenColor} from "@/utils/generalUtils.js";
+
+describe("generalUtils.js", function(){
+
+    it("can check vvv", function () {
+        process.env.VUE_APP_HOSTNAME = 'https://api.fairsharing.org'
+        expect(generalUtils.methods.getHostname()).toBe("https://api.fairsharing.org");
+    })
+
+    it("capitalises aaa", function () {
+        expect(LightenDarkenColor('#aaa222',50)).toBe("#fff333");
+        expect(LightenDarkenColor('#ffffff',50)).toBe("#ffffff");
+        expect(LightenDarkenColor('#AA7E4A',0)).toBe("#aa7e4a");
+        expect(LightenDarkenColor('#aaa',50)).toBe("#ff0fff");
+        expect(LightenDarkenColor('#000',50)).toBe("#0000ff");
+        expect(LightenDarkenColor('#AAAA00',50)).toBe("#ffff00");
+    })
+
+
+
+})

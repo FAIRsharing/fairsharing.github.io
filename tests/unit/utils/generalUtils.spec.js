f9974d4a6e1db93931061594947236c853510e32
Hossein Mirian <hosein.mirian@gmail.com>
2021-12-17 14:38:31 +0000

    Update tests/unit/utils/generalUtils.spec.js

--- tests/unit/utils/generalUtils.spec.js
+++ tests/unit/utils/generalUtils.spec.js
@@ -7,7 +7,7 @@ it("can check getHostName method",function(){
         expect(generalUtils.methods.getHostname()).toBe("https://api.fairsharing.org");
     })
 
-    it("capitalises aaa", function () {
+    it("can check LightenDarkenColor function", function () {
         expect(LightenDarkenColor('#aaa222',50)).toBe("#fff333");
         expect(LightenDarkenColor('#ffffff',50)).toBe("#ffffff");
         expect(LightenDarkenColor('#AA7E4A',0)).toBe("#aa7e4a");

9a46b44fd878e8e67dcc3b2250b4e982125b983f
hoseinmirian <hosein.mirian@gmail.com>
2021-12-23 14:13:44 +0000

    Turn uploadFilesService.js into a singlton class and cover tests for UploadImage

---
+++ tests/unit/components/UploadImages/UploadImages.spec.js
@@ -0,0 +1,109 @@
+import {shallowMount} from "@vue/test-utils";
+import UploadImages from "@/components/UploadImages/UploadImages"
+import UploadService from "@/lib/UploadingServices/UploadFilesService";
+import sinon from "sinon";
+
+// Preparing mocks
+let mocks = {
+    uploadMock: null,
+    restore: function(mockKey) {
+        this[mockKey].restore();
+    },
+    restoreAll: function(){
+        this.restore("uploadMock");
+    },
+    setMock: function(mockKey, targetClass, targetMethod, returnedValue){
+        this[mockKey] = sinon.stub(targetClass, targetMethod);
+        this[mockKey].returns(returnedValue);
+    },
+    throwMock: function(mockKey, targetClass, targetMethod){
+        this[mockKey] = sinon.stub(targetClass, targetMethod).throws(new Error("error"));
+    }
+};
+
+
+describe('UploadImages.vue', () => {
+    let wrapper;
+
+    beforeAll( async () => {
+        mocks.setMock("uploadMock",
+            UploadService.prototype,
+            "uploadLogo",
+            "a.jpg"
+        );
+    });
+    afterAll( () => {
+        mocks.restoreAll();
+    });
+
+
+    it("can be instantiated", () => {
+        wrapper = shallowMount(UploadImages,
+            {
+                propsData: {
+                    credentialInfo: {id: 1, token: 'myToken'},
+                    initialImages: ['/hoseinmirian.jpg'],
+                    uploadServiceName: "uploadLogo",
+                    allowedFileSizeMb: 3,
+                    baseApiEndpoint: "https://fairsharing-api.org/",
+                    mimeType: "image/*",
+                }
+            }
+        );
+        wrapper.vm.$refs['FileUpload'] = {
+            afterUpload: jest.fn()
+        };
+        expect(wrapper.name()).toMatch("UploadImages");
+    });
+
+    it("can check selectFiles method functionality", () => {
+        wrapper.vm.selectFiles([{img:'blablah'},{img:'aasd'}])
+        expect(wrapper.vm.selectedFiles.length).toBe(2);
+        wrapper.vm.selectFiles('image.jpg')
+        expect(wrapper.vm.selectedFiles.length).toBe(1);
+    })
+
+    it("can check downloadFiles method functionality", () => {
+        wrapper.vm.downloadFiles(['a.jpg','b.jpg'])
+        expect(wrapper.vm.fileInfos.length).toBe(2);
+        wrapper.setProps({initialImages:null})
+        wrapper.vm.downloadFiles()
+        expect(wrapper.vm.fileInfos.length).toBe(0);
+        wrapper.vm.downloadFiles('a.jpg')
+        expect(wrapper.vm.fileInfos.length).toBe(1);
+    })
+
+    it("can check uploadFiles method functionality", async() => {
+        await wrapper.vm.uploadFiles(false)
+        expect(wrapper.vm.selectedFiles).toBe(null)
+    })
+
+
+    it("can check upload method functionality", async() => {
+        await wrapper.vm.upload(0,{name:''})
+    })
+
+    it("can be instantiated without credentials", () => {
+        const wrapper2 = shallowMount(UploadImages,
+            {
+                propsData: {
+                    credentialInfo: null,
+                    initialImages: null,
+                    uploadServiceName: "uploadLogo",
+                    allowedFileSizeMb: 3,
+                    baseApiEndpoint: "https://fairsharing-api.org/",
+                    mimeType: "image/*",
+                }
+            }
+        );
+        wrapper2.vm.$refs['FileUpload'] = {
+            afterUpload: jest.fn()
+        };
+        expect(wrapper2.name()).toMatch("UploadImages");
+    });
+
+
+
+
+
+});

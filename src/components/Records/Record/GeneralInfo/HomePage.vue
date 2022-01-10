e02b2f55452bf31d774d2ada4464010085e85135
hoseinmirian <hosein.mirian@gmail.com>
2022-01-10 12:53:18 +0000

    Apply feedback

--- src/components/Records/Record/GeneralInfo/HomePage.vue
+++ src/components/Records/Record/GeneralInfo/HomePage.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="d-flex flex-row mt-4 align-center min-height-40">
+  <div
+    v-if="getField('homepage')"
+    class="d-flex flex-row mt-4 align-center min-height-40"
+  >
     <b class="width-200">Homepage</b>
     <div class="d-flex full-width ml-md-12 ml-13">
       <a
-        v-if="getField('homepage') && !getField('metadata').reference_url"
+        v-if="getField('homepage')"
         :href="getField('homepage')"
         target="_blank"
         class="underline-effect"
       >{{ getField('homepage') }}</a>
-      <span v-else-if="getField('homepage') && getField('metadata').reference_url">
-        Not available
-      </span>
       <NoneFound
         v-else
         :string-field="getField('homepage')"

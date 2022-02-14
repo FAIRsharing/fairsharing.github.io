c1d2b0f41e11745266431951fc6fcda2c94184c7
hoseinmirian <hosein.mirian@gmail.com>
2022-02-14 13:27:56 +0000

    Apply Ally changes

--- src/views/Static/New/New.vue
+++ src/views/Static/New/New.vue
@@ -22,13 +22,13 @@
       >
         {{ item.header }}
       </h2>
-      <!--      <p
-        v-if="item.textBefore"
-        :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="$sanitize(item.textBefore)"
-      />-->
     </div>
-    <div v-if="newData.firstBlock">
+    <br>
+    <strong class="text-h4">What to add</strong>
+    <div
+      v-if="newData.firstBlock"
+      class="mt-2"
+    >
       <ul class="d-flex flex-wrap">
         <li
           v-for="(item,index) in newData.firstBlock.items"
@@ -43,14 +43,17 @@
             wrapper-class="mb-1"
           />
           <b class="text-h5">{{ item.title }}</b>
-          <p class="text-body-1 text-justify px-5">
+          <p
+            class="text-body-1 text-justify px-5"
+            style="font-size: 1.1rem!important"
+          >
             {{ item.description }}
           </p>
         </li>
       </ul>
       <v-sheet
         class="mb-5 pa-6"
-        color="white"
+        color="#f5d082"
         elevation="3"
       >
         <v-img
@@ -59,53 +62,55 @@
         />
         <h2>No datasets, please</h2>
         <b>Datasets</b>
-        <p>
+        <p style="font-size: 1.1rem!important">
           FAIRsharing helps researchers find the right database in which to submit their data. However, we do not store data ourselves. If you would like to find a home for your data, try browsing our complete <a
             href="https://fairsharing.org/browse/subject"
-            class="underline-effect"
+            class="underline-effect primary white--text"
             target="_blank"
           >subject hierarchy</a> or searching our <a
             href="https://fairsharing.org/databases"
-            class="underline-effect"
+            class="underline-effect primary white--text"
             target="_blank"
           >database records</a> to find an appropriate database for your data type. Alternatively, you could look at our collection of <a
             href="https://fairsharing.org/GeneralRepositoryComparison"
-            class="underline-effect"
+            class="underline-effect primary white--text"
             target="_blank"
           >Generalist Repositories</a> or browse all <a
             href="https://fairsharing.org/browse/subject?term=Subject%2520Agnostic"
-            class="underline-effect"
+            class="underline-effect primary white--text"
             target="_blank"
           >subject-agnostic records</a>, which may provide you with a location to store data from a wider range of research areas. If you are submitting to a particular journal, you can also search for that journal or its publisher; if they are registered with us, then their <a
             href="https://fairsharing.org/search?fairsharingRegistry=Policy"
-            class="underline-effect"
+            class="underline-effect primary white--text"
             target="_blank"
           >FAIRsharing data policy record(s)</a> may contain a list of recommended databases and/or standards.
         </p>
       </v-sheet>
       <v-sheet
-        class="mb-5 pa-6"
+        :class="['mb-5 pa-6',{'mx-25-percent':$vuetify.breakpoint.mdAndUp}]"
         color="ready_color white--text"
         elevation="3"
       >
-        <h3>Get Started!</h3>
-        <p>
+        <h3 class="text-h4">
+          Get Started!
+        </h3>
+        <p style="font-size:1.2rem">
           If you have not already done so, you may wish to <a
             href="https://fairsharing.gitbook.io/fairsharing/"
             target="_blank"
             class="underline-effect primary white--text"
-          ><u>create an account.</u></a>
+          >create an account.</a>
         </p>
-        <p>
+        <p style="font-size:1.2rem">
           Any questions? Don't hesitate to <a
             href="mailto:contact@fairsharing.org"
-            class="primary white--text"
-          ><u>contact us</u></a> or to
+            class="primary white--text underline-effect"
+          >contact us</a> or to
           <a
             href="https://fairsharing.gitbook.io/fairsharing/"
-            class="primary white--text"
+            class="primary white--text underline-effect"
           >
-            <u>consult our documentation.</u>
+            consult our documentation.
           </a>
         </p>
         <v-btn

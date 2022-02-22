eff9a9a1694fb93dfd2c1a00bdd07bc511f9f4e1
hoseinmirian <hosein.mirian@gmail.com>
2022-02-22 10:45:46 +0000

    Delph feedback

--- src/views/Static/New/New.vue
+++ src/views/Static/New/New.vue
@@ -1,30 +1,10 @@
 <template>
   <main class="pa-15 mb-10">
     <!-- eslint-disable vue/no-v-html -->
-    <!--  Header  -->
-    <h1
-      class="text-h5 text-xl-h4 mb-2 mb-xl-6"
-      v-html="$sanitize(newData.header)"
-    />
-    <!--  subtitle  -->
-    <p
-      :class="['mb-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-      v-html="$sanitize(newData.firstDescription)"
-    />
     <!--  BulletPoints  -->
-    <div
-      v-for="(item,index) in newData.description"
-      :key="item.header+'_'+index"
-    >
-      <h2
-        v-if="item.header"
-        class="text-h6 text-xl-h5"
-      >
-        {{ item.header }}
-      </h2>
-    </div>
-    <br>
-    <strong class="text-h4">What to add</strong>
+    <h1 class="text-h4">
+      What to add
+    </h1>
     <div
       v-if="newData.firstBlock"
       class="mt-2"
@@ -72,7 +52,7 @@
         </v-row>
       </v-container>
       <v-sheet
-        class="mb-5 pa-6"
+        class="my-5 mb-10 pa-6"
         color="accent3 white--text"
         elevation="3"
       >

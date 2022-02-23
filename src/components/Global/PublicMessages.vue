6fa8803344266d6f0225294b84f481150acfb139
Milo Thurston <knirirr@gmail.com>
2022-02-23 16:42:47 +0000

    Added message editing/creation/deletion but tests needed #1246.

--- src/components/Global/PublicMessages.vue
+++ src/components/Global/PublicMessages.vue
@@ -3,19 +3,20 @@
     v-if="publicMessages.length && !loading"
     class="my-1"
   >
+    <!-- eslint-enable vue/no-v-html -->
     <v-alert
       v-for="(messageObj,index) in publicMessages"
       :key="messageObj.message+'_'+index"
       dense
       type="warning"
       class="mb-2 mx-2 flex-grow-1"
-    >
-      {{ `${moment(messageObj.updatedAt)}: ${messageObj.message}` }}
-    </v-alert>
+      v-html="`${moment(messageObj.updatedAt)}: ${messageObj.message}`"
+    />
   </div>
 </template>
 
 <script>
+// no-v-html allowed as the text will come only from super_curators...
 import {mapState} from "vuex";
 import moment from "moment";
 

2aaa66442b7a15a770fa718084bfc3713335d182
hoseinmirian <hosein.mirian@gmail.com>
2022-03-03 12:19:09 +0000

    Add licence icon in the repo

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -81,8 +81,8 @@
         href="/licence"
         class="underline-effect"
       ><img
-        src="https://legacy.fairsharing.org/static/img/home/FAIRsharingCC-BY-SA.png"
-        alt="fairsharing"
+        src="/assets/home/Footer/FAIRsharingCC-BY-SA.png"
+        alt="fairsharing licence"
       ></a>
     </v-row>
   </v-row>

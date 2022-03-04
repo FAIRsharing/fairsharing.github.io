8721d8dbaba204af6d6e8dc476b188495cf19914
hoseinmirian <hosein.mirian@gmail.com>
2022-03-04 11:20:56 +0000

    Initialize infoBadge component

---
+++ src/components/Records/Record/GeneralInfo/Badge/InfoBadge.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-progress-circular
+    :rotate="360"
+    :size="80"
+    :width="10"
+    :value="0"
+    color="gray"
+    class="mr-1"
+  >
+    {{ 0 }}
+  </v-progress-circular>
+</template>
+
+<script>
+export default {
+  name: "InfoBadge",
+  components: {},
+  data() {
+    return {
+      
+    }
+  },
+  computed:{
+    
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

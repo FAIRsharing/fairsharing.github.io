37886541d6029f7bb096a0c9b41495e17519db3e
Ramon Granell <ragrator@gmail.com>
2022-04-06 10:20:18 +0100

    adding changes

--- src/views/Static/CommunityCuration/CommunityCuration.vue
+++ src/views/Static/CommunityCuration/CommunityCuration.vue
@@ -57,66 +57,100 @@
       </v-data-table>
     </div>
     <br>
-    <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
-      {{ communityCurationData.third_section.main_title }}
-    </h1>
-    <!-- eslint-disable vue/no-v-html -->
-    <p
-      :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-      v-html="communityCurationData.third_section.text"
-    />
-    <div
-      v-for="(item,index) in communityCurationData.third_section.subsections"
-      :key="index"
+    <v-expansion-panels
+      v-model="panel"
+      :readonly="readonly"
+      multiple
     >
-      <a :id="item.anchor" />
-      <h3 class="text-h6 text-xl-h5 mb-2">
-        {{ item.title }}
-      </h3>
-      <!-- eslint-disable vue/no-v-html -->
-      <p
-        :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="item.content"
-      />
-    </div>
-    <!-- eslint-enable vue/no-v-html -->
-    <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
-      {{ communityCodeOfConductData.main_title }}
-    </h1>
-    <div
-      v-for="(item,index) in communityCodeOfConductData.sections"
-      :key="'A'+ index"
-    >
-      <!-- eslint-disable vue/no-v-html -->
-      <p
-        :class="['mb-6 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="item.text"
-      />
-    </div>
-    <div
-      v-for="(item,index_two) in communityCodeOfConductData.commitments"
-      :key="index_two"
-    >
-      <!-- eslint-disable vue/no-v-html -->
-      <p :class="['mb-2 ml-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]">
-        {{ index_two+1 }} . <b>{{ item.black_text }}</b>  {{ item.text }}
-      </p>
-    </div>
-    <!-- eslint-enable vue/no-v-html -->
-    <h3 class="text-h6 text-xl-h5 mb-2 mt-4">
-      Reporting
-    </h3>
-    <div
-      v-for="(item,index) in communityCodeOfConductData.reporting"
-      :key="'B'+index"
-    >
-      <!-- eslint-disable vue/no-v-html -->
-      <p
-        :class="['mb-3 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
-        v-html="item.text"
-      />
-    </div>
-    <!-- eslint-enable vue/no-v-html -->
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+            {{ communityCurationData.third_section.main_title }}
+          </h1>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- eslint-disable vue/no-v-html -->
+          <p
+            :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+            v-html="communityCurationData.third_section.text"
+          />
+          <div
+            v-for="(item,index) in communityCurationData.third_section.subsections"
+            :key="index"
+          >
+            <a :id="item.anchor" />
+            <h3 class="text-h6 text-xl-h5 mb-2">
+              {{ item.title }}
+            </h3>
+            <!-- eslint-disable vue/no-v-html -->
+            <p
+              :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+              v-html="item.content"
+            />
+          </div>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <!-- eslint-enable vue/no-v-html -->
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+            {{ communityCurationData.fourth_section.main_title }}
+          </h1>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- eslint-disable vue/no-v-html -->
+          <p
+            :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+            v-html="communityCurationData.fourth_section.text"
+          />
+          <!-- eslint-enable vue/no-v-html -->
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
+            {{ communityCodeOfConductData.main_title }}
+          </h1>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <div
+            v-for="(item,index) in communityCodeOfConductData.sections"
+            :key="'A'+ index"
+          >
+            <!-- eslint-disable vue/no-v-html -->
+            <p
+              :class="['mb-6 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+              v-html="item.text"
+            />
+          </div>
+          <div
+            v-for="(item,index_two) in communityCodeOfConductData.commitments"
+            :key="index_two"
+          >
+            <!-- eslint-disable vue/no-v-html -->
+            <p :class="['mb-2 ml-4 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]">
+              {{ index_two+1 }} . <b>{{ item.black_text }}</b>  {{ item.text }}
+            </p>
+          </div>
+          <!-- eslint-enable vue/no-v-html -->
+
+          <h3 class="text-h6 text-xl-h5 mb-2 mt-4">
+            Reporting
+          </h3>
+          <div
+            v-for="(item,index) in communityCodeOfConductData.reporting"
+            :key="'B'+index"
+          >
+            <!-- eslint-disable vue/no-v-html -->
+            <p
+              :class="['mb-3 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+              v-html="item.text"
+            />
+          </div>
+          <!-- eslint-enable vue/no-v-html -->
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </main>
 </template>
 

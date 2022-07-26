88d83a8526f70203c05f0e896d4abf0bcb7d5b7e
Milo Thurston <milo.thurston@oerc.ox.ac.uk>
2022-07-26 12:29:49 +0100

    Added status icons to right hand column ofr edit form #1768.

--- src/components/Editor/EditRelationships.vue
+++ src/components/Editor/EditRelationships.vue
@@ -227,15 +227,45 @@
                     </span>
                   </v-list-item-content>
                   <v-list-item-icon>
-                    <v-btn
-                      icon
-                      class="red white--text"
-                      @click="removeItem(association)"
+                    <record-status
+                      :record="association.linkedRecord"
+                      :show-only-status="true"
+                      :in-edit-form="true"
+                    />
+                    <v-tooltip
+                      top
                     >
-                      <v-icon small>
-                        fa-trash
-                      </v-icon>
-                    </v-btn>
+                      <template #activator="{ on }">
+                        <v-btn
+                          icon
+                          class="blue white--text mr-2"
+                          v-on="on"
+                          @click="showPreviewOverlay(association.linkedRecord)"
+                        >
+                          <v-icon small>
+                            fas fa-eye
+                          </v-icon>
+                        </v-btn>
+                      </template>
+                      View Record
+                    </v-tooltip>
+                    <v-tooltip
+                      top
+                    >
+                      <template #activator="{ on }">
+                        <v-btn
+                          icon
+                          class="red white--text"
+                          v-on="on"
+                          @click="removeItem(association)"
+                        >
+                          <v-icon small>
+                            fa-trash
+                          </v-icon>
+                        </v-btn>
+                      </template>
+                      Remove Relation
+                    </v-tooltip>
                   </v-list-item-icon>
                 </v-list-item>
               </v-list>

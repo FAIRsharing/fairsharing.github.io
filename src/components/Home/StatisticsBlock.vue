1cbb3f414760802e6e2630a7ce328d23dbeb98dc
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2022-09-09 12:43:16 +0100

    adding background images to card

--- src/components/Home/StatisticsBlock.vue
+++ src/components/Home/StatisticsBlock.vue
@@ -15,8 +15,9 @@
           <v-card-title
             class="d-inline counter text-h2 font-weight-bold"
             :data-target="statsData.contributors"
+            :style="{ backgroundImage: 'linear-gradient(100deg, rgb(5, 6, 8) 0%, rgb(21, 60, 84) 40%, rgb(24, 71, 105) 85%, rgb(6, 12, 18) 97%, rgb(1, 18, 30) 99%), url(' + 'assets/Home/BlockStatistics/contributors.jpg'}"
           />
-          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold">
+          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold pa-4">
             {{ statisticsData.firstColumn.title }}
           </v-card-subtitle>
           <v-card-text class="text--primary">
@@ -38,8 +39,9 @@
           <v-card-title
             class="d-inline counter text-h2 font-weight-bold"
             :data-target="statsData.resources"
+            :style="{ backgroundImage: 'url(' + 'assets/Home/BlockStatistics/records.jpg'}"
           />
-          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold">
+          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold pa-4">
             {{ statisticsData.secondColumn.title }}
           </v-card-subtitle>
           <v-card-text class="text--primary">
@@ -61,8 +63,9 @@
           <v-card-title
             class="d-inline counter text-h2 font-weight-bold"
             :data-target="statsData.views"
+            :style="{ backgroundImage: 'url(' + 'assets/Home/BlockStatistics/views.jpg'}"
           />
-          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold">
+          <v-card-subtitle class="text--primary mt-0 text-h6 font-weight-bold pa-4">
             {{ statisticsData.thirdColumn.title }}
           </v-card-subtitle>
           <v-card-text class="text--primary">
@@ -151,6 +154,12 @@ export default {
   text-align: center;
   margin: 0 auto;
   color: white;
+}
+.counter {
+  color: white;
+  height: 150px;
+  background-size:cover;
+  background-blend-mode:lighten
 }
 
 </style>

16b7986ede943773772daefc4a6805bd8c8feef4
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2022-09-14 17:20:08 +0100

    adding Curators cohort block

--- src/views/Static/CommunityCuration/CuratorCohorts.vue
+++ src/views/Static/CommunityCuration/CuratorCohorts.vue
@@ -7,73 +7,83 @@
       <v-container fluid>
         <v-row dense>
           <v-col
-              v-for="card in currentCohort"
-              :key="card.id"
-              cols="12"
-              sm="12"
-              md="4"
-              lg="3"
-              xl="2"
+            v-for="card in currentCohort"
+            :key="card.id"
+            cols="12"
+            sm="12"
+            md="4"
+            lg="3"
+            xl="2"
           >
             <v-card
-                class="full-width"
+              class="full-width"
             >
               <v-img
-                  :src="card.logo ? card.logo : '/assets/Community/profiles/profileplaceholder.png'"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  cover
-                  aspect-ratio="1"
+                :src="card.logo ? card.logo : '/assets/Community/profiles/profileplaceholder.png'"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                cover
+                aspect-ratio="1"
               >
                 <v-card-actions style="position: absolute; top: 0; right: 0">
                   <v-btn
-                      icon
-                      :class="card.show_more ? 'showMore' : 'showLess'"
-                      @click="card.show_more = !card.show_more"
+                    icon
+                    :class="card.show_more ? 'showMore' : 'showLess'"
+                    @click="card.show_more = !card.show_more"
                   >
                     <v-icon
-                        class="toggleIcon"
-                        color="white"
-                        dense
+                      class="toggleIcon"
+                      color="white"
+                      dense
                     />
                   </v-btn>
                 </v-card-actions>
-                <div class="socialLinks"
-                     :class="{'hide': !card.show_more}"
+                <div
+                  class="socialLinks"
+                  :class="{'hide': !card.show_more}"
                 >
                   <v-list-item class="align-baseline">
-                    <v-list-item-content >
-                      <v-list-item dark v-if="card.orcid">
+                    <v-list-item-content>
+                      <v-list-item
+                        v-if="card.orcid"
+                        dark
+                      >
                         <a
-                            :href="`https://orcid.org/${card.orcid}`"
-                            target="_blank"
+                          :href="`https://orcid.org/${card.orcid}`"
+                          target="_blank"
                         >
                           <v-icon
-                              left
-                              class="mr-2"
+                            left
+                            class="mr-2"
                           >
                             {{ 'fab fa-orcid' }}
                           </v-icon>Orcid</a>
                       </v-list-item>
-                      <v-list-item dark v-if="card.twitter">
+                      <v-list-item
+                        v-if="card.twitter"
+                        dark
+                      >
                         <a
-                            :href="`https://twitter.com/${card.twitter}`"
-                            target="_blank"
+                          :href="`https://twitter.com/${card.twitter}`"
+                          target="_blank"
                         ><v-icon
-                            left
-                            class="mr-2"
+                          left
+                          class="mr-2"
                         >
                           {{ 'fab fa-twitter' }}
-                        </v-icon>@{{card.twitter}}</a>
+                        </v-icon>@{{ card.twitter }}</a>
                       </v-list-item>
-                      <v-list-item dark v-if="card.linkedin">
+                      <v-list-item
+                        v-if="card.linkedin"
+                        dark
+                      >
                         <a
-                            :href="`https://linkedin.com/in/${card.linkedin}`"
-                            target="_blank"
-                            text-decoration="underline"
+                          :href="`https://linkedin.com/in/${card.linkedin}`"
+                          target="_blank"
+                          text-decoration="underline"
                         ><v-icon
-                            left
-                            class="mr-2"
+                          left
+                          class="mr-2"
                         >
                           {{ 'fab fa-linkedin' }}
                         </v-icon>LinkedIn</a>
@@ -88,14 +98,14 @@
                   </h4>
                 </v-card-title>
                 <v-card-subtitle
-                    v-if="card.early_adopter"
+                  v-if="card.early_adopter"
                 >
                   <v-chip
-                      class="ma-0"
-                      color="pink"
-                      label
-                      text-color="white"
-                      small
+                    class="ma-0"
+                    color="pink"
+                    label
+                    text-color="white"
+                    small
                   >
                     Early Adopter
                   </v-chip>
@@ -103,8 +113,8 @@
               </v-img>
 
               <v-card-text
-                  class="text--primary"
-                  style="height:70px"
+                class="text--primary"
+                style="height:70px"
               >
                 <div v-if="card.organisation">
                   Organisation : {{ card.organisation }}
@@ -116,28 +126,28 @@
               </v-card-text>
 
               <v-card-actions
-                  v-if="card.id"
-                  class="pa-0 full-width"
+                v-if="card.id"
+                class="pa-0 full-width"
               >
                 <v-btn
-                    elevation="2"
-                    dark
-                    color="primary"
-                    :href="`/users/${card.id}`"
-                    rel="external"
-                    target="_blank"
-                    class="full-width py-6"
-                    tile
+                  elevation="2"
+                  dark
+                  color="primary"
+                  :href="`/users/${card.id}`"
+                  rel="external"
+                  target="_blank"
+                  class="full-width py-6"
+                  tile
                 >
                   <v-icon
-                      left
-                      class="mr-2"
+                    left
+                    class="mr-2"
                   >
                     {{ 'fa fa-user-circle' }}
                   </v-icon>
                   <span
-                      class="text-truncate text-capitalize full-width"
-                      :style=" $vuetify.breakpoint.xs ? 'max-width: 166px': 'max-width: 236px'"
+                    class="text-truncate text-capitalize full-width"
+                    :style=" $vuetify.breakpoint.xs ? 'max-width: 166px': 'max-width: 236px'"
                   > Meet {{ card.name }}</span>
                 </v-btn>
               </v-card-actions>

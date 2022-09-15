6475af189048b4820157d4f3e33735b0791763cd
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2022-09-15 17:06:39 +0100

    fixing linting issue

--- src/views/Static/CommunityCuration/CuratorCohorts.vue
+++ src/views/Static/CommunityCuration/CuratorCohorts.vue
@@ -7,34 +7,15 @@
       <v-container fluid>
         <v-row dense>
           <v-col
-<<<<<<< HEAD
-              cols="12"
-              md="10"
-              sm="12"
-=======
             cols="12"
             md="10"
             sm="12"
->>>>>>> d828446b5 (updating curators block)
           >
             <h1 class="header">
               Community Curators for {{ year }}
             </h1>
           </v-col>
           <v-col
-<<<<<<< HEAD
-              cols="12"
-              md="2"
-              sm="12"
-          >
-            <v-select
-                v-model="year"
-                :items="yearList"
-                label="Year"
-                outlined
-                dense
-                @change="selectYear()"
-=======
             cols="12"
             md="2"
             sm="12"
@@ -46,21 +27,11 @@
               outlined
               dense
               @change="selectYear()"
->>>>>>> d828446b5 (updating curators block)
             />
           </v-col>
         </v-row>
         <v-row dense>
           <v-col
-<<<<<<< HEAD
-              v-for="card in currentCohort"
-              :key="card.id"
-              cols="12"
-              sm="12"
-              md="4"
-              lg="3"
-              xl="2"
-=======
             v-for="card in currentCohort"
             :key="card.id"
             cols="12"
@@ -68,99 +39,79 @@
             md="4"
             lg="3"
             xl="2"
->>>>>>> d828446b5 (updating curators block)
           >
             <v-card
-                class="full-width"
+              class="full-width"
             >
               <v-img
-                  :src="card.logo ? card.logo : '/assets/Community/profiles/profileplaceholder.png'"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  cover
-                  aspect-ratio="1"
+                :src="card.logo ? card.logo : '/assets/Community/profiles/profileplaceholder.png'"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                cover
+                aspect-ratio="1"
               >
                 <v-card-actions style="position: absolute; top: 0; right: 0">
                   <v-btn
-                      icon
-                      :class="card.show_more ? 'showMore' : 'showLess'"
-                      @click="card.show_more = !card.show_more"
+                    icon
+                    :class="card.show_more ? 'showMore' : 'showLess'"
+                    @click="card.show_more = !card.show_more"
                   >
                     <v-icon
-                        class="toggleIcon"
-                        color="white"
-                        dense
+                      class="toggleIcon"
+                      color="white"
+                      dense
                     />
                   </v-btn>
                 </v-card-actions>
                 <div
-                    class="socialLinks"
-                    :class="{'hide': !card.show_more}"
+                  class="socialLinks"
+                  :class="{'hide': !card.show_more}"
                 >
                   <v-list-item class="align-baseline">
                     <v-list-item-content>
                       <v-list-item
-                          v-if="card.orcid"
-                          dark
+                        v-if="card.orcid"
+                        dark
                       >
                         <a
-<<<<<<< HEAD
-                            :href="`https://orcid.org/${card.orcid}`"
-                            target="_blank"
-                            class="d-flex align-center"
-=======
                           :href="`https://orcid.org/${card.orcid}`"
                           target="_blank"
                           class="d-flex align-center"
->>>>>>> d828446b5 (updating curators block)
                         >
                           <v-icon
-                              left
-                              class="mr-2"
+                            left
+                            class="mr-2"
                           >
                             {{ 'fab fa-orcid' }}
                           </v-icon><span>Orcid</span></a>
                       </v-list-item>
                       <v-list-item
-                          v-if="card.twitter"
-                          dark
+                        v-if="card.twitter"
+                        dark
                       >
                         <a
-<<<<<<< HEAD
-                            :href="`https://twitter.com/${card.twitter}`"
-                            target="_blank"
-                            class="d-flex align-center"
-=======
                           :href="`https://twitter.com/${card.twitter}`"
                           target="_blank"
                           class="d-flex align-center"
->>>>>>> d828446b5 (updating curators block)
                         ><v-icon
-                            left
-                            class="mr-2"
+                          left
+                          class="mr-2"
                         >
                           {{ 'fab fa-twitter' }}
                         </v-icon><span>@{{ card.twitter }}</span></a>
                       </v-list-item>
                       <v-list-item
-                          v-if="card.linkedin"
-                          dark
+                        v-if="card.linkedin"
+                        dark
                       >
                         <a
-<<<<<<< HEAD
-                            :href="`https://linkedin.com/in/${card.linkedin}`"
-                            target="_blank"
-                            text-decoration="underline"
-                            class="d-flex align-center"
-=======
                           :href="`https://linkedin.com/in/${card.linkedin}`"
                           target="_blank"
                           text-decoration="underline"
                           class="d-flex align-center"
->>>>>>> d828446b5 (updating curators block)
                         ><v-icon
-                            left
-                            class="mr-2"
+                          left
+                          class="mr-2"
                         >
                           {{ 'fab fa-linkedin' }}
                         </v-icon><span>LinkedIn</span></a>
@@ -175,14 +126,14 @@
                   </h4>
                 </v-card-title>
                 <v-card-subtitle
-                    v-if="card.early_adopter"
+                  v-if="card.early_adopter"
                 >
                   <v-chip
-                      class="ma-0"
-                      color="pink"
-                      label
-                      text-color="white"
-                      small
+                    class="ma-0"
+                    color="pink"
+                    label
+                    text-color="white"
+                    small
                   >
                     Early Adopter
                   </v-chip>
@@ -190,8 +141,8 @@
               </v-img>
 
               <v-card-text
-                  class="text--primary"
-                  style="height:70px"
+                class="text--primary"
+                style="height:70px"
               >
                 <div v-if="card.organisation">
                   Organisation : {{ card.organisation }}
@@ -203,35 +154,26 @@
               </v-card-text>
 
               <v-card-actions
-                  v-if="card.id"
-                  class="pa-0 full-width"
+                v-if="card.id"
+                class="pa-0 full-width"
               >
                 <v-btn
-<<<<<<< HEAD
-                    elevation="2"
-                    dark
-                    color="primary"
-                    :href="`/users/${card.id}`"
-                    class="full-width py-6"
-                    tile
-=======
                   elevation="2"
                   dark
                   color="primary"
                   :href="`/users/${card.id}`"
                   class="full-width py-6"
                   tile
->>>>>>> d828446b5 (updating curators block)
                 >
                   <v-icon
-                      left
-                      class="mr-2"
+                    left
+                    class="mr-2"
                   >
                     {{ 'fa fa-user-circle' }}
                   </v-icon>
                   <span
-                      class="text-truncate text-capitalize full-width"
-                      :style=" $vuetify.breakpoint.xs ? 'max-width: 166px': 'max-width: 236px'"
+                    class="text-truncate text-capitalize full-width"
+                    :style=" $vuetify.breakpoint.xs ? 'max-width: 166px': 'max-width: 236px'"
                   > Meet {{ card.name }}</span>
                 </v-btn>
               </v-card-actions>

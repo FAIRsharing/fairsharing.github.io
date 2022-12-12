57530525e7f0e2d8795ff36fd3ede542c3900991
Ramon Granell <ragrator@gmail.com>
2022-12-12 13:25:14 +0000

    first step

--- src/views/Static/CommunityCuration/CommunityCuration.vue
+++ src/views/Static/CommunityCuration/CommunityCuration.vue
@@ -12,6 +12,12 @@
       v-html="communityCurationData.first_section.text_2"
     />
     <!-- eslint-enable vue/no-v-html -->
+    <!-- eslint-disable vue/no-v-html -->
+    <p
+      :class="['mb-8 lato-font-medium lato-text-sm',{'lato-text-md':$vuetify.breakpoint.xlOnly }]"
+      v-html="communityCurationData.first_section.text_3"
+    />
+    <!-- eslint-enable vue/no-v-html -->
     <h1 class="text-h5 text-xl-h4 mb-2 mb-xl-6">
       {{ communityCurationData.second_section.main_title }}
     </h1>

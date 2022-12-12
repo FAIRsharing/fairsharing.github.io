3a9b79e9c1fb566a8e322a4119b8e844becb992f
Ramon Granell <ragrator@gmail.com>
2022-12-12 13:54:33 +0000

    modifying Curator Cohort

--- src/views/Static/CommunityCuration/CuratorCohorts.vue
+++ src/views/Static/CommunityCuration/CuratorCohorts.vue
@@ -69,6 +69,28 @@
           </v-col>
         </v-row>
         <v-row
+          dense
+        >
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+            sm="12"
+          >
+            <i>The Community Curator Programme at FAIRsharing.org is a great working example demonstrating when data communities work FAIR together, they can sustain FAIR innovation together.</i>
+            <b>Lindsey Anderson</b> - Life Science, Omics
+          </v-col>
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+            sm="12"
+          >
+            <i>I find myself dedicating more and more time on curating […] In doing so, I experienced how much standards are key, but also that [these] curation skills and activities are indeed valuable activities that make a difference in increasing discoverability and reusability of resources we want to share..</i>
+            <b>Geneviève Michaud</b> - Humanities and Social Science
+          </v-col>
+        </v-row>
+        <v-row
           v-if="currentCohort.length"
           dense
         >

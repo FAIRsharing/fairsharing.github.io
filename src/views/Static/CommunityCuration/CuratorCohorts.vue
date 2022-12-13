065dc5170242e51248a805a87fcd098abb86a1e4
Ramon Granell <ragrator@gmail.com>
2022-12-13 11:53:37 +0000

    update marging

--- src/views/Static/CommunityCuration/CuratorCohorts.vue
+++ src/views/Static/CommunityCuration/CuratorCohorts.vue
@@ -70,7 +70,6 @@
         </v-row>
         <v-row
           dense
-          mb-6
         >
           <v-col
             cols="12"
@@ -78,9 +77,13 @@
             md="6"
             sm="12"
           >
-            <i>The Community Curator Programme at FAIRsharing.org is a great working example demonstrating when data communities work FAIR together, they can sustain FAIR innovation together.</i>
-            <br>
-            <b>Lindsey Anderson</b> - Life Science, Omics
+            <div
+              class="mb-2"
+            >
+              <i>The Community Curator Programme at FAIRsharing.org is a great working example demonstrating when data communities work FAIR together, they can sustain FAIR innovation together.</i>
+              <br>
+              <b>Lindsey Anderson</b> - Life Science, Omics
+            </div>
           </v-col>
           <v-col
             cols="12"
@@ -88,9 +91,13 @@
             md="6"
             sm="12"
           >
-            <i>I find myself dedicating more and more time on curating […] In doing so, I experienced how much standards are key, but also that [these] curation skills and activities are indeed valuable activities that make a difference in increasing discoverability and reusability of resources we want to share..</i>
-            <br>
-            <b>Geneviève Michaud</b> - Humanities and Social Science
+            <div
+              class="mb-2"
+            >
+              <i>I find myself dedicating more and more time on curating […] In doing so, I experienced how much standards are key, but also that [these] curation skills and activities are indeed valuable activities that make a difference in increasing discoverability and reusability of resources we want to share..</i>
+              <br>
+              <b>Geneviève Michaud</b> - Humanities and Social Science
+            </div>
           </v-col>
         </v-row>
         <v-row

8ee8b4b1ae0e632f1ce436e4bc4acca28c780c21
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-11-10 17:48:50 +0000

    adding Advanced QUery builder

---
+++ src/components/Records/Search/Input/QueryBuilderComponents/Subject.vue
@@ -0,0 +1,25 @@
+<template>
+  <input v-model="model" type="text" placeholder="Subject" />
+</template>
+<script>
+export default {
+  name: "Subject",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+};
+</script>

fe98d04cfa7e86ed3ec23f366918562b06301f69
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-11-21 15:32:57 +0000

    fixing test cases

--- tests/unit/components/Records/Search/Input/AdvancedSearch.spec.js
+++ tests/unit/components/Records/Search/Input/AdvancedSearch.spec.js
@@ -1,24 +1,64 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import Vuetify from "vuetify";
+import Vuex from "vuex";
 
 import AdvancedSearch from "@/components/Records/Search/Input/AdvancedSearch";
+import advancedSearch from "@/store/advancedSearch";
 
 const $router = {
   push: jest.fn(),
 };
 let $route = { path: "/search", query: {} };
-
+const localVue = createLocalVue();
+localVue.use(Vuex);
 let vuetify = new Vuetify();
 
 describe("AdvancedSearch.vue", () => {
-  let wrapper = shallowMount(AdvancedSearch, {
-    mocks: { $router, $route },
-    vuetify,
-  });
+  let wrapper, store, actions;
+  beforeEach(() => {
+    advancedSearch.getters = {
+      getAdvancedSearch: () => {
+        return [
+          {
+            operatorIdentifier: "_and",
+            children: [
+              {
+                operatorIdentifier: "_and",
+                children: [
+                  {
+                    identifier: "registry",
+                    value: ["database", "standard"],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+      },
+    };
 
+    actions = {
+      fetchAdvancedSearchResults: jest.fn(),
+      resetAdvancedSearchQuery: jest.fn(),
+    };
+
+    store = new Vuex.Store({
+      modules: {
+        namespaced: true,
+        actions,
+        advancedSearch: advancedSearch,
+      },
+    });
+
+    wrapper = shallowMount(AdvancedSearch, {
+      localVue,
+      vuetify,
+      store,
+      mocks: { $router, $route },
+    });
+  });
 
   it("can mount", () => {
     expect(wrapper.vm.$options.name).toBe("AdvancedSearch");
   });
-
 });

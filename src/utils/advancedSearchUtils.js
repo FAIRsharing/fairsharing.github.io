f23cf3fb572f52c20ad862cf5b5c093805202de0
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-12-05 16:53:19 +0000

    adding files for advancedSearch

--- src/utils/advancedSearchUtils.js
+++ src/utils/advancedSearchUtils.js
@@ -8,11 +8,12 @@ const uniqueValues = (item) => {
   let merged = uniqWith(item, (pre, cur) => {
     if (pre.identifier === cur.identifier) {
       cur["value"] = cur["value"] + "," + pre["value"];
+
       //Combine string values from different text-fields in same set and convert in into array
-      /*istanbul ignore else */
       if (typeof cur["value"] === "string") {
         cur["value"] = cur["value"].split(",");
       }
+
       //Remove duplicates and create unique array
       cur["value"] = [...new Set(cur["value"])];
       return true;

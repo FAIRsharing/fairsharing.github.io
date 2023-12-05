dfed5c0d75ea7ee7c8749d803329bb94d2610cfc
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-12-05 16:53:19 +0000

    adding files for advancedSearch

--- src/views/Records/AdvancedSearchRecordsView.vue
+++ src/views/Records/AdvancedSearchRecordsView.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-main>
-    <!--Jump to top arrow button -->
-    <v-fade-transition>
-      <JumpToTop v-if="scrollStatus" />
-    </v-fade-transition>
-    <!--Loader-->
+  <div>
     <v-fade-transition>
       <v-overlay
         v-if="getLoadingStatus"
@@ -14,79 +9,25 @@
         <Loaders />
       </v-overlay>
     </v-fade-transition>
-    <!--Search result -->
-    <v-container
-      fluid
-      class="pa-0"
-    >
-      <v-row no-gutters>
-        <!-- Advanced search selection left column-->
-        <v-col
-          v-if="$vuetify.breakpoint.lgAndUp"
-          cols="12"
-          lg="4"
-          md="4"
-          xl="3"
-          class="d-flex mt-5 ml-2"
-        >
-          <AdvancedSearchSelection />
-        </v-col>
-        <!-- Advanced search result right column-->
-        <v-col>
-          <AdvancedSearchResultTable />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+    <AdvancedSearchResultTable />
+  </div>
 </template>
 <script>
-import { mapActions, mapGetters, mapState } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
-import JumpToTop from "@/components/Navigation/jumpToTop.vue";
 import Loaders from "@/components/Navigation/Loaders";
-import onScrollUtil from "@/utils/onScrollUtil";
 import AdvancedSearchResultTable from "@/views/Records/AdvancedSearchResultTable.vue";
-import AdvancedSearchSelection from "@/views/Records/AdvancedSearchSelection.vue";
 
 export default {
   name: "AdvancedSearchRecordsView",
-  components: {
-    JumpToTop,
-    Loaders,
-    AdvancedSearchResultTable,
-    AdvancedSearchSelection,
-  },
-  mixins: [onScrollUtil],
-  data() {
-    return {
-      offsetTop: 0,
-    };
-  },
+  components: { Loaders, AdvancedSearchResultTable },
   computed: {
-    ...mapState("uiController", ["scrollStatus", "stickToTop"]),
-    ...mapGetters("advancedSearch", [
-      "getLoadingStatus",
-      "getAdvancedSearchResponse",
-    ]),
-  },
-  mounted() {
-    window.addEventListener("scroll", () => {
-      this.onScroll(this.getAdvancedSearchResponse);
-    });
+    ...mapGetters("advancedSearch", ["getLoadingStatus"]),
   },
   destroyed() {
     this.resetAdvancedSearchResponse();
-    window.removeEventListener("scroll", () => {
-      this.onScroll(this.getAdvancedSearchResponse);
-    });
-    this.setStickToTop(false);
-    this.$store.dispatch("uiController/setGeneralUIAttributesAction", {
-      drawerVisibilityState: false,
-      headerVisibilityState: true,
-    });
   },
   methods: {
-    ...mapActions("uiController", ["setStickToTop"]),
     ...mapActions("advancedSearch", ["resetAdvancedSearchResponse"]),
   },
 };

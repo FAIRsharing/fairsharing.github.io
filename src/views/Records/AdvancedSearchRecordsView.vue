04ebaf9acdfde152be93858d15d41d6b8074da3b
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-12-11 14:16:28 +0000

    adding selection sidebar

--- src/views/Records/AdvancedSearchRecordsView.vue
+++ src/views/Records/AdvancedSearchRecordsView.vue
@@ -9,18 +9,22 @@
         <Loaders />
       </v-overlay>
     </v-fade-transition>
-    <AdvancedSearchResultTable />
+    <div class="d-flex full-width my-4">
+      <AdvancedSeacrhSelection />
+      <AdvancedSearchResultTable />
+    </div>
   </div>
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
 
 import Loaders from "@/components/Navigation/Loaders";
+import AdvancedSeacrhSelection from "@/views/Records/AdvancedSeacrhSelection.vue";
 import AdvancedSearchResultTable from "@/views/Records/AdvancedSearchResultTable.vue";
 
 export default {
   name: "AdvancedSearchRecordsView",
-  components: { Loaders, AdvancedSearchResultTable },
+  components: { Loaders, AdvancedSearchResultTable, AdvancedSeacrhSelection },
   computed: {
     ...mapGetters("advancedSearch", ["getLoadingStatus"]),
   },

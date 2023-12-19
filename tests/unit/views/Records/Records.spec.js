11290d55b7e7fea2a28036be6a93a932196fbb90
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-12-19 11:12:19 +0000

    adding onScroll functionality

--- tests/unit/views/Records/Records.spec.js
+++ tests/unit/views/Records/Records.spec.js
@@ -1,15 +1,15 @@
-import {createLocalVue,shallowMount} from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import VueMeta from "vue-meta";
 import VueScrollTo from "vue-scrollto";
-import Vuetify from "vuetify"
-import Vuex from "vuex"
+import Vuetify from "vuetify";
+import Vuex from "vuex";
 
-import fakeIntrospection from "@/../tests/fixtures/fakeIntrospection.json"
+import fakeIntrospection from "@/../tests/fixtures/fakeIntrospection.json";
 import Client from "@/lib/GraphClient/GraphClient.js";
-import introspection from "@/store/introspector.js"
-import records from "@/store/recordSearch.js"
-import uiController from "@/store/uiController.js"
-import {actions} from "@/store/uiController.js"
+import introspection from "@/store/introspector.js";
+import records from "@/store/recordSearch.js";
+import uiController from "@/store/uiController.js";
+import { actions } from "@/store/uiController.js";
 import userStore from "@/store/users";
 import Records from "@/views/Records/Records.vue";
 
@@ -19,192 +19,199 @@ const axios = require("axios");
 const localVue = createLocalVue();
 localVue.use(Vuex);
 localVue.use(VueMeta);
-localVue.use(VueScrollTo,{});
+localVue.use(VueScrollTo, {});
 
-userStore.state.user = function() {
-    return {
-        metadata: {
-            preferences: {
-                hide_email: true,
-            },
-            profile_type: "profile 1"
-        },
-        credentials: {
-            username: "username",
-            token: '123'
-        }
-    }
+userStore.state.user = function () {
+  return {
+    metadata: {
+      preferences: {
+        hide_email: true,
+      },
+      profile_type: "profile 1",
+    },
+    credentials: {
+      username: "username",
+      token: "123",
+    },
+  };
 };
 
 const $route = {
-    name: "Standards",
-    path: "standard",
-    query: {
-        fairsharingRegistry: "Standard",
-        grants: "string",
-        publications: null,
-        isRecommended: "false",
-        registry: "[abc,def]",
-        licences: 123
-    }
+  name: "Standards",
+  path: "standard",
+  query: {
+    fairsharingRegistry: "Standard",
+    grants: "string",
+    publications: null,
+    isRecommended: "false",
+    registry: "[abc,def]",
+    licences: 123,
+  },
 };
 const $store = new Vuex.Store({
-    modules: {
-        records: records,
-        introspection: introspection,
-        uiController: uiController,
-        users: userStore
-    },
+  modules: {
+    records: records,
+    introspection: introspection,
+    uiController: uiController,
+    users: userStore,
+  },
 });
 
 describe("Records.vue", () => {
+  let vuetify;
+  let stub = sinon.stub(Client.prototype, "executeQuery");
 
-    let vuetify;
-    let stub = sinon.stub(Client.prototype, "executeQuery");
+  beforeAll(() => {
+    stub
+      .withArgs(sinon.match.object)
+      .returns({ searchFairsharingRecords: { records: [1] } });
+  });
 
-    beforeAll(() => {
-        stub.withArgs(sinon.match.object).returns({searchFairsharingRecords: {records: [1]}});
+  // Set up the wrapper
+  let wrapper;
+  beforeEach(async () => {
+    vuetify = new Vuetify();
+
+    wrapper = await shallowMount(Records, {
+      mocks: { $route, $store },
+      localVue,
+      vuetify,
     });
+    delete global.window["top"];
+    global.window = Object.create(window);
+  });
 
-    // Set up the wrapper
-    let wrapper;
-    beforeEach(async () => {
-        vuetify = new Vuetify();
+  it("can be instantiated", () => {
+    expect(wrapper.vm.$options.name).toMatch("Records");
+  });
 
-        wrapper = await shallowMount(Records, {
-            mocks: {$route, $store},
-            localVue,
-            vuetify,
-        });
-        delete global.window['top'];
-        global.window = Object.create(window);
+  it("has a currentPath computed attribute", () => {
+    expect(wrapper.vm.currentPath[0]).toBe("Standard");
+  });
+
+  it("can correctly raise an error", async () => {
+    sinon
+      .stub(axios, "post")
+      .withArgs(sinon.match.any)
+      .returns({
+        data: {
+          errors: [{ message: "Error" }],
+        },
+      });
+    await expect(wrapper.vm.getData()).rejects;
+    axios.post.restore();
+  });
+
+  it("can get the records", async () => {
+    $route.query = {
+      test: "abc",
+      test2: ["abcdef"],
+      test3: "abc, def",
+      test4: 123,
+      test5: true,
+    };
+    let returnedVal = {
+      data: {
+        data: fakeIntrospection.data,
+      },
+      headers: {
+        maintenance: "false",
+      },
+    };
+    sinon
+      .stub(Client.prototype, "getData")
+      .withArgs(sinon.match.any)
+      .returns(returnedVal);
+    await wrapper.vm.$store.dispatch("introspection/fetchParameters");
+    Client.prototype.getData.restore();
+    const path = wrapper.vm.currentPath;
+    const queryParameters = await wrapper.vm.$store.getters[
+      "introspection/buildQueryParameters"
+    ](path);
+    expect(queryParameters).toStrictEqual({
+      test: "abc",
+      test2: "abcdef",
+      test3: ["abc", " def"],
+      test4: 123,
+      test5: true,
     });
+  });
 
-    it("can be instantiated", () => {
-        expect(wrapper.vm.$options.name).toMatch("Records");
+  it("react to path change", async () => {
+    $route.path = "/search";
+    $route.query = {};
+    expect(wrapper.vm.currentPath[0]).toBe("Search");
+    $route.path = "/standard";
+    expect(wrapper.vm.currentPath[0]).toBe("Standard");
+  });
+
+  it("can correctly redirect", async () => {
+    /*
+     * This test passes when run by itself, but times out when run as part of the suite.
+     * This appears to have been caused by modifications to tryRedirect() as part of
+     * https://github.com/FAIRsharing/fairsharing.github.io/issues/1186.
+     */
+    jest.setTimeout(40000);
+    $route.path = "/standards";
+    $route.params = { fairsharingRegistry: "Standard" };
+    $route.query = { fairsharingRegistry: "Standard" };
+    const $router = {
+      push: jest.fn(),
+    };
+    let localWrapper = await shallowMount(Records, {
+      mocks: { $route, $store, $router },
+      localVue,
+      vuetify,
     });
+    await localWrapper.vm.tryRedirect();
+    expect($router.push).toHaveBeenCalledTimes(2);
+    $route.name = "test";
+    $route.params = { fairsharingRegistry: "123" };
+    $route.query = { fairsharingRegistry: "123" };
+    await localWrapper.vm.tryRedirect();
+    expect($router.push).toHaveBeenCalledTimes(2);
+  });
 
-    it("has a currentPath computed attribute", () => {
-        expect(wrapper.vm.currentPath[0]).toBe("Standard");
+  it("can onScroll function work properly", () => {
+    // wrapper.vm.$store.state.records.records = ['1', '2', '3'];
+    const dataArr = ["1", "2", "3"];
+
+    global.window.top = { scrollY: 150 };
+    actions.commit = jest.fn();
+    actions.setGeneralUIAttributesAction({});
+    wrapper.vm.onScroll(dataArr);
+    expect(actions.commit).toHaveBeenCalledTimes(1);
+
+    global.window.top = { scrollY: 50 };
+    actions.commit = jest.fn();
+    actions.setGeneralUIAttributesAction({});
+    wrapper.vm.onScroll(dataArr);
+    expect(actions.commit).toHaveBeenCalledTimes(1);
+
+    global.window.top = { scrollY: 501 };
+    actions.commit = jest.fn();
+    actions.setGeneralUIAttributesAction({});
+    wrapper.vm.onScroll(dataArr);
+    expect(actions.commit).toHaveBeenCalledTimes(1);
+  });
+
+  it("can reset the store when destroyed", () => {
+    wrapper.destroy();
+    expect(wrapper.vm.$store.state.records.facets).toStrictEqual([]);
+    expect(wrapper.vm.$store.state.records.records).toStrictEqual([]);
+    expect(wrapper.vm.$store.state.records.loading).toBe(false);
+    const otherStates = ["hits", "totalPages", "perPage"];
+    otherStates.forEach((state) => {
+      expect(wrapper.vm.$store.state.records[state]).toBe(null);
     });
+  });
 
-    it("can correctly raise an error", async () => {
-        sinon.stub(axios, "post").withArgs(sinon.match.any).returns({
-            data: {
-                errors: [
-                    {message: "Error"}
-                ]
-            }
-        });
-        await expect(wrapper.vm.getData()).rejects;
-        axios.post.restore();
+  it("responds to an invalid graphql query", async () => {
+    stub.withArgs(sinon.match.object).returns({ error: "invalid query" });
+    await wrapper.vm.fetchRecords({
+      params: { q: '"invalid query"' },
+      token: null,
     });
-
-    it("can get the records", async () => {
-        $route.query = {
-            "test": "abc",
-            "test2": ["abcdef"],
-            "test3": "abc, def",
-            "test4": 123,
-            "test5": true
-        };
-        let returnedVal = {
-            data: {
-                data: fakeIntrospection.data
-            },
-            headers: {
-                maintenance: "false"
-            }
-        };
-        sinon.stub(Client.prototype, "getData").withArgs(sinon.match.any).returns(returnedVal);
-        await wrapper.vm.$store.dispatch("introspection/fetchParameters");
-        Client.prototype.getData.restore();
-        const path = wrapper.vm.currentPath;
-        const queryParameters = await wrapper.vm.$store.getters["introspection/buildQueryParameters"](path);
-        expect(queryParameters).toStrictEqual({
-            test: 'abc',
-            test2: 'abcdef',
-            test3: ['abc', ' def'],
-            test4: 123,
-            test5: true
-        });
-    });
-
-    it("react to path change", async () => {
-        $route.path = "/search";
-        $route.query = {};
-        expect(wrapper.vm.currentPath[0]).toBe("Search");
-        $route.path = "/standard";
-        expect(wrapper.vm.currentPath[0]).toBe("Standard");
-    });
-
-    it("can correctly redirect", async () => {
-        /*
-         * This test passes when run by itself, but times out when run as part of the suite.
-         * This appears to have been caused by modifications to tryRedirect() as part of
-         * https://github.com/FAIRsharing/fairsharing.github.io/issues/1186.
-         */
-        jest.setTimeout(40000);
-        $route.path = "/standards";
-        $route.params = {fairsharingRegistry: "Standard"};
-        $route.query = {fairsharingRegistry: "Standard"};
-        const $router = {
-            push: jest.fn(),
-        };
-        let localWrapper = await shallowMount(Records, {
-            mocks: {$route, $store, $router},
-            localVue,
-            vuetify
-        });
-        await localWrapper.vm.tryRedirect();
-        expect($router.push).toHaveBeenCalledTimes(2);
-        $route.name = "test";
-        $route.params = {fairsharingRegistry: "123"};
-        $route.query = {fairsharingRegistry: "123"};
-        await localWrapper.vm.tryRedirect();
-        expect($router.push).toHaveBeenCalledTimes(2);
-    });
-
-    it("can onScroll function work properly", () => {
-
-        wrapper.vm.$store.state.records.records = ['1', '2', '3'];
-
-        global.window.top = {scrollY:150};
-        actions.commit = jest.fn();
-        actions.setGeneralUIAttributesAction({});
-        wrapper.vm.onScroll();
-        expect(actions.commit).toHaveBeenCalledTimes(1);
-
-
-        global.window.top = {scrollY:50};
-        actions.commit = jest.fn();
-        actions.setGeneralUIAttributesAction({});
-        wrapper.vm.onScroll();
-        expect(actions.commit).toHaveBeenCalledTimes(1);
-
-        global.window.top = {scrollY:501};
-        actions.commit = jest.fn();
-        actions.setGeneralUIAttributesAction({});
-        wrapper.vm.onScroll();
-        expect(actions.commit).toHaveBeenCalledTimes(1);
-    });
-
-
-    it("can reset the store when destroyed", () => {
-        wrapper.destroy();
-        expect(wrapper.vm.$store.state.records.facets).toStrictEqual([]);
-        expect(wrapper.vm.$store.state.records.records).toStrictEqual([]);
-        expect(wrapper.vm.$store.state.records.loading).toBe(false);
-        const otherStates = ["hits","totalPages","perPage"]
-        otherStates.forEach(state => {
-            expect(wrapper.vm.$store.state.records[state]).toBe(null)
-        })
-    });
-
-    it("responds to an invalid graphql query", async () => {
-        stub.withArgs(sinon.match.object).returns({error: 'invalid query'});
-        await wrapper.vm.fetchRecords({ params: {"q":"\"invalid query\""}, token: null });
-        expect(wrapper.vm.$store.state.records.records).toStrictEqual([]);
-    })
-
+    expect(wrapper.vm.$store.state.records.records).toStrictEqual([]);
+  });
 });

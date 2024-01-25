3944a07ac9be34efd1c7ae06db5bd89f949df864
Milo Thurston <knirirr@gmail.com>
2024-01-25 13:52:33 +0000

    Removed commented code.

--- src/store/users.js
+++ src/store/users.js
@@ -317,7 +317,6 @@ export const actions = {
                  getAllUsersQuery.queryParam.q = query;
             }
             else {
-                //delete getAllUsersQuery.queryParam.q;
                 getAllUsersQuery.queryParam.q = "*";
             }
             let usersListData = await graphClient.executeQuery(getAllUsersQuery);

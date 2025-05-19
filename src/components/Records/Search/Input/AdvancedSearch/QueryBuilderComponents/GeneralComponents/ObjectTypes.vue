5a237791e7740289ed34415c6a505591f8a3f9cd
Milo Thurston <knirirr@gmail.com>
2025-05-19 16:17:49 +0100

    Added object type to adv. search (no tests yet). #2569

---
+++ src/components/Records/Search/Input/AdvancedSearch/QueryBuilderComponents/GeneralComponents/ObjectTypes.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="d-flex width-90">
+    <TooltipComponent :tool-tip-text="toolTipText" />
+    <AutoCompleteComponent
+      v-model="model"
+      :item-value="itemValue"
+      :item-list="typeList()"
+      :loading="getLoadingData"
+      @input="selectedValue"
+      @fetchData="getResults"
+    />
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+import objectTypes from "@/store";
+
+import AutoCompleteComponent from "../UtilComponents/AutoCompleteComponent.vue";
+import TooltipComponent from "../UtilComponents/TooltipComponent.vue";
+
+export default {
+  name: "ObjectTypes",
+  components: { TooltipComponent, AutoCompleteComponent },
+  props: {
+    value: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data: () => {
+    return {
+      itemSelected: [],
+      itemValue: [],
+      toolTipText:
+        "Object types applicable to this resource or its data. Multiple selections will be joined with OR. Start typing to see available types.",
+    };
+  },
+
+  computed: {
+    ...mapGetters("objectTypes", ["getObjectTypes", "getLoadingData"]),
+    ...mapGetters("advancedSearch", ["getEditDialogStatus"]),
+
+    model: {
+      get() {
+        return this.itemSelected;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+  watch: {
+    itemSelected(newValue) {
+      this.itemValue = newValue;
+    },
+    /**
+     * Item list for the autocomplete should not be empty
+     * It is assigned with selected values when edit field is opened
+     */
+    getEditDialogStatus: {
+      handler(open) {
+        if (open) {
+          if (this.value && this.value.length) {
+            objectTypes.commit(
+              "objectTypes/setObjectTypes",
+              this.value
+            );
+          }
+        }
+      },
+      immediate: true,
+    },
+  },
+  mounted() {
+    //Pre-fill selected values on edit advanced search is clicked and open
+    this.itemValue = this.value;
+  },
+  methods: {
+    ...mapActions("objectTypes", ["fetchObjectTypes"]),
+
+    selectedValue(item) {
+      this.itemSelected = item;
+    },
+    getResults(queryParams) {
+      console.log("Fetching!");
+      if (queryParams) this.fetchObjectTypes(queryParams);
+      console.log("Fetched: ");
+      console.log(JSON.stringify(queryParams, null, 2));
+    },
+    typeList() {
+      let items = [];
+      this.getObjectTypes.forEach((item) => {
+        items.push(item.label.toLowerCase());
+      })
+      return items;
+    }
+  },
+};
+</script>

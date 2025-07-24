ebdb78088554e246f3a7ec4c9f345451f25b9e01
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-07-24 11:22:59 +0100

    adding text-decoration-underline class

--- src/lib/AlertBuilder/AlertBuilder.js
+++ src/lib/AlertBuilder/AlertBuilder.js
@@ -13,7 +13,7 @@ class AlertBuilder {
               message = 'This record is awaiting review by FAIRsharing curators'
             }
             else {
-              message = 'This record has been modified, but will not be sent to the curation team until all requirements are met. More information is available in our <a class="white--text" href="https://fairsharing.gitbook.io/fairsharing/record-sections-and-fields/how-to-update-a-record#minimal-curation-requirements">documentation</a>.'
+              message = 'This record has been modified, but will not be sent to the curation team until all requirements are met. More information is available in our <a class="white--text text-decoration-underline" href="https://fairsharing.gitbook.io/fairsharing/record-sections-and-fields/how-to-update-a-record#minimal-curation-requirements">documentation</a>.'
             }
             this.alerts['isAwaitingApproval'] = {
                 type: "info",

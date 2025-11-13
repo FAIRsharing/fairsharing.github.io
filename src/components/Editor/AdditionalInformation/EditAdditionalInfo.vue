272a5f406970af7fd4faaf1d9cc507e176cca093
Milo Thurston <knirirr@gmail.com>
2025-11-13 14:10:43 +0000

    Removed console log.

--- src/components/Editor/AdditionalInformation/EditAdditionalInfo.vue
+++ src/components/Editor/AdditionalInformation/EditAdditionalInfo.vue
@@ -406,7 +406,6 @@ export default {
           }
         });
       }
-      console.log(JSON.stringify(Object.keys(output)));
       return output;
     },
     showOverlay(id, fieldName, item, template, required){

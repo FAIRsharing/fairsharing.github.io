13780c835ae2f18221c99c8caac4bd71a6cec953
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-11-27 10:54:31 +0000

    linting fix in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -114,7 +114,8 @@ let routes = [
       let [query, modified] = hackSearch(to.query);
       if (modified) {
         next({ name: "search", query: query });
-      } else {
+      }
+      else {
         next();
       }
     },
@@ -156,7 +157,8 @@ let routes = [
             page: 1,
           },
         };
-      } else if (to.params.name === "live_list_databases_in_policies") {
+      }
+      else if (to.params.name === "live_list_databases_in_policies") {
         return {
           name: "search",
           query: {
@@ -165,7 +167,8 @@ let routes = [
             page: 1,
           },
         };
-      } else if (to.params.name === "live_list_journal_policies") {
+      }
+      else if (to.params.name === "live_list_journal_policies") {
         return {
           name: "search",
           query: {
@@ -174,7 +177,8 @@ let routes = [
             page: 1,
           },
         };
-      } else {
+      }
+      else {
         return { path: "/" };
       }
     },
@@ -216,11 +220,13 @@ let routes = [
         window.location.assign(
           "https://github.com/FAIRsharing/subject-ontology",
         );
-      } else if (to.params.name.toLowerCase() === "drao") {
+      }
+      else if (to.params.name.toLowerCase() === "drao") {
         window.location.assign(
           "https://github.com/FAIRsharing/domain-ontology",
         );
-      } else {
+      }
+      else {
         return { path: "/" };
       }
     },
@@ -811,7 +817,8 @@ export async function beforeEach(to, from, next, store) {
 export function isLoggedIn(to, from, next, store) {
   if (store.state.users.user().isLoggedIn) {
     next();
-  } else {
+  }
+  else {
     const target = to.path;
     next({
       name: "Login", // back to safety route //
@@ -823,7 +830,8 @@ export function isLoggedIn(to, from, next, store) {
 export function isNotLoggedIn(to, from, next, store) {
   if (!store.state.users.user().isLoggedIn) {
     next();
-  } else {
+  }
+  else {
     next(from);
   }
 }
@@ -831,7 +839,8 @@ export function isNotLoggedIn(to, from, next, store) {
 export function isSuperCurator(to, from, next, store) {
   if (store.state.users.user().is_super_curator) {
     next();
-  } else {
+  }
+  else {
     const target = to.path;
     next({
       name: "Login", // back to safety route //

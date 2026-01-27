37154a050bd5693bc3738c5b13d45e6fd4f1ff37
Milo Thurston <knirirr@gmail.com>
2026-01-27 10:41:43 +0000

    Added process.env to a test.

--- tests/unit/components/Global/PublicMessages.spec.js
+++ tests/unit/components/Global/PublicMessages.spec.js
@@ -30,6 +30,7 @@ describe("PublicMessages.vue", () => {
     })
 
     it("can be check moment method", () => {
+        process.env.VUE_APP_API_ENDPOINT = "https://dev-api.fairsharing.org";
         const momentifiedDate = wrapper.vm.moment('2021-08-26T14:24:46');
         expect(momentifiedDate).toBe('Thursday, August 26th 2021, 14:24')
     });

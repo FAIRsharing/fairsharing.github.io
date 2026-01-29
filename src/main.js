7c8718ed6bfc9f2a63d040a5563d8face74b4363
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-01-29 16:01:13 +0000

    remvoing commented code from main.js

--- src/main.js
+++ src/main.js
@@ -3,45 +3,28 @@ import "vue-code-highlight/themes/prism-twilight.css";
 import "vue-code-highlight/themes/window.css";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-// import Highcharts from "highcharts";
-// import options3D from "highcharts/highcharts-3d";
-// import More from "highcharts/highcharts-more";
-// import Drilldown from "highcharts/modules/drilldown";
-// import Export from "highcharts/modules/exporting";
-// import Networkgraph from "highcharts/modules/networkgraph";
-// import Sunburst from "highcharts/modules/sunburst";
-// import Variablepie from "highcharts/modules/variable-pie";
 import HighchartsVue from "highcharts-vue";
 import SimpleAnalytics from "simple-analytics-vue";
 import VueCodeHighlight from "vue-code-highlight";
 import VueGtag from "vue-gtag";
 /* import linkify to turn url within text into an actual url link */
 import linkify from "vue-3-linkify";
-import {createMetaManager} from "vue-meta";
-// import VueMoment from "vue-moment";
+import { createMetaManager } from "vue-meta";
 import Vue3Sanitize from "vue-3-sanitize";
 import VueScrollTo from "vue-scrollto";
 import Particles from "@tsparticles/vue3";
 
 import App from "./App.vue";
-import router, {afterEach, beforeEach} from "./router";
+import router, { afterEach, beforeEach } from "./router";
 import store from "./store";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "vue-json-pretty/lib/styles.css";
-import {createApp} from "vue";
+import { createApp } from "vue";
 import createVuetify from "../src/plugins/vuetify";
-import {loadFull} from "tsparticles";
+import { loadFull } from "tsparticles";
 import Highcharts from "highcharts";
 import accessibilityInit from "highcharts/modules/accessibility";
-
-// Variablepie(Highcharts);
-// More(Highcharts);
-// Export(Highcharts);
-// Networkgraph(Highcharts);
-// Drilldown(Highcharts);
-// options3D(Highcharts);
-// Sunburst(Highcharts);
 
 router.beforeEach(
   async (to, from, next) => await beforeEach(to, from, next, store),
@@ -55,27 +38,20 @@ async function bootstrapApp() {
     await store.dispatch("introspection/fetchParameters");
     await store.dispatch("searchFilters/assembleFilters");
     await store.dispatch("messages/setMessages");
-  }
-  catch (error) {
+  } catch (error) {
     if (error.response && error.response.status === 503) {
       store.commit("introspection/setMaintenanceMode");
-    }
-    else {
+    } else {
       await router.replace("/error/500");
     }
   }
 }
 
-// configureCompat({
-//   MODE: 3,
-// });
-
 //Implement accessibility module for Highcharts
 // check if accessibilityInit is a function, if not try .default
 if (typeof accessibilityInit === "function") {
   accessibilityInit(Highcharts);
-}
-else if (typeof accessibilityInit.default === "function") {
+} else if (typeof accessibilityInit.default === "function") {
   accessibilityInit.default(Highcharts);
 }
 
@@ -91,7 +67,6 @@ const app = createApp(App)
   .use(bootstrapApp)
   .use(HighchartsVue)
   .use(VueScrollTo)
-  // .use(VueMoment)
   .use(createMetaManager())
   .use(VueCodeHighlight)
   .use(Vue3Sanitize)

683b9ff2df1371551ecbbee036a9e14ade53c061
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-02-17 16:19:37 +0000

    fixing QueryBuilderComponents/GroupSlot/index.ja

--- src/components/Records/Search/Input/AdvancedSearch/QueryBuilderComponents/GroupCtrlSlot/index.js
+++ src/components/Records/Search/Input/AdvancedSearch/QueryBuilderComponents/GroupCtrlSlot/index.js
@@ -3,11 +3,3 @@ export { default as DatabaseRule } from "./DatabaseRule.vue";
 export { default as PolicyRule } from "./PolicyRule.vue";
 export { default as StandardRule } from "./StandardRule.vue";
 export { default as FairassistRule } from "./FairassistRule.vue";
-
-export const DatabaseRule = () =>
-  import("./DatabaseRule.vue");
-
-export const PolicyRule = () =>
-  import("./PolicyRule.vue");
-export const StandardRule = () =>
-  import("./StandardRule.vue");

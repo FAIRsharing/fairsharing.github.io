bba3afb7b05499d572170a3e9965bd59424432c8
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-04-09 17:59:03 +0100

    fixing bug-44 logo size in record page

--- src/components/Records/Record/GeneralInfo/DOITitle.vue
+++ src/components/Records/Record/GeneralInfo/DOITitle.vue
@@ -10,7 +10,9 @@
             v-if="currentRecord['fairsharingRecord'].urlForLogo"
             :max-width="finalImageWidth"
             :src="newImg.src"
-            class="mr-2 contain full-width"
+            class="mr-2"
+            contain
+            width="300px"
           />
           <h3
             :style="

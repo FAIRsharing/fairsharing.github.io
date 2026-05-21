8f249dc0bcf97afc03d8d5cebaa4a0318e7016c3
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-05-21 10:46:42 +0100

    adding favicon.ico in config file

--- src/pages/all/+description.js
+++ src/pages/all/+description.js
@@ -9,8 +9,9 @@ export default function description(pageContext) {
       `View the full registry profile for ${record.name} on FAIRsharing.`
     );
   }
-  if (path === "/search") return "Search Records | FAIRsharing";
-  if (path === "/about") return "About Us | FAIRsharing";
+  if (path === "/search") return "Search";
+  if (path === "/advancedsearch")
+    return "Advanced filtering and searching for FAIRsharing records";
 
   return "A curated, informative and educational resource on data and metadata standards, databases and policies.";
 }

5dd50c32602fa8429a29818bca5be58810ff2c86
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-05-22 16:35:01 +0100

    adding title and description for the non-logged in pages

--- src/pages/all/+data.js
+++ src/pages/all/+data.js
@@ -2,18 +2,25 @@ import Client from "@/lib/GraphClient/GraphClient.js";
 import recordQuery from "@/lib/GraphClient/queries/getRecordSEO.json";
 
 export async function data(pageContext) {
-  //Extract the wildcard route parameter (e.g., "3493")
-  const recordId = pageContext.routeParams["*"];
+  const paramURL = pageContext.routeParams["*"];
 
-  // If there's no numeric record ID, return early (handles /search, /about, etc.)
-  if (!recordId || isNaN(recordId)) {
-    return { record: null };
+  // Check for record page in id or DOI format
+  const FAIRsharingDOIregex = /FAIRsharing\.[a-zA-Z0-9]+/;
+  if (isNaN(paramURL) || !FAIRsharingDOIregex.test(paramURL)) {
+    return recordPage(paramURL);
   }
+}
 
+/**
+ * Record Page data
+ * @param paramURL
+ * @return {Promise<{record}|{record: null}>}
+ */
+async function recordPage(paramURL) {
   // Mirror the core execution logic from the recorddata.js action
   const client = new Client();
   recordQuery.queryParam = {
-    id: recordId,
+    id: paramURL,
   };
 
   try {
@@ -26,7 +33,7 @@ export async function data(pageContext) {
   catch (error) {
     console.error(
       "Failed to execute server-side SEO query for ID:",
-      recordId,
+      paramURL,
       error,
     );
     return { record: null };

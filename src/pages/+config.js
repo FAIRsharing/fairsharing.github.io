b753ea60a45f8cd39fd4f0c05e72889eb086a46d
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2026-05-26 13:56:23 +0100

    removing index.

--- src/pages/+config.js
+++ src/pages/+config.js
@@ -5,5 +5,9 @@ export default {
   extends: [vikeVue],
   Layout,
   ssr: true,
+
   favicon: "/assets/favicon.ico",
+  title: "FAIRsharing",
+  viewport:
+    "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no",
 };
